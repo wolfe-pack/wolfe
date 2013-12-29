f692126e5b817abd8ac4c1a732c362b6dcaa8b22
riedelcastro <sebastian.riedel@gmail.com>
2013-12-29 20:10:02 +0000

    does a off-the-shelf benchmark work?

--- src/test/scala/scalapplcodefest/benchmark/MPGraphCompilationBenchmark.scala
+++ src/test/scala/scalapplcodefest/benchmark/MPGraphCompilationBenchmark.scala
@@ -18,63 +18,63 @@ import java.security.AccessController
 import sun.security.action.GetPropertyAction
 
 
-object MPGraphCompilationBenchmark extends PerformanceTest.Regression {
-
-  import scalapplcodefest.TermDSL._
-
-  def persistor = new SerializationPersistor("target")
-  //  def persistor = new DummyPersistor
-
-
-  override def reporter = Reporter.Composite(
-    new RegressionReporter(
-      RegressionReporter.Tester.OverlapIntervals(),
-      RegressionReporter.Historian.ExponentialBackoff()),
-    HtmlReporter(true)
-  )
-
-  override def executor: Executor =
-    new MySeperateJVMExecutor(SeparateJvmsExecutor(warmer, aggregator, measurer))
-
-
-  val lengths: Gen[Int] = Gen.range("length")(5, 10, 5)
-
-  val models = for (l <- lengths) yield linearChain(l, 5)
-
-  performance of "MPGraph compilation" in {
-    measure method "compile" in {
-      using(models) config(
-        exec.maxWarmupRuns -> 2,
-        exec.benchRuns -> 2,
-        exec.independentSamples -> 2
-        ) in {
-        model =>
-          MPGraphCompiler.compile(model.sig, TermConverter.pushDownConditions(model.body))
-      }
-    }
-
-  }
-
-  def linearChain(sentenceLength: Int, labelCount: Int) = {
-    val key = new Index
-    val n = 'n of ints
-    val k = 'k of ints
-    val labels = 0 ~~ k
-    val tokens = 0 ~~ n
-    val label = 'label of tokens |-> labels
-    val word = 'word of tokens |-> strings
-    val weights = 'weights of vectors
-    val bias = vectors.sum(for (i <- tokens) yield unit(key('bias, label(i))))
-    val emission = vectors.sum(for (i <- tokens) yield unit(key('emission, label(i), word(i))))
-    val trans = vectors.sum(for (i <- 0 ~~ (n - 1)) yield unit(key('trans, label(i), label(i + 1))))
-    val model = (bias + emission + trans) dot weights
-    val words = Range(0, sentenceLength).map(i => word.atom(i) -> "A word").toMap
-    val condition = State(words.asInstanceOf[Map[Variable[Any], Any]]) + state(n -> sentenceLength, k -> labelCount)
-    lam(label, model | condition)
-  }
-
-
-}
+//object MPGraphCompilationBenchmark extends PerformanceTest.Regression {
+//
+//  import scalapplcodefest.TermDSL._
+//
+//  def persistor = new SerializationPersistor("target")
+//  //  def persistor = new DummyPersistor
+//
+//
+//  override def reporter = Reporter.Composite(
+//    new RegressionReporter(
+//      RegressionReporter.Tester.OverlapIntervals(),
+//      RegressionReporter.Historian.ExponentialBackoff()),
+//    HtmlReporter(true)
+//  )
+//
+//  override def executor: Executor =
+//    new MySeperateJVMExecutor(SeparateJvmsExecutor(warmer, aggregator, measurer))
+//
+//
+//  val lengths: Gen[Int] = Gen.range("length")(5, 10, 5)
+//
+//  val models = for (l <- lengths) yield linearChain(l, 5)
+//
+//  performance of "MPGraph compilation" in {
+//    measure method "compile" in {
+//      using(models) config(
+//        exec.maxWarmupRuns -> 2,
+//        exec.benchRuns -> 2,
+//        exec.independentSamples -> 2
+//        ) in {
+//        model =>
+//          MPGraphCompiler.compile(model.sig, TermConverter.pushDownConditions(model.body))
+//      }
+//    }
+//
+//  }
+//
+//  def linearChain(sentenceLength: Int, labelCount: Int) = {
+//    val key = new Index
+//    val n = 'n of ints
+//    val k = 'k of ints
+//    val labels = 0 ~~ k
+//    val tokens = 0 ~~ n
+//    val label = 'label of tokens |-> labels
+//    val word = 'word of tokens |-> strings
+//    val weights = 'weights of vectors
+//    val bias = vectors.sum(for (i <- tokens) yield unit(key('bias, label(i))))
+//    val emission = vectors.sum(for (i <- tokens) yield unit(key('emission, label(i), word(i))))
+//    val trans = vectors.sum(for (i <- 0 ~~ (n - 1)) yield unit(key('trans, label(i), label(i + 1))))
+//    val model = (bias + emission + trans) dot weights
+//    val words = Range(0, sentenceLength).map(i => word.atom(i) -> "A word").toMap
+//    val condition = State(words.asInstanceOf[Map[Variable[Any], Any]]) + state(n -> sentenceLength, k -> labelCount)
+//    lam(label, model | condition)
+//  }
+//
+//
+//}
 
 class DummyPersistor extends Persistor {
   def load(context: Context) = {
@@ -101,5 +101,22 @@ class MySeperateJVMExecutor(executor: SeparateJvmsExecutor) extends Executor {
     val tmp = File.createTempFile("blah", "blub")
     println(tmp.getAbsolutePath)
     executor.runSetup(setup)
+  }
+}
+
+class RegressionTest extends PerformanceTest.Regression {
+  def persistor = new SerializationPersistor
+  val sizes = Gen.range("size")(1000000, 5000000, 2000000)
+  val arrays = for (sz <- sizes) yield (0 until sz).toArray
+
+  performance of "Array" in {
+    measure method "foreach" in {
+      using(arrays) config (
+        exec.independentSamples -> 1
+        ) in { xs =>
+        var sum = 0
+        xs.foreach(x => sum += x)
+      }
+    }
   }
 }
