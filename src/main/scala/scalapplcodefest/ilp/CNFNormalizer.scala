560d822e6d4cf3a1117896ddd170c8a544e2eb65
Jan Noessner <jan.noessner@gmail.com>
2013-12-30 16:16:08 +0100

    CNFNormalizer first version

--- src/main/scala/scalapplcodefest/ilp/CNFNormalizer.scala
+++ src/main/scala/scalapplcodefest/ilp/CNFNormalizer.scala
@@ -1,51 +1,134 @@
 package scalapplcodefest.ilp
 
 import scalapplcodefest.TermDSL._
-import scalapplcodefest.TermConverter._
 import scalapplcodefest.term._
 import scalapplcodefest.MPGraphCompiler.factorize
-import scalapplcodefest.value.Bools.Implies
-import scalapplcodefest.value.Vectors.Dot
-import scalapplcodefest.value.{Reduce, Fun}
+import scalapplcodefest.value.Fun
 import scalapplcodefest.value.Reduce
 import scalapplcodefest.term.Var
 import scalapplcodefest.term.TupleTerm2
 import scalapplcodefest.term.FunApp
 
 /**
- * User: rockt
- * Date: 12/29/13
- * Time: 11:00 AM
+ * Transformes all formulae to CNF by factorizing (unrolling) them beforehand.
  */
 
 object CNFNormalizer {
   def apply[T](base: Term[T], op: ConstantOperator[T]): Term[T] = {
-    def normalize(term: Term[_]): Term[_] = term match {
-      case FunApp(bools.implies, TupleTerm2(left, right)) =>
-        println("Found it!")
-        term
-//      case FunApp(vectors.dot, TupleTerm2(left, right)) =>
-//        FunApp(vectors.dot.asInstanceOf[Term[Fun[Any, Any]]], TupleTerm2(normalize(left), normalize(right)))
-//      case FunApp(vectors.add, TupleTerm2(left, right)) =>
-//        FunApp(vectors.add.asInstanceOf[Term[Fun[Any, Any]]], TupleTerm2(normalize(left), normalize(right)))
-      case Reduce(operator, arguments) =>
-        Reduce(operator.asInstanceOf[Term[Fun[(Any, Any), Any]]], normalize(arguments).asInstanceOf[Term[Seq[Any]]])
-      case SeqTerm(seq) => SeqTerm(seq map normalize)
-      case Var(_, _) => term
-      case FunApp(operator, TupleTerm2(left, right)) => FunApp(operator, TupleTerm2(normalize(left), normalize(right)))
-      case FunApp(operator, inner) => FunApp(operator, normalize(inner))
-      case _ =>
-        println("TODO: " + term.getClass)
-        term
+    val myImplies = bools.implies.asInstanceOf[Term[Fun[Any, Boolean]]]
+    val myAnd = bools.and.asInstanceOf[Term[Fun[Any, Boolean]]]
+    val myOr = bools.or.asInstanceOf[Term[Fun[Any, Boolean]]]
+    val myNeg = bools.neg.asInstanceOf[Term[Fun[Any, Boolean]]]
+
+    // recursive method for normalization
+    def normalize(term: Term[_]): Term[_] = {
+      /**
+       * Checks if formula is normalized.
+       *
+       * @param term given factorized formula
+       * @return
+       */
+      def checkIfNormalized(term: Term[_]): Boolean = {
+        def checkForOr(term: Term[_]): Boolean = {
+          term match {
+            case FunApp(bools.and | bools.equiv | bools.implies, _) =>
+              false
+            case FunApp(bools.or, TupleTerm2(left, right)) =>
+              checkForOr(left) && checkForOr(right)
+            case predCon@(Predicate(_, _, _) | Constant(_)) => true
+            case FunApp(bools.neg, predCon) => true
+            case FunApp(Predicate(_, _, _), t) => true
+            case FunApp(Constant(_), t) => true
+            case _ =>
+              false
+          }
+        }
+        term match {
+          case FunApp(bools.and, TupleTerm2(left, right)) => checkForOr(left) && checkForOr(right)
+          case _ => checkForOr(term)
+        }
+      }
+
+      /**
+       * Transforms a factorized (unrolled, etc.) formula to CNF.
+       *
+       * @param term
+       * @return
+       */
+      def normalizeFormula(term: Term[_]): Term[_] = term match {
+        //If φ has the form P -> Q, then:
+        //Return CONVERT(~P v Q). // equivalent
+        case FunApp(bools.implies, TupleTerm2(left, right)) =>
+          normalize(FunApp(myOr, TupleTerm2(FunApp(myNeg, left), right)))
+        //If φ has the form P <-> Q, then: return CONVERT((P ^ Q) v (~P ^ ~Q)).
+        case FunApp(bools.equiv, TupleTerm2(left, right)) =>
+          normalize(FunApp(myAnd, TupleTerm2(
+            FunApp(myImplies, TupleTerm2(left, right)),
+            FunApp(myImplies, TupleTerm2(right, left))
+          )))
+        // If φ has the form ~(~P), then return CONVERT(P). // double negation
+        case FunApp(bools.neg, FunApp(bools.neg, expr)) =>
+          normalize(expr)
+        // If φ has the form ~(P ^ Q), then return CONVERT(~P v ~Q). // de Morgan's Law
+        case FunApp(bools.neg, FunApp(bools.and, TupleTerm2(left, right))) =>
+          normalize(FunApp(myOr, TupleTerm2(FunApp(myNeg, left), FunApp(myNeg, right))))
+        // If φ has the form ~(P v Q), then return CONVERT(~P ^ ~Q). // de Morgan's Law
+        case FunApp(bools.neg, FunApp(bools.or, TupleTerm2(left, right))) =>
+          normalize(FunApp(myAnd, TupleTerm2(FunApp(myNeg, left), FunApp(myNeg, right))))
+        //  If φ has the form (P1 ^ P2)  v Q, then:
+        //  return CONVERT((P1 v Q) ^ (P2 v Q))
+        case FunApp(bools.or, TupleTerm2(FunApp(bools.and, TupleTerm2(left, right)), expr)) =>
+          normalize(FunApp(myAnd, TupleTerm2(FunApp(myOr, TupleTerm2(left, expr)), FunApp(myOr, TupleTerm2(right, expr)))))
+        //  If φ has the form P v (Q1 ^ Q2), then:
+        //  return CONVERT((P v Q1) ^ (P v Q2))
+        case FunApp(bools.or, TupleTerm2(expr, FunApp(bools.and, TupleTerm2(left, right)))) =>
+          normalize(FunApp(myAnd, TupleTerm2(FunApp(myOr, TupleTerm2(expr, left)), FunApp(myOr, TupleTerm2(expr, right)))))
+        //  If φ has the form P v Q, then:
+        //  return CONVERT(P) v CONVERT(Q)
+        case FunApp(bools.or, TupleTerm2(left, right)) =>
+          FunApp(myOr, TupleTerm2(normalize(left), normalize(right)))
+        //  If φ has the form P ^ Q, then:
+        //  return CONVERT(P) ^ CONVERT(Q)
+        case FunApp(bools.and, TupleTerm2(left, right)) =>
+          FunApp(myAnd, TupleTerm2(normalize(left), normalize(right)))
+        // If φ is a constant, then: return φ.
+        case conpred@(Constant(_) | Predicate(_, _, _)) =>
+          term
+        // If φ has the form ~A for some constant A, then return φ.
+        case FunApp(bools.neg, conpred) =>
+          term
+        case _ =>
+          println("not yet covered " + term + " class " + term.getClass)
+          term
+      }
+      term match {
+        // dig to the actual formulas
+        case Reduce(operator, arguments) =>
+          Reduce(operator.asInstanceOf[Term[Fun[(Any, Any), Any]]], normalize(arguments).asInstanceOf[Term[Seq[Any]]])
+        case SeqTerm(seq) => SeqTerm(seq map normalize)
+        case Var(_, _) => term
+        // TODO How can I ensure that this is only called once per formula?
+        case FunApp(ops@(bools.equiv | bools.implies | bools.or | bools.and | bools.neg), _) =>
+          var normalizedTerm = normalizeFormula(term)
+          //println("normalized " + normalizedTerm + " isNormalized " + checkIfNormalized(normalizedTerm))
+          while (!checkIfNormalized(normalizedTerm)) {
+            normalizedTerm = normalizeFormula(normalizedTerm)
+          }
+          normalizedTerm
+        case FunApp(operator, TupleTerm2(left, right)) if ((operator != bools.equiv) && (operator != bools.implies) && (operator != bools.or) && (operator != bools.and) && (operator != bools.neg)) =>
+          FunApp(operator, TupleTerm2(normalize(left), normalize(right)))
+        case FunApp(operator, inner) if ((operator != bools.equiv) && (operator != bools.implies) && (operator != bools.or) && (operator != bools.and) && (operator != bools.neg)) =>
+          FunApp(operator, normalize(inner))
+        case Constant(_) => term
+        case _ =>
+          println("not yet covered: " + term + " class " + term.getClass)
+          term
+      }
     }
-
+    // factorize term
     val factorizedTerm = factorize[T](base, op)
-
-    println("Seq:")
-    println(factorizedTerm mkString "\n")
-
+    // normalize term
     val normalized = factorizedTerm map normalize
-
     normalized.head.asInstanceOf[Term[T]]
   }
 }
