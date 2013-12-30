f4322cbf5941d24d05b326f9f3bc0d673e77e6e8
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2013-12-30 16:02:12 +0000

    tracking problematic terms in TermDebugger

--- src/main/scala/scalapplcodefest/example/MatrixFactorizationExample.scala
+++ src/main/scala/scalapplcodefest/example/MatrixFactorizationExample.scala
@@ -56,18 +56,13 @@ object MatrixFactorizationExample extends App {
 
   val predict = model | learned | kb
 
-  //val query = predict(Constant("r1"), Constant("e1"), Constant("e2")) //rockt: working
+  //rockt: working
+  //val query = predict(Constant("r1"), Constant("e1"), Constant("e2"))
+  //println("score of r1(e1,e2): " + query.value())
+
 
   val query = predict(Constant("r2"), Constant("e1"), Constant("e2"))
-
-  //println(TermToDebugString(query))
-
   TermDebugger(query)
-
-  println("score of r2(e1,e2): " + query.value())
-
-  //println("score of r1(e1,e2): " + query.value())
-  println("score of r2(e1,e2): " + query.value()) //FIXME
 
   //TODO: objective
   // || I(kbPred(r,e1,e2)) - (model | learned | kb(r,e1,e2) -> true) ||^2
@@ -78,12 +73,65 @@ object MatrixFactorizationExample extends App {
 
 object TermDebugger {
   def apply(term: Term[_]) = {
-    println("trying")
+//    try term.value() catch {
+//      case e: NoSuchElementException =>
+//        System.err.println(e)
+//        System.err.println("========== Model ==========")
+//        System.err.println(TermToDebugString(term))
+//        System.err.println("===========================")
+//        System.err.println(e.getStackTraceString)
+//    }
 
-    try term.value() catch {
-      case e: NoSuchElementException => {
-        println("woops!")
+    var problem: Term[_] = term
+
+    def crashes(probe: Term[_]): Boolean = {
+      var result = false
+      try probe.value() catch {
+        case e: Exception => result = true
+        case _ => result = false
       }
+      result
+    }
+
+    def check(probe: Term[_]) = if (crashes(probe)) problem = probe
+
+    def diagnose(probe: Term[_], depth: Int = 0): Unit = {
+      check(probe)
+      probe match {
+        case t: Predicate[_, _] => //nothing to do
+        case t: Var[_] => //nothing to do
+        case t: LambdaAbstraction[_, _] =>
+          diagnose(t.sig)
+          diagnose(t.body)
+        case t: FunApp[_, _] =>
+          diagnose(t.function)
+          diagnose(t.arg)
+        case t: TupleTerm2[_, _] =>
+          diagnose(t.a1)
+          diagnose(t.a2)
+        case t: TupleTerm3[_, _, _] =>
+          diagnose(t.a1)
+          diagnose(t.a2)
+          diagnose(t.a3)
+        case t: Reduce[_] =>
+          diagnose(t.op)
+          diagnose(t.arguments)
+        case t: SeqTerm[_] =>
+          t.componentSeq.foreach(diagnose(_))
+        case t: Conditioned[_] =>
+          t.componentSeq.foreach(diagnose(_))
+        case t: Composite[_] =>
+          t.componentSeq.foreach(diagnose(_))
+        case t: Term[_] =>
+          println("TODO: " + term.getClass)
+        case _ => throw new NotImplementedError(s"I don't know yet how to debug a $term")
+      }
+      println("tracking problem: " + problem)
+    }
+
+    if (crashes(term)) {
+      println(TermToDebugString(term))
+      diagnose(term)
     }
   }
 }
@@ -106,7 +154,7 @@ object TermToDebugString {
       case t: Term[_] =>
         //println("TODO: " + term.getClass)
         term.toString
-      case _ => throw new NotImplementedError(s"I don't know yet how to debug a $term")
+      case _ => throw new NotImplementedError(s"I don't know yet how to prettify the output of a $term")
     }
   }
 }
