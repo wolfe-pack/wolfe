4f5b1dddb0bfade6ee4ac31c3b2e5a27527085c1
visenger <visenger@gmail.com>
2013-12-30 10:40:28 +0000

    clean up;

--- src/test/scala/scalapplcodefest/mln/Example.scala
+++ src/test/scala/scalapplcodefest/mln/Example.scala
@@ -48,18 +48,4 @@ class MLNTest extends FlatSpec with Matchers {
 
 }
 
-object Test {
-  def main(args: Array[String]) {
-    val x = Test(5)
-    println(x)
-    x match {
-      case Test(num) => println(x + " is bigger two times than " + num)
-      //unapply is invoked
-      case _ => println("i cannot calculate")
-    }
-  }
 
-  def apply(x: Int) = x * 2
-
-  def unapply(z: Int): Option[Int] = if (z % 2 == 0) Some(z / 2) else None
-}
