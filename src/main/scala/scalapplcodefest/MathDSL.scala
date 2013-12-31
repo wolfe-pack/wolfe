81fc47ea1b0bf6200aecc989ac236e8d3ad627ed
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2013-12-31 11:56:22 +0000

    type erasure work-around

--- src/main/scala/scalapplcodefest/MathDSL.scala
+++ src/main/scala/scalapplcodefest/MathDSL.scala
@@ -3,6 +3,7 @@ package scalapplcodefest
 import scalapplcodefest.term._
 import scalapplcodefest.value.Fun
 import scalapplcodefest.term.LambdaAbstraction
+import scalapplcodefest.term.Reduce
 
 /**
  * User: rockt
@@ -42,5 +43,53 @@ object MathDSL {
     val feat = 'feat of vectors
 
     val model = θ ∘ feat
+
+
+    //val is = Σ(for (i <- 0 ~~ n) yield i)
+    //println(is.value(n -> 3))
+
+    def doMagic(a: Array[_]) = a match {
+      case b: Array[Int] => b.head
+      case b: Array[Double] => b.last
+      case b: Array[String] => "blub"
+    }
+
+    println(doMagic(Array(1, 2, 3)))
+    println(doMagic(Array(1.0, 2.0, 3.0)))
+    println(doMagic(Array("abc", "efg")))
+    
+    def doWondersBuggy(a: Array[(_, _)]) = a match {
+      case b: Array[(_, Int)] => b.head._2
+      case b: Array[(_, Double)] => b.last._2
+      case b: Array[(_, String)] => "blub"
+    }
+
+    println(doWondersBuggy(Array("a" -> 1, "b" -> 2, "c" -> 3)))
+    println(doWondersBuggy(Array("a" -> 1.0, "b" -> 2.0, "c" -> 3.0)))
+    println(doWondersBuggy(Array("a" -> "abc", "b" -> "def")))
+
+    import reflect._
+    class Def[C](implicit desired : Manifest[C]) {
+      def unapply[X](c : X)(implicit m : Manifest[X]) : Option[C] = {
+        def sameArgs = desired.typeArguments.zip(m.typeArguments).forall { case (desired, actual) => desired >:> actual }
+        if (desired >:> m && sameArgs) Some(c.asInstanceOf[C])
+        else None
+      }
+    }
+
+    val IntTupleArray = new Def[Array[(_, Int)]]
+    val DoubleTupleArray = new Def[Array[(_, Double)]]
+    val StringTupleArray = new Def[Array[(_, String)]]
+
+    def doWonders[A](a: A)(implicit m: Manifest[A]) = a match {
+      case IntTupleArray(b) => b.head._2
+      case DoubleTupleArray(b) => b.last._2
+      case StringTupleArray(b) => "blub"
+      case _ => println("I don't know yet what to do with: " + a)
+    }
+
+    println(doWonders(Array("a" -> 1, "b" -> 2, "c" -> 3)))
+    println(doWonders(Array("a" -> 1.0, "b" -> 2.0, "c" -> 3.0)))
+    println(doWonders(Array("a" -> "abc", "b" -> "def")))
   }
 }
