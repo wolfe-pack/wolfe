070c0298af3670b77495a5d7925723b847f4a0e0
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2014-01-02 16:02:26 +0000

    introduced Tree alias for Global#Tree changed Tree to CompilationUnit

--- src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
@@ -1,10 +1,40 @@
 package scalapplcodefest.compiler
 
-import scala.tools.nsc.Global
-
 /**
  * @author sameer
  */
 trait WolfeTransformer {
-  def transform(tree: Tree): Tree
+  def transform[T <: CompilationUnit](unit: T): T
 }
+
+/**
+ * Prints the abstract syntax tree
+ */
+class DummyTransformer extends WolfeTransformer {
+  def transform[Tree](tree: Tree): Tree = {
+    tree match {
+      case ??? => println(tree)
+      case _ => println("Can't compile")
+    }
+
+    tree
+  }
+}
+
+object TransformerApp extends App {
+  val compiler = new StringCompiler
+
+  val code =
+    """
+      | object A {
+      |   val x = 1
+      | }
+    """.stripMargin
+
+  val tree = compiler.compileCode(code)
+
+  val transformer = new DummyTransformer
+  transformer.transform(tree)
+}
+
+
