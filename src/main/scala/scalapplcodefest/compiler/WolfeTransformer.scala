25ae843de5cf79db89546d2b8f56724a6166cb58
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2014-01-02 17:01:04 +0000

    local commit

--- src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
@@ -3,6 +3,7 @@ package scalapplcodefest.compiler
 import scala.reflect.internal.Trees
 import scalapplcodefest.compiler._
 import scala.tools.nsc.Global
+import scala.tools.nsc.transform.Transform
 
 /**
  * @author sameer
@@ -18,6 +19,9 @@ class DummyTransformer extends WolfeTransformer {
 
   def transform(global: Global)(unit: global.type#CompilationUnit): Unit = {
     unit.body match {
+      case global.PackageDef(pid, stats) => println("found PackageDef " + pid.name)
+      case global.ModuleDef(mods, name, impl) => println("found Module: " + name)
+      case global.TypeDef(_, name, _, rhs) => println(name)
       case _ => println("Can't compile")
     }
   }
@@ -39,7 +43,14 @@ object TransformerApp extends App {
       | }
     """.stripMargin
 
-  val (global, unit) = compiler.compileCode(code)
+  val mln =
+    """
+      | object Foo {
+      |   type Persons = String
+      | }
+    """.stripMargin
+
+  val (global, unit) = compiler.compileCode(mln)
 
   val transformer = new DummyTransformer
   transformer.transform(global)(unit.asInstanceOf[global.CompilationUnit])
