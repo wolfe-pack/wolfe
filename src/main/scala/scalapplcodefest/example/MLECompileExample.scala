fe864494c7e22f164af3520e5b7169385c44cc53
riedelcastro <sebastian.riedel@gmail.com>
2014-01-03 10:34:25 +0000

    now with simplex

--- src/main/scala/scalapplcodefest/example/MLECompileExample.scala
+++ src/main/scala/scalapplcodefest/example/MLECompileExample.scala
@@ -49,13 +49,13 @@ object MLECompileExample {
     val path = dirPathOfClass(getClass.getName)
     val compiler = new StringCompiler(additionalClassPath = List(path))
     val (global, unit) = compiler.compileCode(source)
-//
+    //
     val tree = unit.body.asInstanceOf[global.Tree]
 
-//    println(jarPathOfClass("scalapplcodefest.example.MLECompileExample"))
+    //    println(jarPathOfClass("scalapplcodefest.example.MLECompileExample"))
     global.treeBrowser.browse(tree)
-//    val path = dirPathOfClass(getClass.getName)
-//    println(path)
+    //    val path = dirPathOfClass(getClass.getName)
+    //    println(path)
   }
 
   object MLETransformer extends WolfeTransformer {
@@ -66,7 +66,7 @@ object MLECompileExample {
     }
   }
 
-  private def dirPathOfClass(className:String) = try {
+  private def dirPathOfClass(className: String) = try {
     val resource = className.split('.').mkString("/", "/", ".class")
     val path = getClass.getResource(resource).getPath
     val root = path.dropRight(resource.length)
@@ -76,10 +76,9 @@ object MLECompileExample {
 }
 
 
-
 object WolfeEnv {
   def funs[A, B](dom: Set[A], range: Set[B]): Set[A => B] = ???
-  def seqs[A](dom:Set[A],length:Int):Set[Seq[A]] = ???
+  def seqs[A](dom: Set[A], length: Int): Set[Seq[A]] = ???
 
   @Operator.Sum
   def sum[T](elems: Seq[T])(f: T => Double) = elems.map(f).sum
@@ -92,9 +91,14 @@ object WolfeEnv {
     dom.maxBy(obj)
   }
 
-  type Query[T,M] = (T=>M,Set[M])
+  @Domain.Simplex
+  def simplex[T](domain: Set[T], range: Set[Double] = doubles) =
+    for (p <- funs(domain,range); if sum(domain.toSeq) {p(_)} == 1.0 && domain.forall(p(_) >= 0.0)) yield p
 
-  def expect[K,M,T](dom:Set[T],queries:Map[K,Query[T,M]])(obj: T => Double): Map[K,M=>Double] = {
+
+  type Query[T, M] = (T => M, Set[M])
+
+  def expect[K, M, T](dom: Set[T], queries: Map[K, Query[T, M]])(obj: T => Double): Map[K, M => Double] = {
     type Marg = Seq[M => T]
     ???
   }
@@ -105,23 +109,56 @@ object WolfeEnv {
 
   val strings: Set[String] = Iterator.continually(math.random.toString).toSet
 
-  case class RichSet[T](set:Set[T]) {
+  case class RichSet[T](set: Set[T]) {
     def ->[B](that: Set[B]) = funs(set, that)
   }
+
   implicit def toRichSet[T](set: Set[T]) = RichSet(set)
 
   implicit def toSeq[T](seq: Set[T]) = seq.toSeq
 
-  case class RichCurried[A1,A2,B](f: A1 => A2 => B) {
-    def apply(pair:(A2,A1)) = f(pair._2)(pair._1)
-  }
-  implicit def toRichCurried[A1,A2,B](f: A1 => A2 => B)= new RichCurried(f)
-
-  implicit class BarBuilder[T](t:T) {
-    def |[A](that:A) = t -> that
+  case class RichCurried[A1, A2, B](f: A1 => A2 => B) {
+    def apply(pair: (A2, A1)) = f(pair._2)(pair._1)
   }
 
+  implicit def toRichCurried[A1, A2, B](f: A1 => A2 => B) = new RichCurried(f)
 
+  implicit class BarBuilder[T](t: T) {
+    def |[A](that: A) = t -> that
+  }
 
 
 }
+
+
+
+object Operator {
+
+  class Argmax extends scala.annotation.StaticAnnotation
+
+  class Sum extends scala.annotation.StaticAnnotation
+
+  class Max extends scala.annotation.StaticAnnotation
+
+
+}
+
+object Objective {
+
+  class LogLikelihood extends scala.annotation.StaticAnnotation
+
+  class MaxProduct(iterations: Int) extends scala.annotation.StaticAnnotation
+
+}
+
+object Domain {
+
+  class PMF extends scala.annotation.StaticAnnotation
+
+  class Simplex extends scala.annotation.StaticAnnotation
+
+  class Marginals extends scala.annotation.StaticAnnotation
+
+}
+
+
