667f1a57f787878da2949be25c8fa03dc205767a
Sameer Singh <sameeersingh@gmail.com>
2014-01-03 17:19:12 +0000

    new transformer interface

--- src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
@@ -6,41 +6,16 @@ import scala.tools.nsc.Global
  * @author sameer
  */
 trait WolfeTransformer {
-  def transform(global: Global)(unit: global.type#CompilationUnit): Unit
-  def transformTree[T <: Global#Tree](global: Global, tree: T): T = tree
+  def transformTree[T <: Global#Tree](global: Global, tree: T): T
 }
 
 /**
  * Prints the abstract syntax tree
  */
 class DummyTransformer extends WolfeTransformer {
-
   override def transformTree[T <: Global#Tree](g2: Global, tree: T) = {
     println(tree)
     tree.asInstanceOf[T]
-  }
-
-  def transform(global: Global)(unit: global.type#CompilationUnit): Unit = {
-    for (tree <- unit.body) {
-      transformTree(global,tree)
-    }
-
-    //for debugging
-    //global.treeBrowser.browse(unit.body)
-
-    println()
-    println("after transformation:")
-    println()
-
-    transformTest(global)(unit)
-  }
-
-  def transformTest(global: Global)(unit: global.type#CompilationUnit): Unit = {
-    //testing AST transformer template
-    val transformed = TreeOperations.noopTemplate(global)(unit.body)
-    for (tree <- transformed) {
-      transformTree(global,tree)
-    }
   }
 }
 
