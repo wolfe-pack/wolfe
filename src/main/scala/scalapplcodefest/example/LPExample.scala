6a53f64b81a007091dab8b55732f9d01391aff3e
riedelcastro <sebastian.riedel@gmail.com>
2014-01-03 23:39:33 +0000

    silly LP example.

---
+++ src/main/scala/scalapplcodefest/example/LPExample.scala
@@ -0,0 +1,32 @@
+package scalapplcodefest.example
+
+import scalapplcodefest.Wolfe
+
+/**
+ * @author Sebastian Riedel
+ */
+object LPExample extends App {
+
+  import Wolfe._
+
+  case class Data(edge: Map[(Int, Int), Double])
+
+  def tokens = Range(0, 3).toSet
+  def edges = for (edge <- c(tokens, tokens) -> Set(0.0, 0.5, 1.0)) yield Data(edge)
+
+  def constraints(data: Data) = {
+    import data._
+    forall (tokens) {m => sum(tokens) {h => 1.0 * edge(h, m)} == 1.0}
+  }
+
+  def obj(coeff: Map[(Int, Int), Double])(data:Data) = {
+    import data._
+    sum (c(tokens,tokens)) {case (h,m) => edge(h,m) * coeff(h,m)}
+  }
+
+  val coeffs = Map((1,3) -> 0.3) withDefaultValue 0.0
+
+  val solution = argmax(edges filter constraints) { obj(coeffs)}
+
+
+}
