2b4eae0d7bcca30044689900503425f9e16450a0
riedelcastro <sebastian.riedel@gmail.com>
2014-01-03 17:31:08 +0000

    example now in linear form

--- src/main/scala/scalapplcodefest/example/MLECompileExample.scala
+++ src/main/scala/scalapplcodefest/example/MLECompileExample.scala
@@ -226,6 +226,7 @@ object Objective {
   trait GradientBasedOptimizerSetting
 
   case class Adagrad(rate:Double) extends GradientBasedOptimizerSetting
+
   class JointLoglikelihood extends StaticAnnotation
 
   case class MaxProduct(iterations:Int) extends InferenceSetting
@@ -237,6 +238,8 @@ object Objective {
 
 
   class LinearModel(setting:InferenceSetting = MaxProduct(1)) extends StaticAnnotation
+
+  class Categorical extends StaticAnnotation
 
   class Atomic extends StaticAnnotation
 
