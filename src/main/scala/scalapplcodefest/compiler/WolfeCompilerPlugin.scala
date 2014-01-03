3417766b1c00f42dffdb9712bd234a900ec22240
Sameer Singh <sameeersingh@gmail.com>
2014-01-03 17:11:17 +0000

    support for global.Transformer in plugin

--- src/main/scala/scalapplcodefest/compiler/WolfeCompilerPlugin.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeCompilerPlugin.scala
@@ -5,11 +5,12 @@ import nsc.Global
 import nsc.Phase
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
+import scala.tools.nsc.transform.Transform
 
 /**
  * @author sameer
  */
-class WolfeCompilerPlugin(val global: Global, transform: WolfeTransformer) extends Plugin {
+class WolfeCompilerPlugin(val global: Global, t: WolfeTransformer) extends Plugin {
 
   import global._
 
@@ -17,28 +18,16 @@ class WolfeCompilerPlugin(val global: Global, transform: WolfeTransformer) exten
   val description = "compiles and transforms wolfe code"
   val components = List[PluginComponent](Component)
 
-  private object Component extends PluginComponent {
+  private object Component extends PluginComponent with Transform {
     val global: WolfeCompilerPlugin.this.global.type = WolfeCompilerPlugin.this.global
-    val runsAfter = List("refchecks")
-    // Using the Scala Compiler 2.8.x the runsAfter should be written as below
-    // val runsAfter = List[String]("refchecks");
+    val runsAfter = List("typer")
     val phaseName = WolfeCompilerPlugin.this.name
-    def newPhase(_prev: Phase) = new WolfePhase(_prev)
 
-    class WolfePhase(prev: Phase) extends StdPhase(prev) {
-      override def name = WolfeCompilerPlugin.this.name
-      def apply(unit: CompilationUnit) {
-        transform.transform(global)(unit)
-        /*
-        for ( tree @ Apply(Select(rcvr, nme.DIV), List(Literal(Constant(0)))) <- unit.body;
-             if rcvr.tpe <:< definitions.IntClass.tpe) 
-          {
-            unit.error(tree.pos, "definitely division by zero")
-          }
-        */
+    protected def newTransformer(unit: global.CompilationUnit): global.Transformer = new global.Transformer {
+      override def transform(tree: global.Tree) = {
+        t.transformTree[global.Tree](global, super.transform(tree))
       }
     }
-
   }
 
 }
--- src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
@@ -7,6 +7,7 @@ import scala.tools.nsc.Global
  */
 trait WolfeTransformer {
   def transform(global: Global)(unit: global.type#CompilationUnit): Unit
+  def transformTree[T <: Global#Tree](global: Global, tree: T): T = tree
 }
 
 /**
@@ -14,9 +15,14 @@ trait WolfeTransformer {
  */
 class DummyTransformer extends WolfeTransformer {
 
+  override def transformTree[T <: Global#Tree](g2: Global, tree: T) = {
+    println(tree)
+    tree.asInstanceOf[T]
+  }
+
   def transform(global: Global)(unit: global.type#CompilationUnit): Unit = {
     for (tree <- unit.body) {
-      transformTree(global)(tree)
+      transformTree(global,tree)
     }
 
     //for debugging
@@ -33,34 +39,7 @@ class DummyTransformer extends WolfeTransformer {
     //testing AST transformer template
     val transformed = TreeOperations.noopTemplate(global)(unit.body)
     for (tree <- transformed) {
-      transformTree(global)(tree)
-    }
-  }
-
-  def transformTree(global: Global)(tree: global.Tree): Unit = {
-    import global._
-
-    tree match {
-        //example Types
-      case Ident(typeName) => println(s"basic type $typeName")
-        //example Aliasing new types
-      case TypeDef(_, from, _, to) => println(s"defined $from as $to")
-        //example Creating objects
-      case Literal(Constant(c)) => println(s"constant $c")
-        //example Assignments to val
-      case ValDef(_, name, _, rhs) => println(s"assigned $rhs to $name")
-        //example Function calls
-      case Apply(fun, args) => println(s"applying $fun to $args")
-        //example Defining functions
-      case DefDef(_, name, tparams, vparamss, _, rhs) => println(s"defining $name($tparams) as $rhs")
-      case PackageDef(pid, _) => println("found PackageDef: " + pid.name)
-      case ClassDef(_, name, _, _) => println("found ClassDef: " + name)
-      case ModuleDef(_, name, _) => println("found ModuleDef: " + name)
-
-
-      //case TypeDef(_, name, _, _) => println("found TypeDef: " + name)
-      case Literal(value) => println("found Literal: " + value)
-      case _ => //println("Can't compile: " + tree)
+      transformTree(global,tree)
     }
   }
 }
