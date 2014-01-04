c77323d62f61f4e7acc69ff7a3c44395a3765674
Vivek Srikumar <me@svivek.com>
2014-01-04 15:41:14 +0000

    Added a "canonical" WolfeTransformer

--- src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfeTransformer.scala
@@ -2,20 +2,37 @@ package scalapplcodefest.compiler
 
 import scala.tools.nsc.Global
 
-import scala.reflect.internal._
 
 /**
  * @author sameer
  */
 trait WolfeTransformer {
-  def transformTree[T <: Global#Tree](global: Global, env:WolfeCompilerPlugin2#Environment, tree:T): T
+  def transformTree[T <: Global#Tree](global: Global, env: WolfeCompilerPlugin2#Environment, tree: T): T
+}
+
+object WolfeTransformers extends WolfeTransformer {
+  private val ops = scala.mutable.collection.LinkedList[WolfeTransformer]()
+
+  /**
+   * Register a operator. This is package private to ensure that bad things don't happen inadvertently
+   *
+   * @param operator
+   */
+  private[compiler] def register(operator: WolfeTransformer): Unit = { ops += operator }
+
+  def transformTree[T <: Global#Tree](global: Global, env: WolfeCompilerPlugin2#Environment, tree: T): T = {
+    ops.foldLeft(tree) {
+      (t: global.Tree, op: WolfeTransformer) => op.transformTree(global, env, tree)
+    }.asInstanceOf[T]
+  }
+
 }
 
 /**
  * Prints the abstract syntax tree
  */
 class DummyTransformer extends WolfeTransformer {
-  override def transformTree[T <: Global#Tree](g2: Global, env:WolfeCompilerPlugin2#Environment, tree: T) = {
+  override def transformTree[T <: Global#Tree](g2: Global, env: WolfeCompilerPlugin2#Environment, tree: T) = {
     println(tree)
     tree.asInstanceOf[T]
   }
