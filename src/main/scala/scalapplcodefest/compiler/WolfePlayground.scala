43e78830cf9f6cf84747d6c4fdc06be20c6a41d2
Vivek Srikumar <me@svivek.com>
2014-01-04 01:36:03 +0000

    A working example that transforms code before writing the class files to disk

--- src/main/scala/scalapplcodefest/compiler/WolfePlayground.scala
+++ src/main/scala/scalapplcodefest/compiler/WolfePlayground.scala
@@ -1,5 +1,8 @@
 package scalapplcodefest.compiler
 
+import scala.tools.nsc.{ObjectRunner, Global}
+import scala.reflect.io.{Path, AbstractFile, VirtualDirectory}
+
 
 /**
  * User: rockt
@@ -9,15 +12,38 @@ package scalapplcodefest.compiler
 
 object WolfePlayground extends App {
   val code = """
-               |object A {
+               |object A extends App {
                |  val x = 1
+               |
+               |  println(x)
                | }
              """.stripMargin
 
-  StringCompiler.compileCode(code)
 
-  // val tree = unit.body.asInstanceOf[global.Tree]
+  val outputDir = AbstractFile.getDirectory(Path("tmp"))
 
-  // global.treeBrowser.browse(tree)
+  val compiler = new StringCompiler(Some(new PlaygroundTransformer), Nil, outputDir, runsAfter = List("typer"))
 
+
+  compiler.compileCode(code)
+
+  ObjectRunner.run(List(compiler.outputDir.toURL), "A", "tmp" :: compiler.additionalClassPath ::: compiler.libPath ::: compiler.compilerPath)
+
+  class PlaygroundTransformer extends WolfeTransformer {
+    def transformTree[T <: Global#Tree](global: Global, env: WolfeCompilerPlugin2#Environment, tree: T): T = {
+      import global._
+      tree match {
+        case v@ValDef(mods, name, tpt, r) => {
+          println("Before: " + v)
+          val newRHS = treeCopy.Literal(r, Constant(41))
+          val newName = newTermName("newX")
+
+          val newVal = treeCopy.ValDef(v, mods, newName, tpt, newRHS)
+          println("After: " + newVal)
+          newVal.asInstanceOf[T]
+        }
+        case t => t
+      }
+    }
+  }
 }
