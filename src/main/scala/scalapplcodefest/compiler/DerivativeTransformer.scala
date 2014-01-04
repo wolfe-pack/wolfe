4e8ea4d7d19660d5234955e2b7b6b32bf5dc134d
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2014-01-04 11:42:29 +0000

    played around with Deriva, no symolic representation of derivaties

--- src/main/scala/scalapplcodefest/compiler/DerivativeTransformer.scala
+++ src/main/scala/scalapplcodefest/compiler/DerivativeTransformer.scala
@@ -1,7 +1,6 @@
 package scalapplcodefest.compiler
 
 import scala.tools.nsc.Global
-import org.apache.commons.math3.analysis.differentiation.DerivativeStructure
 
 /**
  * User: rockt
@@ -10,30 +9,29 @@ import org.apache.commons.math3.analysis.differentiation.DerivativeStructure
  */
 
 /**
- * Searches for @Differentiable annotations and transforms generates an AST for gradient calculation
+ * Searches for @Differentiable annotation and generates an AST for gradient calculation
  */
 class DerivativeTransformer extends WolfeTransformer {
   def transformTree[T <: Global#Tree](global: Global, env: WolfeCompilerPlugin2#Environment, tree: T) = tree match {
-    case _ => tree
+    case _ => tree //TODO
   }
 }
 
 object DerivativeTransformerPlayground extends App {
-  val params = 2
-  val order = 2
-  val xRealValue = 2.5
-  val yRealValue = -1.3
+  //http://developers.opengamma.com/blog/2013/10/28/deriva-automating-algorithmic-differentiation
 
-  val x = new DerivativeStructure(params, order, 0, xRealValue)
-  val y = new DerivativeStructure(params, order, 1, yRealValue)
-  val f = DerivativeStructure.hypot(x, y)
-  val g = f.log()
+  import com.lambder.deriva.Deriva._
+  import com.lambder.deriva.Expression
 
-  println("g        = " + g.getValue)
-  println("dg/dx    = " + g.getPartialDerivative(1, 0))
-  println("dg/dy    = " + g.getPartialDerivative(0, 1))
-  println("d2g/dx2  = " + g.getPartialDerivative(2, 0))
-  println("d2g/dxdy = " + g.getPartialDerivative(1, 1))
-  println("d2g/dy2  = " + g.getPartialDerivative(0, 2))
-  //have a look at: http://commons.apache.org/sandbox/commons-nabla/
+  val sigmoid = div(1.0, add(1.0, exp(mul(-1.0, "z"))))
+  val func = sigmoid.function("z")
+
+  println(s"sigmoid:        $sigmoid")
+  println(s"sigmoid at 0.5: ${func.execute(0.5)}")
+
+  val result = d[Expression](sigmoid, "z")
+  println(s"sigmoid dz:     $result")
+
+  println(result.describe())
+
 }
