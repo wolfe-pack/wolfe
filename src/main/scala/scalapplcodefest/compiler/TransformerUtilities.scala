4e593ce2fc66bc30b5d09468e9d906a5849c05e3
Vivek Srikumar <me@svivek.com>
2014-01-05 10:29:12 +0000

    Started an object that collects utility functions for AST transformations

---
+++ src/main/scala/scalapplcodefest/compiler/TransformerUtilities.scala
@@ -0,0 +1,26 @@
+package scalapplcodefest.compiler
+
+import scala.tools.nsc.Global
+
+/**
+ *
+ *
+ * @author svivek
+ */
+object TransformerUtilities {
+  def getClassByName(global: Global, fullName: String): global.type#Symbol =
+    global.rootMirror.getClassIfDefined(fullName)
+
+  def getClassTypeByName(global: Global, fullName: String): Global#Type =
+    getClassByName(global, fullName).tpe
+
+
+  def getMemberByName(global: Global, fullClassName: String, memberName: String): global.type#Symbol = {
+    val clazz = getClassByName(global, fullClassName)
+    global.definitions.getMember(clazz, methodName)
+  }
+
+  def getMemberTypeByName(global: Global, fullClassName: String, memberName: String): Global#Type =
+    getMemberByName(global, fullClassName, memberName).tpe
+
+}
