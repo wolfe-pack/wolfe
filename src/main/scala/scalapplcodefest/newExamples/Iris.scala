e72410e5ef7aa53412005092de64bcf31817ef05
riedelcastro <sebastian.riedel@gmail.com>
2014-01-22 19:01:43 +0000

    generated classifier code now runs

--- src/main/scala/scalapplcodefest/newExamples/Iris.scala
+++ src/main/scala/scalapplcodefest/newExamples/Iris.scala
@@ -5,34 +5,34 @@ import scalapplcodefest.{Wolfe, Util}
 import scala.io.Source
 
 /**
- * @author Sebastian Riedel
- */
+* @author Sebastian Riedel
+*/
 @Compile class Iris extends (() => Unit) {
 
   import Wolfe._
   import Iris._
 
-  def apply() = {
+  def apply() {
     val dataset = Iris.loadIris()
     val (train, test) = dataset.splitAt(dataset.size / 2)
 
-    val sampleSpace = all(Data)(c(all(Observed)(c(doubles, doubles, doubles, doubles)), classes))
+    def sampleSpace = all(Data)(c(all(Observed)(c(doubles, doubles, doubles, doubles)), classes))
 
-//    def features(data: Data) = {
-//      import data._
-//      import data.x._
-//      oneHot('sl -> y, sepalLength) + oneHot('sw -> y, sepalWidth) + oneHot('pl -> y, petalLength) + oneHot('pw -> y, petalWidth)
-//    }
+    //    def features(data: Data) = {
+    //      import data._
+    //      import data.x._
+    //      oneHot('sl -> y, sepalLength) + oneHot('sw -> y, sepalWidth) + oneHot('pl -> y, petalLength) + oneHot('pw -> y, petalWidth)
+    //    }
 
     def features(data: Data) = oneHot('sl -> data.y, data.x.sepalLength)
 
     def model(weights: Vector)(data: Data) = features(data) dot weights
 
-    def loss(weights:Vector)(data: Data) = max2(sampleSpace)(_.x == data.x)(model(weights)) - model(weights)(data)
+    def loss(weights: Vector)(data: Data) = max2(sampleSpace)(_.x == data.x)(model(weights)) - model(weights)(data)
 
     val learned = argmin2(vectors)(_ => true)(w => sum2(train)(_ => true)({data => loss(w)(data)}))
 
-    def predict(data:Data) = argmax2(sampleSpace)(_.x == data.x)(model(learned))
+    def predict(data: Data) = argmax2(sampleSpace)(_.x == data.x)(model(learned))
 
     println(learned)
 
@@ -41,8 +41,9 @@ import scala.io.Source
     println(predicted.head)
 
   }
-}
 
+
+}
 @Analyze object Iris {
 
   type IrisClass = String
@@ -54,10 +55,10 @@ import scala.io.Source
 
   def loadIris() = {
     val stream = Util.getStreamFromClassPathOrFile("scalapplcodefest/datasets/iris/iris.data")
-    val data = (for (line <- Source.fromInputStream(stream).getLines() if line.trim != "") yield {
+    val data = for (line <- Source.fromInputStream(stream).getLines().toBuffer if line.trim != "") yield {
       val Array(sl, sw, pl, pw, ic) = line.split(",")
       Data(Observed(sl.toDouble, sw.toDouble, pl.toDouble, pw.toDouble), ic)
-    }).toArray
+    }
     stream.close()
     data
   }
@@ -65,20 +66,10 @@ import scala.io.Source
   def main(args: Array[String]) {
     GenerateSources.generate(
       sourcePath = "src/main/scala/scalapplcodefest/newExamples/Iris.scala",
-      replacers = List(env => new ArgminByFactorieTrainerReplacer(env) with SimpleDifferentiator))
+      replacers = List(
+        env => new ArgminByFactorieTrainerReplacer(env) with SimpleDifferentiator))
 
   }
 
 }
-
-object RunOptimizedIris {
-  def main(args: Array[String]) {
-    val optimizedClass = WolfeOptimizer.optimizeClass[() => Unit](
-      classOf[Iris],
-      List(env => new ArgminByFactorieTrainerReplacer(env) with SimpleDifferentiator))
-    val iris = optimizedClass.newInstance()
-    println(iris)
-    iris()
-
-  }
-}
+//
