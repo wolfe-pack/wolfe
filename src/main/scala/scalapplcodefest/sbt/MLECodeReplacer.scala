c231d7c199b12849c0c9eab6608d22f6caa76afb
riedelcastro <sebastian.riedel@gmail.com>
2014-01-22 00:43:15 +0000

    now generated gradient code optimizes the inner argmax by replacing predicate with smaller domain

--- src/main/scala/scalapplcodefest/sbt/MLECodeReplacer.scala
+++ src/main/scala/scalapplcodefest/sbt/MLECodeReplacer.scala
@@ -11,14 +11,14 @@ class MLECodeReplacer(val env: GeneratorEnvironment) extends CodeStringReplacer 
     val reduced = env.betaReduce(replaced)
 
     reduced match {
-      case ApplyArgmin2(_,_, vectors, _, LogLikelihood2(data, domain, f, w), _)
+      case ApplyArgmin2(_, _, vectors, _, LogLikelihood2(data, domain, f, w), _)
         if f.symbol.hasAnnotation(MarkerOneHot) =>
 
         val featName = f.symbol.name.toString
         val sum = s"""sum2 ($data) (_ => true) { y_i => $featName(y_i) } mapValues(w => math.log(w / $data.size)) """
         modification.replace(tree.pos.start, tree.pos.end, sum)
         true
-      case ApplyArgmin2(_,_,_,_,_,_) =>
+      case ApplyArgmin2(_, _, _, _, _, _) =>
         println(replaced)
         println(reduced)
         false
@@ -42,28 +42,40 @@ class ConditionReplacer(val env: GeneratorEnvironment) extends CodeStringReplace
 
   import env.global._
 
+  def newDomainAndPredicate(dom: Tree, pred: Tree): Option[(String, String)] = {
+    val data = env.inlineVals(dom)
+    data match {
+      case CaseClassDomain(constructor, dataFields, sets) =>
+        pred match {
+          case Function(List(dataArgDef), FieldEquality(dataArgUse, fieldName, conditionValue)) =>
+            val indexOfField = dataFields.indexWhere(_.name == fieldName)
+            val crossProductArgs = for ((set, index) <- sets.zipWithIndex) yield
+              if (index == indexOfField) s"Set($conditionValue)" else set.toString()
+            val newDomain = s"""all(unwrap2($constructor))(c(${crossProductArgs.mkString(",")}))"""
+            val newPredicate = """_ => true"""
+            //find out argument index of fieldName
+            Some(newDomain,newPredicate)
+          case _ => None
+        }
+      case _ => None
+    }
+  }
 
-  def replace(tree: Tree, modification: ModifiedSourceText) = env.betaReduce(tree) match {
-    case ApplyArgmax2(_,_, dom, pred, obj, num) =>
-      val data = env.inlineVals(dom)
-      data match {
-        case CaseClassDomain(constructor, dataFields, sets) =>
-          pred match {
-            case Function(List(dataArgDef), FieldEquality(dataArgUse, fieldName, conditionValue)) =>
-              val indexOfField = dataFields.indexWhere(_.name == fieldName)
-              val crossProductArgs = for ((set, index) <- sets.zipWithIndex) yield
-                if (index == indexOfField) s"Set($conditionValue)" else set.toString()
-              val replacement = s"""argmax2 (all(unwrap2($constructor))(c(${crossProductArgs.mkString(",")}))) (_ => true)($obj)"""
-              //find out argument index of fieldName
-              modification.replace(tree.pos.start, tree.pos.end, replacement)
-              true
-            case _ => false
-          }
-        case _ => false
-      }
-      false
-    case _ => false
 
+  def replacementText(tree: Tree): Option[String] = env.betaReduce(tree) match {
+    case ApplyArgmax2(_, _, dom, pred, obj, num) => newDomainAndPredicate(dom,pred) match {
+      case Some((newDomain,newPredicate)) => Some(s"""argmax2 ($newDomain)($newPredicate)($obj)($num)""")
+      case _ => None
+    }
+    case _ => None
+  }
+
+
+  def replace(tree: Tree, modification: ModifiedSourceText) = {
+    replacementText(tree) match {
+      case Some(text) => modification.replace(tree.pos.start, tree.pos.end, text); true
+      case None => false
+    }
   }
 
 }
