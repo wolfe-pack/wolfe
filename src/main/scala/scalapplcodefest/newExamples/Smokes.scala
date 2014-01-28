f0da5957f7458c9d1e1367f2bbf33ec5fe68cbcf
riedelcastro <sebastian.riedel@gmail.com>
2014-01-28 00:21:27 +0000

    started to implement first order MPGraph generator

---
+++ src/main/scala/scalapplcodefest/newExamples/Smokes.scala
@@ -0,0 +1,34 @@
+package scalapplcodefest.newExamples
+
+import scalapplcodefest.sbt.Compile
+import scalapplcodefest.Wolfe
+
+/**
+ * @author Sebastian Riedel
+ */
+@Compile
+class Smokes extends (() => Unit) {
+
+  import Wolfe._
+
+  def apply() = {
+    case class Person(first: String, last: String)
+    case class Data(smokes: Pred[Person], cancer: Pred[Person], outbreak: Boolean)
+
+    val anna = Person("Anna", "F.")
+    val bob = Person("Bob", "B.")
+
+    implicit val allPersons = Set(anna, bob)
+    implicit val allData = all(Data)
+
+    def model(data: Data) = {
+      import data._
+      -1.3 * I(outbreak) + sum(allPersons)(_ => true) {p => I(smokes(p) --> cancer(p))}
+    }
+
+    val mpe = argmax(allData)(d => d.smokes(anna))(model)
+
+    println(mpe)
+
+  }
+}
