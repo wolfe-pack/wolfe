167bbe55be2bcfbde83be27921ca132cc323c98a
riedelcastro <sebastian.riedel@gmail.com>
2014-02-10 21:30:06 +0000

    beginning to evaluate chunking

--- src/test/scala/scalapplcodefest/macros/TestChunking.scala
+++ src/test/scala/scalapplcodefest/macros/TestChunking.scala
@@ -4,7 +4,7 @@ import scalapplcodefest.Wolfe
 import Wolfe._
 import scalapplcodefest.Wolfe.Objective.Perceptron
 import scala.io.Source
-import scalapplcodefest.util.Util
+import scalapplcodefest.util.{Evaluator, Util}
 import cc.factorie.optimize.{Perceptron, OnlineTrainer}
 
 /**
@@ -24,35 +24,42 @@ object TestChunking {
     import Util._
     import Source._
 
-    println("Chunking ..")
+    println("Chunking ...")
 
     //get CoNLL data
     val stream = getStreamFromClassPathOrFile("scalapplcodefest/datasets/conll2000/train.txt")
     val train = loadCoNLL(fromInputStream(stream).getLines().take(100), {
       case Array(word, tag, chunk) => Token(word, tag, chunk)
-    }).map(Sentence)
+    }).map(Sentence).take(1)
 
     def S(s: Sentence) = all2(Sentence)(seqs(s.tokens.map(t => all2(Token)(c(Seq(t.word), Seq(t.tag), chunks)))))
 
     //todo: the VectorNumeric is currently needed in the macro, remove this.
     def features(s: Sentence) =
-      sum(0 until s.tokens.size)(_ => true)(i => oneHot('bias -> s.tokens(i).chunk))(VectorNumeric) +
+//      sum(0 until s.tokens.size)(_ => true)(i => oneHot('bias -> s.tokens(i).chunk))(VectorNumeric) +
       sum(0 until s.tokens.size)(_ => true)(i => oneHot('obs -> s.tokens(i).chunk -> s.tokens(i).word))(VectorNumeric) +
       sum(0 until s.tokens.size - 1)(_ => true)(i => oneHot('trans -> s.tokens(i).chunk -> s.tokens(i + 1).chunk))(VectorNumeric)
 
     def model(w: Vector)(s: Sentence) = features(s) dot w
 
-    //    def predict(w: Vector)(s: Sentence) = argmax(S(s))(_ => true) {model(w)}
-
-    @MinByDescent(new OnlineTrainer(_, new Perceptron, 4))
-    def loss(weights: Vector) = sum(train.take(1))(_ => true)(s => max(S(s))(_ => true)(model(weights)) - model(weights)(s))
+    @MinByDescent(new OnlineTrainer(_, new Perceptron, 10))
+    def loss(weights: Vector) = sum(train)(_ => true)(s => max(S(s))(_ => true)(model(weights)) - model(weights)(s))
 
     val learned = argmin(vectors)(_ => true)(loss)
 
     def predict(s:Sentence) = argmax(S(s))(_ => true)(model(learned))
 
+    val predicted = train.map(predict)
+
+    println(predicted.mkString("\n"))
+
+    println(Evaluator.evaluate(train.flatMap(_.tokens),predicted.flatMap(_.tokens))(_.chunk))
+
+
+
+
     //use a generic def wildcard[T](implicit provider: WildcardProvider[T]) = provider.wildcard
-    def observed(s:Sentence) = s.copy(tokens = s.tokens.map(_.copy(chunk = "")))
+    //def observed(s:Sentence) = s.copy(tokens = s.tokens.map(_.copy(chunk = "")))
 
   }
 }
