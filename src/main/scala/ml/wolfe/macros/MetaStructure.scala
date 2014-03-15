e31eac65537041cd77ceb6d64a1df5fb7317eab1
riedelcastro <sebastian.riedel@gmail.com>
2014-03-15 17:25:44 +0000

    atomic meta structure now works and can be tested via a sub-macro

--- src/main/scala/ml/wolfe/macros/MetaStructure.scala
+++ src/main/scala/ml/wolfe/macros/MetaStructure.scala
@@ -97,6 +97,60 @@ trait MetaAtomicStructure extends MetaStructure {
 
   def matcher(parent: Tree => Option[Tree], result: Tree => Option[Tree]): Tree => Option[Tree] = result
 
+}
+
+trait MetaCaseClassStructure extends MetaStructure {
+
+  self =>
+
+  import universe._
+
+  def tpe: Type
+  def fields: List[ValDef]
+  def types: List[MetaStructure {type U = self.universe.type}]
+  def repository: CodeRepository
+
+  val fieldsAndTypes = fields zip types
+  def subClassDefs(graphName: TermName): List[Tree] = types.map(_.classDef(graphName))
+  val argTypeName     = tpe.typeSymbol.name.toTypeName
+  val className       = newTypeName(repository.fresh(tpe.typeSymbol.name.encoded + "Structure"))
+  val structureFields = for ((ValDef(mods, name, _, _), t) <- fields zip types) yield
+    q"val $name = new ${t.className}"
+  val fieldIds        = fields.map(f => Ident(f.name))
+  val fieldValues     = fieldIds.map(i => q"$i.value()")
+  val observeFields   = fields.map(f => q"${f.name}.observe(value.${f.name})")
+  def classDef(graphName: TermName) = q"""
+      final class $className extends Structure[$argTypeName] {
+        ..${subClassDefs(graphName)}
+        ..$structureFields
+        private var iterator:Iterator[Unit] = _
+        def fields:Iterator[Structure[Any]] = Iterator(..$fieldIds)
+        def value():$argTypeName = new $argTypeName(..$fieldValues)
+        def nodes():Iterator[Node] = fields.flatMap(_.nodes())
+        def resetSetting() { iterator = Structure.settingsIterator(List(..$fieldIds).reverse)()}
+        def hasNextSetting = iterator.hasNext
+        def nextSetting = iterator.next
+        def setToArgmax() {fields.foreach(_.setToArgmax())}
+        def observe(value:$argTypeName) { ..$observeFields }
+      }
+    """
+  def children = types
+  def argType = tpe
+  def domainDefs = Nil
+
+  def matcher(parent: Tree => Option[Tree], result: Tree => Option[Tree]): Tree => Option[Tree] = {
+    def matchField(field: ValDef)(tree: Tree): Option[Tree] = tree match {
+      case q"$data.$f" if field.name == f => for (s <- parent(data)) yield q"$s.$f"
+      case _ => None
+    }
+    val fieldMatchers = fieldsAndTypes.map({case (f, t) => t.matcher(matchField(f), matchField(f))})
+    def firstMatch(matchers: List[Tree => Option[Tree]]): Tree => Option[Tree] = matchers match {
+      case Nil => result
+      case head :: tail => (t: Tree) => head(t).orElse(firstMatch(tail)(t))
+    }
+    firstMatch(fieldMatchers)
+  }
+
 
 }
 
@@ -113,13 +167,25 @@ object MetaStructure {
    */
   def apply(repo: CodeRepository)(sampleSpace: repo.universe.Tree): MetaStructure {type U = repo.universe.type} = {
     //todo: assert that domain is an iterable
+    import repo.universe._
     sampleSpace match {
-      case _ => new MetaAtomicStructure {
-        type U = repo.universe.type
-        val universe: repo.universe.type = repo.universe
-        def repository = repo
-        def domain = sampleSpace
-      }
+      case q"$all[..${_}]($unwrap[..${_}]($constructor))($cross(..$sets))"
+        if all.symbol.name.encoded.startsWith("all") && unwrap.symbol.name.encoded.startsWith("unwrap") =>
+        new MetaCaseClassStructure {
+          type U = repo.universe.type
+          val universe: repo.universe.type = repo.universe
+          def tpe = ???
+          val types = sets.map(apply(repo)(_))
+          def fields = ???
+          def repository = repo
+        }
+      case _ =>
+        new MetaAtomicStructure {
+          type U = repo.universe.type
+          val universe: repo.universe.type = repo.universe
+          def repository = repo
+          def domain = sampleSpace
+        }
     }
   }
 
