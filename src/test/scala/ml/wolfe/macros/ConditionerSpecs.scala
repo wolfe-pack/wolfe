81fc8b6908060b9de25c79150c7c84adca098394
riedelcastro <sebastian.riedel@gmail.com>
2014-03-28 07:42:10 +0000

    Now references to vals are not getting inlined (but also not analyzed---can this be changed?)

--- src/test/scala/ml/wolfe/macros/ConditionerSpecs.scala
+++ src/test/scala/ml/wolfe/macros/ConditionerSpecs.scala
@@ -11,31 +11,31 @@ class ConditionerSpecs extends StructureIsomorphisms {
 
   "A conditioner" should {
     "condition an atomic sample space" in {
-      val space = Seq(1, 2, 3, 4)
+      def space = Seq(1, 2, 3, 4)
       val structure = Conditioner.conditioned(space)(x => x == 1)
       structure mustBeIsomorphicTo (space filter (_ == 1))
     }
     "condition an atomic boolean sample space" in {
-      val space = Seq(true, false)
+      def space = Seq(true, false)
       val structure = Conditioner.conditioned(space)(x => x)
       structure mustBeIsomorphicTo (space filter (x => x))
     }
     "condition a complex sample space " in {
       implicit val persons = Seq("Sameer", "Vivek")
       case class Data(smokes: Pred[String])
-      val space = Wolfe.all(Data)
+      def space = Wolfe.all(Data)
       val structure = Conditioner.conditioned(space)(x => x.smokes("Sameer"))
       structure mustBeIsomorphicTo (space filter (_.smokes("Sameer")))
     }
     "condition a complex sample space with a conjunction" in {
       implicit val ints = Range(0, 4)
-      val space = Wolfe.Pred[Int]
+      def space = Wolfe.Pred[Int]
       val structure = Conditioner.conditioned(space)(x => x(0) && x(2) && x(3))
       structure mustBeIsomorphicTo (space filter (x => x(0) && x(2) && x(3)))
     }
     "condition by specifying hidden variables in flat case class" in {
       case class Data(x: Boolean, y: Boolean, z: Boolean)
-      val space = Wolfe.all(Data)
+      def space = Wolfe.all(Data)
       def observed(d: Data) = d.copy(y = hide[Boolean])
       val instance = Data(true, true, false)
       val expected = space filter (observed(_) == observed(instance))
@@ -45,8 +45,8 @@ class ConditionerSpecs extends StructureIsomorphisms {
     "condition by specifying hidden variables in nested case class" in {
       case class Nested(a: Boolean, b: Boolean)
       case class Data(x: Nested, y: Boolean, z: Boolean)
-      implicit val nested = Wolfe.all(Nested)
-      val space = Wolfe.all(Data)
+      implicit def nested = Wolfe.all(Nested)
+      def space = Wolfe.all(Data)
       val instance = Data(Nested(false, false), true, false)
       def observed(d: Data) = d.copy(x = d.x.copy(a = hide[Boolean]))
       val expected = space filter (observed(_) == observed(instance))
@@ -55,8 +55,8 @@ class ConditionerSpecs extends StructureIsomorphisms {
     }
     "condition by specifying hidden variables in case classes nested in a sequence " in {
       case class Element(a: Boolean, b: Boolean)
-      val elements = Wolfe.all(Element)
-      val space = seqs(elements, 4)
+      def elements = Wolfe.all(Element)
+      def space = seqs(elements, 4)
       val instance = Seq.fill(3)(Element(true, false))
       def observed(s: Seq[Element]) = s.map(_.copy(b = hide[Boolean]))
       val expected = space filter (observed(_) == observed(instance))
@@ -66,8 +66,8 @@ class ConditionerSpecs extends StructureIsomorphisms {
     "condition by specifying hidden variables in a sequence nested in a case class " in {
       case class Element(a: Boolean, b: Boolean)
       case class Container(elements: Seq[Element])
-      val elements = Wolfe.all(Element)
-      val space = Wolfe.all(Container)(seqs(elements, 4))
+      def elements = Wolfe.all(Element)
+      def space = Wolfe.all(Container)(seqs(elements, 4))
       val instance = Container(Seq.fill(3)(Element(true, false)))
       def observed(c: Container) = c.copy(elements = c.elements.map(_.copy(b = hide[Boolean])))
       val expected = space filter (observed(_) == observed(instance))
