2281ace7a2d08230e0c2e56b0efe187b6e93e0cf
riedelcastro <sebastian.riedel@gmail.com>
2014-03-30 12:32:38 +0100

    clean up

--- src/main/scala/ml/wolfe/Operators.scala
+++ src/main/scala/ml/wolfe/Operators.scala
@@ -9,10 +9,18 @@ trait Operators {
 
   import Wolfe._
 
-  def argmax[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) = overWhereOf.dom.filter(overWhereOf.filter).maxBy(overWhereOf.obj)
-  def argmin[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) = overWhereOf.dom.filter(overWhereOf.filter).minBy(overWhereOf.obj)
-  def max[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) = overWhereOf.dom.filter(overWhereOf.filter).map(overWhereOf.obj).max
-  def sum[T, N: Numeric](overWhereOf: OverWhereOf[T, N]) = overWhereOf.dom.filter(overWhereOf.filter).map(overWhereOf.obj).sum
+  def argmax[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) =
+    overWhereOf.dom.filter(overWhereOf.filter).maxBy(overWhereOf.obj)
+
+  def argmin[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) =
+    overWhereOf.dom.filter(overWhereOf.filter).minBy(overWhereOf.obj)
+
+  def max[T, N: Ordering](overWhereOf: OverWhereOf[T, N]) =
+    overWhereOf.dom.filter(overWhereOf.filter).map(overWhereOf.obj).max
+
+  def sum[T, N: Numeric](overWhereOf: OverWhereOf[T, N]) =
+    overWhereOf.dom.filter(overWhereOf.filter).map(overWhereOf.obj).sum
+
 
 
 }
