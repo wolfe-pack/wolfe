f4419f8c8448ec4c0210e29a95571f4fd9ada2d2
riedelcastro <sebastian.riedel@gmail.com>
2014-03-30 10:51:27 +0100

    minor fix of spec description

--- src/test/scala/ml/wolfe/macros/OptimizeByLearningSpecs.scala
+++ src/test/scala/ml/wolfe/macros/OptimizeByLearningSpecs.scala
@@ -21,15 +21,14 @@ class OptimizeByLearningSpecs extends WolfeSpec {
       w should be(vector(0 -> -0.2, 1 -> -0.2, 2 -> -0.2, 3 -> 0.8, 4 -> -0.2))
       //this solution arises from the fact that the MP solution at ties distributes scores across features.
     }
-    "return the argmin of the perceptron loss  MAP log-likelihood)" in {
+    "return the argmin of the perceptron loss (MAP log-likelihood)" in {
       import Wolfe._
       implicit val space = Range(0, 5)
       def features(i: Int) = oneHot(i)
       def model(w: Vector)(i: Int) = w dot features(i)
-      def mapLL(i: Int)(w: Vector) = max { over[Int] of model(w) } - model(w)(i)
-      val w = argmin { over[Vector] of mapLL(3) }
+      def perceptronLoss(i: Int)(w: Vector) = max { over[Int] of model(w) } - model(w)(i)
+      val w = argmin { over[Vector] of perceptronLoss(3) }
       w should be(vector(0 -> -0.2, 1 -> -0.2, 2 -> -0.2, 3 -> 0.8, 4 -> -0.2))
-      //this solution arises from the fact that the MP solution at ties distributes scores across features.
     }
 
     "react to different learning annotation on the objective " in {
