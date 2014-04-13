8d33c8da454f134d81f6ab6a60ae41208289fa0d
riedelcastro <sebastian.riedel@gmail.com>
2014-04-13 23:05:34 +0100

    alternative LDA formulations

--- wolfe-examples/src/main/scala/ml/wolfe/examples/LDAExample.scala
+++ wolfe-examples/src/main/scala/ml/wolfe/examples/LDAExample.scala
@@ -6,28 +6,66 @@ import ml.wolfe.Wolfe
  * @author Sebastian Riedel
  */
 object LDAExample {
+
   import Wolfe._
-  case class Token(topic:Int,word:String)
-  case class Doc(theta:Map[Int,Double],tokens:Seq[Token])
-  case class Data(phi:Seq[Map[String,Double]],docs:Seq[Doc])
 
-  def prod[T](dom:Iterable[T])(p:T => Double) = dom.map(p).product
+  case class Token(topic: Int, word: String)
+  case class Doc(theta: Map[Int, Double], tokens: Seq[Token])
+  case class Data(phi: Seq[Map[String, Double]], docs: Seq[Doc])
 
-  def dir[T](dist:Map[T,Double],beta:Double):Double = 0.0
-  def cat[T](c:T,dist:Map[T,Double]):Double = 0.0
+  def prod[T](dom: Iterable[T])(p: T => Double) = dom.map(p).product
+  def gen[T](dom: Iterable[T])(p: T => Double) = prod(dom)(p)
 
 
-def lda(w:Vector)(d:Data) = {
-  import d._
-  prod(phi) { dir(_,w('beta))} *
-  prod(docs) { d =>
-    dir(d.theta, w('alpha)) *
-    prod(d.tokens) {t =>
-      cat(t.topic, d.theta) *
-      cat(t.word, phi(t.topic))
+  def dir[T](dist: Map[T, Double], beta: Double): Double = 0.0
+  def cat[T](c: T, dist: Map[T, Double]): Double = 0.0
+
+  trait Dist[T] {
+    def apply(t: T): Double = 0.0
+  }
+  implicit class RV[T](t: T) {
+    def ~(dist: Dist[T]) = dist(t)
+  }
+  case class Cat[T](dist: Map[T, Double]) extends Dist[T]
+  case class Dir[T](beta: Double) extends Dist[Map[T, Double]]
+
+  def lda(w: Vector)(d: Data) = {
+    import d._
+    prod(phi) { dir(_, w('beta)) } *
+    prod(docs) {
+      d =>
+        dir(d.theta, w('alpha)) *
+        prod(d.tokens) {
+          t =>
+            cat(t.topic, d.theta) *
+            cat(t.word, phi(t.topic))
+        }
+    }
+
+  }
+
+  def lda2(w: Vector)(d: Data) = {
+    import d._
+    gen(phi) { _ ~ Dir(w('beta)) } *
+    gen(docs)(d => d.theta ~ Dir(w('alpha)) * gen(d.tokens)(t => t.topic ~ Cat(d.theta) * t.word ~ Cat(phi(t.topic))))
+  }
+
+  implicit class DoubleWithAnd(d: Double) {
+    def and(that: Double) = d * that
+  }
+
+  def lda3(w: Vector)(d: Data) = {
+    import d._
+    gen(phi) { _ ~ Dir(w('beta)) } and
+    gen(docs) {
+      d =>
+        d.theta ~ Dir(w('alpha)) * gen(d.tokens) {
+          t =>
+            t.topic ~ Cat(d.theta) and
+            t.word ~ Cat(phi(t.topic))
+        }
     }
   }
 
-}
 
 }
