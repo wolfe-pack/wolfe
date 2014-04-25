545bb90a47d65df735a2a252b0b220aac244d716
riedelcastro <sebastian.riedel@gmail.com>
2014-04-25 22:05:18 +0100

    more use of new operator signature

--- wolfe-examples/src/main/scala/ml/wolfe/examples/GateExample.scala
+++ wolfe-examples/src/main/scala/ml/wolfe/examples/GateExample.scala
@@ -15,16 +15,16 @@ object GateExample {
 
   case class XY(in1: Boolean, in2: Boolean, out: Boolean)
 
-  def f(d:XY) = oneHot('in1, I(d.in1 && d.out)) + oneHot('in2, I(d.in2 && d.out)) + oneHot('bias, I(d.out))
+  def f(d: XY) = oneHot('in1, I(d.in1 && d.out)) + oneHot('in2, I(d.in2 && d.out)) + oneHot('bias, I(d.out))
 
   def s(w: Vector)(d: XY) = w dot f(d)
 
   def q(obs: XY)(d: XY) = d.in1 == obs.in1 && d.in2 == obs.in2
 
-  def h(w: Vector)(obs: XY) = argmax { over(all(XY)) of s(w) st q(obs) }
+  def h(w: Vector)(obs: XY) = argmax(all(XY) where q(obs)) { s(w) }
 
   @OptimizeByLearning(new OnlineTrainer(_, new Perceptron, 10))
-  def l(data: Seq[XY])(w: Vector) = sum { over(data) of (d => s(w)(h(w)(d)) - s(w)(d)) }
+  def l(data: Seq[XY])(w: Vector) = sum(data) { d => s(w)(h(w)(d)) - s(w)(d) }
 
   val andData = Seq(
     XY(true, true, true),
@@ -34,17 +34,15 @@ object GateExample {
 
   def main(args: Array[String]) {
 
-    val w = argmin { over(vectors) of l(andData) }
+    val w = argmin(vectors) { l(andData) }
 
     println(w)
 
-    val guess = h(w)(XY(true,true,true))
+    val guess = h(w)(XY(true, true, true))
 
     println(guess)
-    println(Evaluator.evaluate(andData,andData.map(h(w)))(_.out))
+    println(Evaluator.evaluate(andData, andData.map(h(w)))(_.out))
   }
-
-
 
 
 }
