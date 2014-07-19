5fd798733b789a6b31ff31ca3c1285f2cd561dd8
riedelcastro <sebastian.riedel@gmail.com>
2014-07-19 10:03:57 +0900

    more expectation specs

--- wolfe-core/src/test/scala/ml/wolfe/macros/ExpectationSpecs.scala
+++ wolfe-core/src/test/scala/ml/wolfe/macros/ExpectationSpecs.scala
@@ -1,6 +1,6 @@
 package ml.wolfe.macros
 
-import ml.wolfe.{BruteForceOperators, WolfeSpec}
+import ml.wolfe.{Wolfe, BruteForceOperators, WolfeSpec}
 
 /**
  * @author Sebastian Riedel
@@ -9,10 +9,20 @@ class ExpectationSpecs extends WolfeSpec {
 
   import ml.wolfe.Wolfe._
 
-  "return the expectations of a one-node sample space and atomic objective " in {
-    val actual = OptimizedOperators.expect(0 until 5) { _.toDouble } {oneHot(_)}
-    val expected = BruteForceOperators.expect(0 until 5) { _.toDouble } {oneHot(_)}
-    actual should equal (expected)
+  "return the expectations of a one-node sample space and atomic objective" in {
+    val actual = OptimizedOperators.expect(0 until 5) { _.toDouble } { oneHot(_) }
+    val expected = BruteForceOperators.expect(0 until 5) { _.toDouble } { oneHot(_) }
+    actual should equal(expected)
   }
+
+  "return the argmax of a two node case class sample space, one observation and atomic objective" in {
+    case class Data(x: Boolean, y: Boolean)
+    def model(d: Data) = I(!d.x || d.y)
+    def stats(d: Data) = oneHot('x -> d.x) + oneHot('y -> d.y)
+    val actual = OptimizedOperators.expect(Wolfe.all(Data) filter (_.x)) { model } { stats }
+    val expected = BruteForceOperators.expect(Wolfe.all(Data) filter (_.x)) { model } { stats }
+    actual should equal(expected)
+  }
+
 
 }
