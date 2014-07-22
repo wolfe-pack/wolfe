dcd89bb24d01e681b1da742d94790d682b5caf64
riedelcastro <sebastian.riedel@gmail.com>
2014-07-22 14:20:30 +0900

    fixed scalac options in build

--- project/Build.scala
+++ project/Build.scala
@@ -40,7 +40,7 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
     scalaVersion := buildScalaVersion,
-    scalacOptions := Seq("unchecked", "deprecation", "feature"),
+    scalacOptions := Seq("-unchecked", "-deprecation", "-feature"),
     shellPrompt := ShellPrompt.buildShellPrompt,
     fork in run := true //use a fresh JVM for sbt run
   )
