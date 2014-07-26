feef070a4e3867dc53e5bb2ee5f9fb8baab03b17
riedelcastro <sebastian.riedel@gmail.com>
2014-07-26 11:30:29 +0900

    grid example

--- wolfe-examples/src/main/scala/ml/wolfe/examples/GridExample.scala
+++ wolfe-examples/src/main/scala/ml/wolfe/examples/GridExample.scala
@@ -10,29 +10,27 @@ object GridExample {
 
   type Image = Map[(Int, Int), Boolean]
 
-//  def main(args: Array[String]) {
-//    val n = 10
-//    val l_w = 1.0
-//    val l_g = 0.1
-//    def pixels = (0 until n) x (0 until n)
-//    def images = maps(pixels, bools)
-//    def local(x: Image)(y: Image) = sum(pixels) { p => l_w * I(x(p) == y(p)) }
-//    def vertical(y: Image) = sum(0 until n - 1) { i => sum(0 until n) { j => l_g * I(y(i, j) == y(i + 1, j)) } }
-//    def horizontal(y: Image) = sum(0 until n) { i => sum(0 until n - 1) { j => l_g * I(y(i, j) == y(i, j + 1)) } }
-//    def grid(x: Image)(y: Image) = local(x)(y) + vertical(y) + horizontal(y)
-//    def stats(y: Image) = sum(pixels) { p => oneHot(p, I(y(p))) }
-//
-//    val x = (pixels map { p => p -> random.nextBoolean() }).toMap
-//    //    println("MPE")
-//    //    val mpe = argmax(images) { grid(x) }
-//        println("Expect")
-//        val mu = expect(images) { grid(x) } { stats }
-//    //
-//    //    println(x)
-//    //    //    println(mpe)
-//    //    println(mu)
-//
-//
-//  }
+  def main(args: Array[String]) {
+    val n = 10
+    val l_w = 1.0
+    val l_g = 0.1
+    def pixels = (0 until n) x (0 until n)
+    def images = maps(pixels, bools)
+    def local(x: Image)(y: Image) = sum(pixels) { p => l_w * I(x(p) == y(p)) }
+    def vertical(y: Image) = sum(0 until n - 1) { i => sum(0 until n) { j => l_g * I(y(i, j) == y(i + 1, j)) } }
+    def horizontal(y: Image) = sum(0 until n) { i => sum(0 until n - 1) { j => l_g * I(y(i, j) == y(i, j + 1)) } }
+    def grid(x: Image)(y: Image) = local(x)(y) + vertical(y) + horizontal(y)
+    def stats(y: Image) = sum(pixels) { p => oneHot(p, I(y(p))) }
+
+    val x = (pixels map { p => p -> random.nextBoolean() }).toMap
+    println("MPE")
+    val mpe = argmax(images) { grid(x) }
+    println(mpe)
+    println("Expect")
+    val mu = expect(images) { grid(x) } { stats }
+    println(mu)
+
+
+  }
 
 }
