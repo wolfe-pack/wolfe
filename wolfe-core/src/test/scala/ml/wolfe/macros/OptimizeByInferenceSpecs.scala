5a1428a799612107009c5f703c6dea66f4794c05
luke <insperatum@gmail.com>
2014-08-01 13:33:02 +0100

    Fixed maxBy annotation spec

--- wolfe-core/src/test/scala/ml/wolfe/macros/OptimizeByInferenceSpecs.scala
+++ wolfe-core/src/test/scala/ml/wolfe/macros/OptimizeByInferenceSpecs.scala
@@ -55,22 +55,14 @@ class OptimizeByInferenceSpecs extends WolfeSpec {
 
 
     "use the algorithm in the maxBy annotation" in {
-      //todo: can we find a better way to check whether an annotation was used
-      case class Data(x: Boolean, y: Boolean, z: Boolean)
-      implicit def data = Wolfe.all(Data)
-      @OptimizeByInference(BeliefPropagation(_, 10))
-      def model(d: Data) = I(d.x && d.y) + I(d.y && !d.z) + I(d.z && !d.x)
-      @OptimizeByInference(BeliefPropagation(_, 2))
-      def twoIterations(d: Data) = model(d)
-      @OptimizeByInference(BeliefPropagation(_, 1))
-      def oneIteration(d: Data) = model(d)
-      val afterOneIter = argmax(data) { oneIteration }
-      val afterTwoIter = argmax(data) { twoIterations }
-      val afterTenIter = argmax(data) { model }
-      val expected = BruteForceOperators.argmax(data) { model }
-      afterOneIter should not be expected
-      afterTwoIter should be(expected)
-      afterTenIter should be(expected)
+      var usedAnnotation = false
+      def fun(fg:FactorGraph):Unit = usedAnnotation = true
+
+      @OptimizeByInference(fun)
+      def model(b:Boolean):Unit = I(b)
+      val b = argmax(bools) {model}
+
+      usedAnnotation should be(true)
     }
 
     "find the optimal solution of a linear chain" in {
