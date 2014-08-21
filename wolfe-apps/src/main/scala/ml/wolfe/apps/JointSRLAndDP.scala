f79ca3adba6dfdb040f2111fb1309c4e2e044add
riedelcastro <sebastian.riedel@gmail.com>
2014-08-21 22:12:35 +0100

    Initial version of joint SRL and DepParsing model. Using distinct types for observed and latent variables to make sure unrolling works.

---
+++ wolfe-apps/src/main/scala/ml/wolfe/apps/JointSRLAndDP.scala
@@ -0,0 +1,87 @@
+package ml.wolfe.apps
+
+import ml.wolfe.FactorGraph.Edge
+import ml.wolfe.Wolfe
+
+/**
+ * @author Sebastian Riedel
+ */
+object JointSRLAndDP extends App {
+
+  import Wolfe._
+  import ml.wolfe.macros.OptimizedOperators._
+
+  val labels = List('A, 'B)
+
+  type Edge = (Int, Int)
+  type Labelled = (Edge, String)
+
+  case class Y(args: Pred[Edge], roles: Pred[Labelled], deps: Pred[Edge])
+  case class X(words: Seq[String], tags: Seq[String]) {
+    def n = words.size
+    def candidateArgs = for (h <- 0 until n; m <- 0 until n) yield (h, m)
+    def candidateDeps = for (h <- 0 until n; m <- 1 until n; if h != m) yield (h, m)
+    def candidateRoles = for (edge <- candidateArgs; label <- labels) yield edge -> label
+  }
+
+  def argLocal(x: X)(y: Y)(h: Int, m: Int) =
+    oneHot('ww ->(x.words(h), x.words(m), y.args(h, m)))
+
+  def depLocal(x: X)(y: Y)(h: Int, m: Int) =
+    oneHot('ww ->(x.words(h), x.words(m), y.deps(h, m)))
+
+
+  def feats(x: X)(y: Y) =
+    sum(x.candidateArgs) { case (h, m) => argLocal(x)(y)(h, m) } +
+    sum(x.candidateDeps) { case (h, m) => depLocal(x)(y)(h, m) } +
+    sum(x.candidateArgs) { case (h, m) => oneHot('nand, I(y.args(h, m) && y.deps(h, m))) }
+
+  def model(w:Vector)(x:X)(y:Y) = feats(x)(y) dot w
+
+
+  //  case class Sentence(words: Seq[String], tags: Seq[String],
+  //                      args: Pred[(Int, Int)],
+  //                      roles: Pred[(Int, Int, Symbol)],
+  //                      dp: Pred[(Int, Int)])
+  //
+  //  def argLocal(i: Int, j: Int)(s: Sentence) = {
+  //    oneHot('ww ->(s.words(i), s.words(j), s.args(i, j)))
+  //  }
+  //
+  //
+  //  def feats(s: Sentence) =
+  //    sum(s.args.keys) { case (i, j) => argLocal(i, j)(s) } +
+  //    sum(s.args.keys) { case (i, j) => oneHot('nand, I(s.args(i, j) && s.dp(i, j))) }
+  //
+  //  def constraints(s: Sentence) =
+  //    sum(s.args.keys) { case (i, j) => leq(s.args(i, j), labels map (s.roles(i, j, _))) } +
+  //    tree(s.dp)
+  //
+  //  @Potential(leqPot(_, _))
+  //  def leq(indicator: Boolean, others: Seq[Boolean]) =
+  //    I(others.count(identity) <= 1 && indicator == others.exists(identity))
+  //
+  //  @Potential(treePot(_))
+  //  def tree[T](graph: Map[(T, T), Boolean]) = Double.NegativeInfinity
+  //
+  //  def model(w: Vector)(s: Sentence) = {
+  //    (feats(s) dot w) + constraints(s)
+  //  }
+  //
+  //
+  //
+  //  def treePot(graph: Map[(Any, Any), Edge]) = new ml.wolfe.fg.Potential {}
+  //
+  //  def leqPot(indicator: Edge, others: Seq[Edge]) = new ml.wolfe.fg.Potential {
+  //    /**
+  //     * Calculate and update the marginal message from the factor of this potential to
+  //     * the given edge.
+  //     * @param edge the edge to pass the message on.
+  //     */
+  //    override def marginalF2N(edge: Edge) = {
+  //      ???
+  //    }
+  //  }
+
+
+}
