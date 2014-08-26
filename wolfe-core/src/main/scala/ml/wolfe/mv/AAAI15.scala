c07110c6738d464929820f735841c7f48a1670c1
riedelcastro <sebastian.riedel@gmail.com>
2014-08-26 21:33:28 +0100

    more manual conversions, and sketch of an algorithm in documentation.

--- wolfe-core/src/main/scala/ml/wolfe/mv/AAAI15.scala
+++ wolfe-core/src/main/scala/ml/wolfe/mv/AAAI15.scala
@@ -72,7 +72,7 @@ object AAAI15 {
 
   //Automatically converted structure
   sealed trait TermMyList[+T] extends Term[MyList[T]]
-  case class TermMyCons[T](h: Term[T], t: TermMyList[T]) extends TermMyList[T] {
+  case class TermMyCons[T](h: Term[T], t: Term[MyList[T]]) extends TermMyList[T] {
     def apply(state: State) = MyCons(h(state), t(state))
     def vars = h.vars ++ t.vars
   }
@@ -81,7 +81,7 @@ object AAAI15 {
     def vars = Nil
   }
   type TermMyMap[A, B] = TermMyList[(A, B)]
-  case class TermWorld(smokes: TermMyMap[String, Boolean], cancer: TermMyMap[String, Boolean]) extends Term[World]{
+  case class TermWorld(smokes: Term[MyMap[String, Boolean]], cancer: Term[MyMap[String, Boolean]]) extends Term[World]{
     def apply(state: State) = World(smokes(state), cancer(state))
     def vars = smokes.vars ::: cancer.vars
   }
@@ -101,7 +101,7 @@ object AAAI15 {
 
   sealed trait TermList[T] extends Term[List[T]] {
   }
-  case class TermCons[T](h: Term[T], t: TermList[T]) extends TermList[T] {
+  case class TermCons[T](h: Term[T], t: Term[List[T]]) extends TermList[T] {
     def apply(s: State) = h(s) :: t(s)
     def vars = h.vars ::: t.vars
   }
@@ -116,7 +116,7 @@ object AAAI15 {
     case Cons((k,v),t) => if (key == k) v else mapGet(key,t)
   }
    */
-  def termMapGet[A, B](key: Term[A], map: TermMyMap[A, B]): Term[B] = map match {
+  def termMapGet[A, B](key: Term[A], map: Term[MyMap[A, B]]): Term[B] = map match {
     case TermMyNil => sys.error("Key not in map")
     case TermMyCons(TermTuple2(k: Const[_], v), t) if key.isInstanceOf[Const[_]] => if (k == key) v else termMapGet(key, t)
     case _ => ???
@@ -140,7 +140,7 @@ object AAAI15 {
     def vars = arg1.vars ::: arg2.vars
   }
 
-  def termSum[T](dom: TermList[T])(obj: TermFun[T, Double]): Term[Double] = {
+  def termSum[T](dom: Term[List[T]])(obj: TermFun[T, Double]): Term[Double] = {
     dom match {
       case TermNil => Const(0.0)
       case TermCons(h, t) => Plus(obj.f(h), termSum(t)(obj))
@@ -150,6 +150,64 @@ object AAAI15 {
   /*
    def model(y:World) =
     sum(persons) { p => 2.1 * I(!mapGet(p,y.smokes) || mapGet(p,y.cancer))}
+
+   def termModel(y:TermWorld) = Generic(s =>
+    sum(persons) { p => 2.1 * I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { Generic(s => p => 2.1 * I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { TermFun(p => Generic(s => 2.1 * I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { TermFun(p => Times(Const(2.1), Generic(s => I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { TermFun(p => Times(Const(2.1), termI(Generic(s => !mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { TermFun(p => Times(Const(2.1), termI(Generic(s => !mapGet(p,y.smokes(s)) || mapGet(p,y.cancer(s)))}
+
+   def termModel(y:TermWorld) =
+    termSum(toTermList(persons)) { TermFun(p => Times(Const(2.1), termI(Generic(s => !termMapGet(p,y.smokes)(s) || termMapGet(p,y.cancer)(s))}
+
+    ---------------
+
+   def termModel(y:TermWorld)(implicit s: State): Double =
+    sum(persons) { p => 2.1 * I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+
+   def termModel(y:TermWorld): Double =
+    sum(persons) { p => 2.1 * I(!mapGet(p,y(s).smokes) || mapGet(p,y(s).cancer))}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => 2.1 * I(!mapGet(p,y.smokes(s)) || mapGet(p,y.cancer(s)))}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => 2.1 * I(!termMapGet(p,y.smokes)(s) || termMapGet(p,y.cancer)(s))}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => 2.1 * I(Generic(sq => !termMapGet(p,y.smokes)(sq) || termMapGet(p,y.cancer)(s))}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => 2.1 * I(Generic(sq => !termMapGet(p,y.smokes)(sq) || termMapGet(p,y.cancer)(sq)(s))}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => 2.1 * termI(Generic(sp => !Generic(sq => termMapGet(p,y.smokes)(sq))(sp) || termMapGet(p,y.cancer)(sp)), List())(s)}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { p => Times(Const(2.1), termI(Generic(sp => !termMapGet(p,y.smokes)(sp) || termMapGet(p,y.cancer)(sp)), List())(s)}
+
+   def termModel(y:TermWorld): TermDouble =
+    sum(persons) { TermFun(p => Times(Const(2.1), termI(Generic(sp => !termMapGet(p,y.smokes)(sp) || termMapGet(p,y.cancer)(sp)), List()))(s)}
+
+   def termModel(y:TermWorld): TermDouble = Generic(s =>
+    termSum(toTermList(persons)) { TermFun(p => Times(Const(2.1), termI(Generic(sp => !termMapGet(p,y.smokes)(sp) || termMapGet(p,y.cancer)(sp)), List()))(s)}
+
+   def f(TermWorld): TermDouble = Generic(s => term(s))
+
+   def f(TermWorld): TermDouble = term
    */
 
   def toTermList[T](list: List[T]): TermList[T] = list match {
@@ -182,7 +240,7 @@ object AAAI15 {
 
   def termToList[T](term: Term[T]): List[T] = ???
 
-  def termAllMaps[A, B](dom: Term[A], range: Term[B]): TermMyMap[A, B] = termToList(dom) match {
+  def termAllMaps[A, B](dom: Term[A], range: Term[B]): Term[MyMap[A, B]] = termToList(dom) match {
     case Nil => TermMyNil
     case h :: t => TermMyCons(TermTuple2(Const(h), range), termAllMaps(new Var(t), range))
   }
@@ -220,7 +278,7 @@ object AAAI15 {
     case _ => sys.error("Damn")
   }
 
-  def termDot[K](keys: TermList[K])(arg1: TermMyMap[K, Double], arg2: TermMyMap[K, Double]): Term[Double] = {
+  def termDot[K](keys: Term[List[K]])(arg1: Term[MyMap[K, Double]], arg2: Term[MyMap[K, Double]]): Term[Double] = {
     termSum(keys) { TermFun(k => Times(termMapGet(k, arg1), termMapGet(k, arg2)), toConst(keys).value) }
   }
 
@@ -239,3 +297,105 @@ object AAAI15 {
 
 
 }
+
+/*
+
+(SearchSpaceAST,ObjAST) => (FactorGraphAST) (AST that evaluates to a factor graph)
+
+Theorem/Lemma: Let s be an AST of type List[T], and o an AST of type T => Double. Let (t,f) = convert(s,o)
+where t is an AST of type Term[T] and f of type Seq[Term[Double]]. Let eval(x) of an
+AST be the runtime object x evaluates to in the current scope. Then
+ (i) for every element e in eval(s) there exists an assignment a(e) to the variables of eval(t), and vice versa.
+ (ii) for every element e in eval(s) we have: eval(o)(e) == eval(f) map (factor => factor(a(e))) sum
+
+Lemma: When we have an MLN in Wolfe using the style in example ..., the resulting factor graph is isomorphic to
+ the Ground Markov Network of the MLN.
+
+
+Search space:
+
+
+  1. Take search space List[T]
+
+    1. result has to evaluate to Term[T]
+
+  2. for (a1 <- dom1; a2 <- dom2) yield ADT(a1,a2)
+
+    1. Create isomorphic class TermADT for ADT constructor which takes Term[A] arguments instead of A arguments
+    2. => TermADT(convert(dom1),convert(dom2))
+
+  3. f(args..) (of return type  T => List[R])
+
+    1. add termf definition
+
+
+      1. Arguments types for different original types
+
+        1. arguments of type A
+
+          1. stay the same
+
+        2. arguments of type List[A]
+
+          1. replaced by Term[A] //or something more concrete?
+
+            1. or TermA?
+
+
+        3. each argument of structured type Function1[A,List[B]]
+
+      2. Return type
+
+        1. TermR?
+        2. Term[R]?
+
+      3. new Body = convert(body with arguments replaced by termToList(arg))
+
+
+  4. new Var(termToList(arg) => arg
+  5. All other scala expressions are converted in the trivial way
+
+    1. for example, "case pat => result" becomes "case pat => convert(body)”
+    2. if (cond) r1 else r2
+
+      1. if (cond) convert(r1) else convert(r2)
+
+
+
+funConvert method: Args => Double:
+
+
+  1. input: arguments => Double
+
+    1. for each argument of type A,
+
+      1. create TermA (if needed)
+      2. replace type A with TermA
+
+    2. replace body with newBody = Generic(s => body where each arg is replaced by arg(s))
+
+
+  2. convert(newBody) into Term[Double]
+
+
+Double AST to Term[Double] AST
+
+
+   * arg1 + arg2 => Plus(convert(arg1),convert(arg2))
+   * arg1 * arg2 => Times(convert(arg1),convert(arg2))
+   * f(arg) => {funConvert(f); termf(simplify(toStructuredConst(arg))) }
+
+      * by default Const(arg)
+
+
+         * or better toStructuredConst(arg)
+
+            * this method exists for each ADT and creates a TermADT
+
+
+
+   *
+
+
+
+ */
