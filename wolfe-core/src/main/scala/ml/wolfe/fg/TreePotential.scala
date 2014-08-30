e7a8c585e17132ce5533c0c97e3c24368bdefe31
riedelcastro <sebastian.riedel@gmail.com>
2014-08-30 13:49:55 +0100

    Factored out tree checking code and use it also in TreePotential

--- wolfe-core/src/main/scala/ml/wolfe/fg/TreePotential.scala
+++ wolfe-core/src/main/scala/ml/wolfe/fg/TreePotential.scala
@@ -21,7 +21,29 @@ class TreePotential(edges:Map[(Any,Any),Edge], multinode:Boolean) extends Potent
 
   }
   override def valueForCurrentSetting() = {
-    val setting = edges(1 -> 2).n.variable.asDiscrete.value
-    ???
+    val graph = edges mapValues(_.n.variable.asDiscrete.value == 1)
+    val tree = TreePotential.isFullyConnectedTree(graph)
+    if (tree) 0.0 else Double.NegativeInfinity
   }
+}
+
+object TreePotential {
+
+  def isFullyConnectedTree[T](graph: Map[(T, T), Boolean]) = {
+    val edges = (graph filter (_._2) map (_._1)).toList
+    val nodes = (graph.keys map (_._1)).toList.distinct
+    val children = edges groupBy (_._1) mapValues (_ map (_._2))
+
+    //def recurse(node: T, tree: Set[T], currentChain: Set[T]): Set[T] =
+    def reached(remainder: List[T], covered: Set[T] = Set.empty): Set[T] = remainder match {
+      case Nil => covered
+      case h :: t if covered(h) => reached(t, covered)
+      case h :: t =>
+        val toVisit = children.getOrElse(h, Nil)
+        reached(toVisit, covered ++ toVisit)
+    }
+    val result = reached(nodes)
+    result.size == nodes.size - 1
+  }
+
 }
--- wolfe-core/src/test/scala/ml/wolfe/fg/TreePotentialSpecs.scala
+++ wolfe-core/src/test/scala/ml/wolfe/fg/TreePotentialSpecs.scala
@@ -8,23 +8,8 @@ import ml.wolfe.{Wolfe, WolfeSpec}
  */
 class TreePotentialSpecs extends WolfeSpec {
 
-  def isFullyConnectedTree[T](graph: Map[(T, T), Boolean]) = {
-    val edges = (graph filter (_._2) map (_._1)).toList
-    val nodes = (graph.keys map (_._1)).toList.distinct
-    val children = edges groupBy (_._1) mapValues (_ map (_._2))
 
-    //def recurse(node: T, tree: Set[T], currentChain: Set[T]): Set[T] =
-    def reached(remainder: List[T], covered: Set[T] = Set.empty): Set[T] = remainder match {
-      case Nil => covered
-      case h :: t if covered(h) => reached(t, covered)
-      case h :: t =>
-        val toVisit = children.getOrElse(h, Nil)
-        reached(toVisit, covered ++ toVisit)
-    }
-    val result = reached(nodes)
-    result.size == nodes.size - 1
-  }
-
+  import TreePotential._
 
   def graph[T](pairs: (T, T)*) = (pairs map (x => x -> true)).toMap withDefaultValue false
 
