be96e7152e30a550f7e7e845ce2e7bb8c4a3fd2a
luke <insperatum@gmail.com>
2014-09-11 17:28:49 +0100

    alalalala

---
+++ wolfe-examples/src/main/scala/ml/wolfe/examples/Scratch.scala
@@ -0,0 +1,63 @@
+package ml.wolfe.examples
+
+import ml.wolfe.Wolfe._
+import ml.wolfe.macros.OptimizedOperators._
+import ml.wolfe.util.Timer
+import org.scalameter.{Gen, PerformanceTest}
+
+
+object Scratch extends PerformanceTest.Quickbenchmark {
+
+  val ranges = (3 +: (1 to 10)).map(0 to _)
+  /*
+  val sizes = Gen.range("size")(1, 10, 1)
+
+  val ranges = for {
+    size <- sizes
+  } yield 1 to size
+
+
+
+  measure method "runExperiment" in {
+    using(ranges) in */
+
+  ranges.foreach {
+      r =>
+
+        def runExperiment = {
+          case class World(smokes: Pred[Symbol], cancer: Pred[Symbol],
+                           friends: Pred[(Symbol, Symbol)])
+
+          implicit def persons = r.map(i => Symbol(i.toString)).toList
+          def worlds = all(World)
+
+          @Atomic
+          def smokingCausesCancer(w: World): Double = {
+            sum(persons) { p =>
+              1.5 * I(w.smokes(p) --> w.cancer(p))
+            }
+          }
+
+          //@Atomic
+          def smokingIsInfectious(w: World): Double = {
+            sum(persons) { p1 => sum(persons) { p2 =>
+              1.1 * I(w.friends(p1, p2) --> (w.smokes(p1) == w.smokes(p2)))
+            }}
+          }
+
+          //@Atomic
+          def mln(world: World) = {
+            smokingCausesCancer(world) + smokingIsInfectious(world)
+          }
+
+          def query(world: World) = oneHot(world.cancer(persons.head))
+          Timer.time("foo") {
+            argmax(worlds) { mln }
+          }
+          println(r.last + "\t" + Timer.reported("foo"))
+        }
+
+        runExperiment
+    }
+  /*}*/
+}
