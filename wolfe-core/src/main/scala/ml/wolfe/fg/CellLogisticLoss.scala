1914b5f96cf260e2a7c14bfc089f528ffaa54c77
rockt <tim.rocktaeschel@gmail.com>
2014-09-18 11:25:05 +0100

    Issue #6: L2Regularization

--- wolfe-core/src/main/scala/ml/wolfe/fg/CellLogisticLoss.scala
+++ wolfe-core/src/main/scala/ml/wolfe/fg/CellLogisticLoss.scala
@@ -1,15 +1,13 @@
 package ml.wolfe.fg
 
+import cc.factorie.la.SparseTensor1
 import ml.wolfe.FactorGraph._
+import ml.wolfe.FactorieVector
 
 /**
  * @author Sebastian Riedel
  */
-final class CellLogisticLoss(rowEdge: Edge, columnEdge: Edge, truth: Double = 1.0) extends Potential {
-
-  //todo: incorporate truth
-  //rockt: truth should be degree of truth in the interval  (0, 1)
-
+class CellLogisticLoss(rowEdge: Edge, columnEdge: Edge, truth: Double = 1.0, val λ: Double = 0.0) extends Potential with Regularization {
   //nodes of edges may change hence the def and not val.
   def rowVar = rowEdge.n.variable.asVector
   def columnVar = columnEdge.n.variable.asVector
@@ -22,19 +20,37 @@ final class CellLogisticLoss(rowEdge: Edge, columnEdge: Edge, truth: Double = 1.
     if (truth >= s) (1 + s - truth, 1)
     else (1 + truth - s, -1)
 
-  override def valueForCurrentSetting() = {
+  override def valueForCurrentSetting(): Double = {
     val a = rowVar.setting
     val v = columnVar.setting
     val s = sig(a dot v)
     val p = innerLossAndDirection(s)._1
-    math.log(p)
+    math.log(p) + regLoss(a) + regLoss(v)
   }
 
-  override def valueAndGradientForAllEdges() = {
+  override def valueAndGradientForAllEdges(): Double = {
     val s = sig (rowMsgs.n2f dot columnMsgs.n2f)
     val (p, dir) = innerLossAndDirection(s)
-    rowMsgs.f2n = columnMsgs.n2f * (1.0 - p) * dir
-    columnMsgs.f2n = rowMsgs.n2f * (1.0 - p) * dir
-    math.log(p)
+    rowMsgs.f2n = (columnMsgs.n2f * (1.0 - p) * dir) + regGradient(rowMsgs.n2f)
+    columnMsgs.f2n = (rowMsgs.n2f * (1.0 - p) * dir) + regGradient(columnMsgs.n2f)
+    math.log(p) + regLoss(rowMsgs.n2f) + regLoss(columnMsgs.n2f)
   }
+}
+
+trait Regularization {
+  def regLoss(vector: FactorieVector): Double = 0
+  def regGradient(vector: FactorieVector): FactorieVector = new SparseTensor1(vector.length) //all zeros
+}
+
+/**
+ * λ||x||²
+ */
+trait L2Regularization extends Regularization {
+  val λ: Double
+  override def regLoss(vector: FactorieVector): Double =
+    if (λ == 0) 0
+    else vector dot vector * λ
+  override def regGradient(vector: FactorieVector): FactorieVector =
+    if (λ == 0) new SparseTensor1(vector.length) //all zeros
+    else vector * λ
 }
