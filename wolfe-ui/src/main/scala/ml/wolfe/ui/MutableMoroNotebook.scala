bafc2da81ddaca9238083cc5e142994f20d44281
riedelcastro <sebastian.riedel@gmail.com>
2014-10-02 00:39:57 +0100

    writing moro notebooks from scala

---
+++ wolfe-ui/src/main/scala/ml/wolfe/ui/MutableMoroNotebook.scala
@@ -0,0 +1,65 @@
+package ml.wolfe.ui
+
+import java.io.{FileWriter, File}
+
+import org.json4s.NoTypeHints
+import org.json4s.native.Serialization
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * @author Sebastian Riedel
+ */
+class MutableMoroNotebook extends MutableNotebook[MutableMoroNotebook] {
+
+  val cells = new ArrayBuffer[Cell]()
+
+  object OutputFormats extends Enumeration {
+    val string, html, javascript = Value
+  }
+  case class Input(code: String, outputFormat: String = OutputFormats.html.toString, extraFields: Map[String, String] = Map.empty)
+  case class Cell(id: Int, compiler: String, input: Input)
+  case class DocumentData(name: String, cells: Seq[Cell])
+
+  def stripLeadingWhitespace(text:String) = text.split("\n").map(_.trim).mkString("\n")
+
+  def html(text: String) = {
+    cells += Cell(cells.size,"html",Input(text,OutputFormats.html.toString))
+    this
+  }
+  def h2(text: String) = {
+    cells += Cell(cells.size,"heading2",Input(text,OutputFormats.html.toString))
+    this
+  }
+
+  def trimEmptyLines(text:String) = {
+    val lines = text.split("\n").toList
+    val prefixRemoved = lines.dropWhile(_.trim == "")
+    val postfixRemoved = prefixRemoved.reverse.dropWhile(_.trim == "").reverse
+    postfixRemoved.mkString("\n")
+  }
+
+  def source(src: String) = {
+    cells += Cell(cells.size,"scala",Input(trimEmptyLines(src.stripMargin),OutputFormats.string.toString))
+    this
+  }
+
+  def h1(text: String) = {
+    cells += Cell(cells.size,"heading1",Input(text,OutputFormats.html.toString))
+    this
+  }
+  def md(markdown: String) = {
+    cells += Cell(cells.size,"markdown",Input(stripLeadingWhitespace(markdown.stripMargin),OutputFormats.html.toString))
+    this
+  }
+
+  def saveTo(name:String, file:File): Unit = {
+    implicit val formats = Serialization.formats(NoTypeHints)
+    val ser = Serialization.writePretty(DocumentData(name,cells))
+    val fw = new FileWriter(file)
+    fw.write(ser)
+    fw.close()
+
+  }
+
+}
