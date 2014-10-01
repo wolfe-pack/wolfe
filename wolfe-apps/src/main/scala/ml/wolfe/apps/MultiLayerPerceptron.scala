8437d66684717f255c90651aa85ec01189124474
riedelcastro <sebastian.riedel@gmail.com>
2014-10-01 18:22:34 +0100

    beginnings of a possible MLP formulation in Wolfe

---
+++ wolfe-apps/src/main/scala/ml/wolfe/apps/MultiLayerPerceptron.scala
@@ -0,0 +1,43 @@
+package ml.wolfe.apps
+
+import ml.wolfe.{BruteForceOperators, Wolfe}
+
+/**
+ * @author Sebastian Riedel
+ */
+object MultiLayerPerceptron {
+
+  import Wolfe._
+  import ml.wolfe.macros.OptimizedOperators._
+  import math._
+
+  type MLP = Seq[Seq[Double]]
+
+  case class Instance(mlp:MLP,label:Boolean)
+
+  def space: Iterable[MLP] = ???
+
+  def layerPredicate(w: Vector)(l: Int, mlp: MLP) = {
+    forall(0 until mlp(l).length) { i =>
+      mlp(l)(i) == tanh(sum(0 until mlp(l - 1).length) { j => w((l, i, j)) * mlp(l - 1)(j) })
+    }
+  }
+
+  def layer(w: Vector)(l: Int, mlp: MLP) = {
+    sum(0 until mlp(l).length) { i =>
+      logI(mlp(l)(i) == tanh(sum(0 until mlp(l - 1).length) { j => w((l, i, j)) * mlp(l - 1)(j) }))
+    }
+  }
+
+  def model(w: Vector)(mlp: MLP) = sum(0 until mlp.length) { l => layer(w)(l, mlp) }
+
+  def propagate(w: Vector)(x:Seq[Double]) = BruteForceOperators.argmax(space where (mlp => mlp(0) == x)) { model(w) }
+
+  def f(i:Instance) = sum(0 until i.mlp.last.length) { j => oneHot((i.label,j))}
+
+  def classifier(w:Vector)(i:Instance) = model(w)(i.mlp) + (f(i) dot w)
+
+
+
+
+}
