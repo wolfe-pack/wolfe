dadf5e4959f462b78dedbdcc7fec25e91c4f491b
narad <jason.narad@gmail.com>
2014-10-07 18:40:08 +0100

    FreebaseReader now uses MongoDB (adds to the build file for Casbah)

---
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/GZipReader.scala
@@ -0,0 +1,25 @@
+package ml.wolfe.nlp.io
+
+import java.io.{InputStreamReader, BufferedReader, FileInputStream}
+import java.util.zip.GZIPInputStream
+
+/**
+ * Created by narad on 9/21/14.
+ */
+class GZipReader(filename: String) extends Iterator[String] {
+  val gzip = new GZIPInputStream(new FileInputStream(filename))
+  val br = new BufferedReader(new InputStreamReader(gzip))
+
+  var nextUp = br.readLine()
+
+  def close = br.close()
+
+  def hasNext = nextUp != null
+
+  def next = {
+    val cur = nextUp
+    nextUp = br.readLine()
+    cur
+  }
+
+}
