8d622496088be010b0e5b856f877be1718630f08
riedelcastro <sebastian.riedel@gmail.com>
2014-10-09 09:35:08 +0100

    log scale d3

--- wolfe-ui/src/main/scala/ml/wolfe/ui/D3Plotter.scala
+++ wolfe-ui/src/main/scala/ml/wolfe/ui/D3Plotter.scala
@@ -10,6 +10,8 @@ object D3Plotter {
 
   type XY = (Double,Double)
 
+  val colors = Seq("#ff7f0e","#2ca02c","#7777ff")
+
   def lineplot(chart:XYChart) = {
     val id = "d3line" + Math.abs(chart.hashCode()).toString
     chart.data.serieses
@@ -18,7 +20,7 @@ object D3Plotter {
       series.points.map(p => s"{x:${p._1}, y:${p._2}}").mkString("[",",","]")
     }
 
-    def seriesToJson(series:XYSeries) =
+    def seriesToJson(series:XYSeries, color:String = colors.head) =
       s"""
          |{
          |  values: ${seriesDataToJson(series)},
@@ -28,8 +30,9 @@ object D3Plotter {
        """.stripMargin
 
 
+    def seriesWithColor(series:(XYSeries,Int)) = seriesToJson(series._1, colors(series._2 % colors.size))
 
-    val data = chart.data.serieses.map(seriesToJson).mkString("[",",","]")
+    val data = chart.data.serieses.zipWithIndex.map(seriesWithColor).mkString("[",",","]")
 
     val style =
       s"""
@@ -52,7 +55,6 @@ object D3Plotter {
     def xlabel(in:String) = if (in == "") "X" else in
     def ylabel(in:String) = if (in == "") "Y" else in
 
-
     val html =
       s"""
         |<div id="$id">
@@ -67,7 +69,7 @@ object D3Plotter {
         |    /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
         |  nv.addGraph(function() {
         |  var chart = nv.models.lineChart()
-        |                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+        |                .margin({left: 100, right:100})  //Adjust chart margins to give the x-axis some breathing room.
         |                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
         |                .transitionDuration(350)  //how fast do you want the lines to transition?
         |                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
@@ -82,6 +84,9 @@ object D3Plotter {
         |  chart.yAxis     //Chart y-axis settings
         |      .axisLabel("${ylabel(chart.y.label)}")
         |      .tickFormat(d3.format('.02f'));
+        |
+        |  ${if (chart.x.isLog) "chart.xScale(d3.scale.log());" else ""}
+        |  ${if (chart.y.isLog) "chart.yScale(d3.scale.log());" else ""}
         |
         |  /* Done setting the chart up? Time to render it!*/
         |  var myData = $data;   //You need data...
@@ -105,7 +110,7 @@ object D3Plotter {
     import org.sameersingh.scalaplot.Implicits._
     val x = 0.0 until 2.0 * math.Pi by 0.1
     val y = x map (_ => 0.5)
-    val chart = plot(x ->(math.sin(_)))
+    val chart = plot(x ->(math.sin(_)),x=Axis("freq",log=true))
     val chart2 = Seq(Y(y,"y"))
   }
 
