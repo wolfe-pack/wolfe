58bfc9eb7087688ed1e501b0c6e6cf2d2317fe25
mbosnjak <matko.bosnjak@gmail.com>
2014-10-28 18:01:32 +0000

    Added reader for Algebra Word Problems

---
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/AlgebraProblemReader.scala
@@ -0,0 +1,47 @@
+package ml.wolfe.nlp.io
+
+
+import org.json4s._
+import org.json4s.native.JsonParser._
+
+/**
+ * Created by mbosnjak on 10/28/14.
+ */
+
+// Algebra Word Problems reader
+// for "Learning to Automatically Solve Algebra Word Problems" data
+// downloadable at: http://groups.csail.mit.edu/rbg/code/wordprobs/questions.json
+
+class AlgebraProblemReader(filename: String) extends Iterable[AlgebraProblem] {
+
+  def iterator: Iterator[AlgebraProblem] = {
+    val reader = new java.io.FileReader(filename)
+    val json = parse(reader)
+    reader.close()
+    val list =
+      for {
+        JObject(problem) <- json
+        JField("iIndex", JInt(iIndex)) <- problem
+        JField("sQuestion", JString(sQuestion)) <- problem
+        JField("lEquations", JArray(lEquations)) <- problem
+        JField("lSolutions", JArray(lSolutions)) <- problem
+      } yield {
+        val equations = lEquations.map(_.asInstanceOf[JString].s).toIterable
+        val solutions = lSolutions.map(_.asInstanceOf[JDouble].num).toIterable
+        AlgebraProblem(iIndex.toInt, sQuestion, equations, solutions)
+      }
+
+    list.asInstanceOf[List[AlgebraProblem]].toIterator
+  }
+}
+
+object AlgebraProblemReader {
+  def main(args: Array[String]) {
+    for (q <- new AlgebraProblemReader(args(0))) {
+      println(q) + "\n"
+    }
+  }
+}
+
+
+case class AlgebraProblem(id: Int, question: String, equations: Iterable[String], solutions: Iterable[Double])
