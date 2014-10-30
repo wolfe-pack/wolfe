2c173266ffdbcaa656a378af25c5d6a6e3208bfe
rockt <tim.rocktaeschel@gmail.com>
2014-10-30 18:02:05 +0000

    refactoring; output dirs;  cell & formulae weights

--- wolfe-apps/src/main/scala/ml/wolfe/apps/factorization/MatrixFactorization.scala
+++ wolfe-apps/src/main/scala/ml/wolfe/apps/factorization/MatrixFactorization.scala
@@ -1,41 +1,50 @@
 package ml.wolfe.apps.factorization
 
-import java.io.{OutputStream, File}
+import java.io.{FileWriter, OutputStream, File}
 
 import cc.factorie.la.DenseTensor1
 import cc.factorie.optimize._
 import cc.factorie.util.{Logger, FastLogging}
 import com.typesafe.config.ConfigFactory
 import ml.wolfe.FactorGraph.Edge
-import ml.wolfe.apps.{ImplNeg, Impl, TensorKB}
+import ml.wolfe.apps.factorization.TensorKB
 import ml.wolfe.apps.factorization.io.{EvaluateNAACL, LoadNAACL, WriteNAACL}
 import ml.wolfe.fg.L2Regularization
 import ml.wolfe.fg._
 import ml.wolfe.util.{Conf, ProgressLogging, ProgressBar, Timer}
 import ml.wolfe.{DenseVector, FactorieVector, GradientBasedOptimizer, Wolfe}
 
+import scala.io.Source
 import scala.util.Random
 
 /**
  * @author Sebastian Riedel
+ * @author rockt
  */
 object MatrixFactorization extends App {
   val debug = false //whether to use sampled matrices or the NAACL data
-  val loadFormulae = true //whether to use formulae or not
-  val print = false //whether to print the matrix (only do this for small ones!)
+  val loadFormulae = debug && true //whether forumlae should be sampled for debugging
+  //val print = false //whether to print the matrix (only do this for small ones!)
 
   val confPath = if (args.size > 0) args(0) else "conf/mf.conf"
   Conf.add(confPath)
   implicit val conf = Conf
-  println("Using config at " + confPath)
+  println("Using " + confPath)
 
-  val outputPath = conf.getString("mf.outPath")
+  val outputPath = conf.getString("outDir")
   val fileName = conf.getString("mf.outFile")
 
   val k = conf.getInt("mf.k")
   val lambda = conf.getDouble("mf.lambda")
   val alpha = conf.getDouble("mf.alpha")
   val maxIter = conf.getInt("mf.maxIter")
+
+  val subsample = conf.getDouble("mf.subsample")
+  val negPerPos = conf.getInt("mf.negPerPos")
+  val unobservedPerF = conf.getInt("mf.unobservedPerF")
+
+  val cellWeight = conf.getDouble("mf.cellWeight")
+  val formulaeWeight = conf.getDouble("mf.formulaeWeight")
 
   val optimizer = conf.getString("mf.optimizer")
   val batchTraining = conf.getBoolean("mf.batchTraining")
@@ -49,7 +58,7 @@ object MatrixFactorization extends App {
       tmp += ImplNeg("r8", "r6")
     }
     tmp
-  } else LoadNAACL(k, loadFormulae)
+  } else LoadNAACL(k, subsample)
 
   val rand = new Random(0l)
 
@@ -58,33 +67,27 @@ object MatrixFactorization extends App {
   val V = db.ix1ToNodeMap //cols
   val A = db.ix2ToNodeMap //rows
 
-
   //initialize embeddings
   //def nextInit() = (rand.nextDouble() - 0.5) * 0.1
   def nextInit() = rand.nextGaussian() * 0.1
   (V.values.view ++ A.values.view).foreach(n =>
     n.variable.asVector.b = new DenseVector((0 until k).map(i => nextInit()).toArray))
 
-  //println(V.values.head.variable.asVector.b)
 
   //fact factors
   for (d <- data) {
-    //colIx: relation
-    //rowIx: entity
     val (colIx, rowIx, _) = d.key
-    val a = A(rowIx)
-    val v = V(colIx)
+    val a = A(rowIx) //entity
+    val v = V(colIx) //relation
 
-    //create positive fact factor
-    fg.buildFactor(Seq(a, v))(_ map (_ => new VectorMsgs)) { 
-      e => new CellLogisticLoss(e(0), e(1), 1.0, lambda) with L2Regularization
-      //e => new CellLogisticLoss(e(0), e(1), 0.9, lambda) with L2Regularization
+    fg.buildFactor(Seq(a, v))(_ map (_ => new VectorMsgs)) {
+      e => new CellLogisticLoss(e(0), e(1), 1.0, lambda, cellWeight) with L2Regularization
     }
 
-    //also create a sampled stochastic negative factor in the same column
-    fg.buildStochasticFactor(Seq(v, db.sampleNode(colIx)))(_ map (_ => new VectorMsgs)) {
-      e => new CellLogisticLoss(e(0), e(1), 0.0, lambda) with L2Regularization
-      //e => new CellLogisticLoss(e(0), e(1), 0.1, lambda) with L2Regularization
+    (0 until negPerPos).foreach { i =>
+      fg.buildStochasticFactor(Seq(v, db.sampleNode(colIx)))(_ map (_ => new VectorMsgs)) {
+        e => new CellLogisticLoss(e(0), e(1), 0.0, lambda, cellWeight / negPerPos) with L2Regularization
+      }
     }
   }
 
@@ -106,19 +109,22 @@ object MatrixFactorization extends App {
         formula match {
           case Impl(_, _, target) =>
             fg.buildFactor(Seq(cNode, p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
-              e => new ImplPotential(e(0), e(1), e(2), target, lambda) with L2Regularization
+              e => new ImplPotential(e(0), e(1), e(2), target, lambda, formulaeWeight) with L2Regularization
             }
-            //also inject the formula for a constant it hasn't been observed with
-            fg.buildStochasticFactor(Seq(db.sampleNode(colIx), p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
-              e => new ImplPotential(e(0), e(1), e(2), target, lambda) with L2Regularization
+            (0 until unobservedPerF).foreach { i =>
+              fg.buildStochasticFactor(Seq(db.sampleNode(colIx), p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
+                e => new ImplPotential(e(0), e(1), e(2), target, lambda, formulaeWeight) with L2Regularization
+              }
             }
+
           case ImplNeg(_, _, target) =>
             fg.buildFactor(Seq(cNode, p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
-              e => new ImplNegPotential(e(0), e(1), e(2), target, lambda) with L2Regularization
+              e => new ImplNegPotential(e(0), e(1), e(2), target, lambda, formulaeWeight) with L2Regularization
             }
-            //also inject the formula for a constant it hasn't been observed with
-            fg.buildStochasticFactor(Seq(db.sampleNode(colIx), p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
-              e => new ImplNegPotential(e(0), e(1), e(2), target, lambda) with L2Regularization
+            (0 until unobservedPerF).foreach { i =>
+              fg.buildStochasticFactor(Seq(db.sampleNode(colIx), p1Node, p2Node))(_ map (_ => new VectorMsgs)) {
+                e => new ImplNegPotential(e(0), e(1), e(2), target, lambda, formulaeWeight) with L2Regularization
+              }
             }
         }
       } else {
@@ -131,13 +137,15 @@ object MatrixFactorization extends App {
   fg.build()
 
 
-  println(db.toInfoString)
+  println("DB:" + db.toInfoString)
+  println("FG:" + fg.toInspectionString)
 
   val gradientOptimizer = optimizer match {
     case "SGD" => new ConstantLearningRate(baseRate = alpha)
     case "AdaGrad" => new AdaGrad(rate = alpha)
     case "AdaMira" => new AdaMira(rate = alpha)
     case "LBFGS" => new LBFGS(Double.MaxValue, Int.MaxValue) //rockt: not working atm
+    case "AvgPerceptron" => new AveragedPerceptron()
   }
 
   println("Optimizing...")
@@ -149,20 +157,28 @@ object MatrixFactorization extends App {
   }
   println("Done after " + Timer.reportedVerbose("optimization"))
 
-  if (debug && print) {
+  if (debug) {
     println("train:")
     println(db.toVerboseString(showTrain = true))
     println()
 
     println("predicted:")
     println(db.toVerboseString())
-
   } else {
-    WriteNAACL(db, outputPath + fileName)
-    EvaluateNAACL.main(Array("./conf/eval.conf", outputPath + fileName))
+    val pathToPredict = Conf.outDir.getAbsolutePath + "/" + fileName
+    WriteNAACL(db, pathToPredict)
+    EvaluateNAACL.main(Array("./conf/eval.conf", pathToPredict))
 
     import scala.sys.process._
-    Process("pdflatex -interaction nonstopmode -shell-escape table.tex", new File(outputPath)).!!
+    Process("pdflatex -interaction nonstopmode -shell-escape table.tex", new File(Conf.outDir.getAbsolutePath)).!!
+
+    if (Conf.hasPath("formulaeFile")) {
+      val formulaeFile = new File(Conf.getString("formulaeFile"))
+      val lines = Source.fromFile(formulaeFile).getLines()
+      val writer = new FileWriter(Conf.outDir.getAbsolutePath + "/" + formulaeFile.getAbsolutePath.split("/").last)
+      writer.write(lines.mkString("\n"))
+      writer.close()
+    }
   }
 }
 
