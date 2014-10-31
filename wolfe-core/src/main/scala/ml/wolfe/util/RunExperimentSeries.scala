f717cfa0d73a828937c27e084dd79d8ec91297d6
rockt <tim.rocktaeschel@gmail.com>
2014-10-31 18:36:40 +0000

    refactored series of experiments

--- wolfe-core/src/main/scala/ml/wolfe/util/RunExperimentSeries.scala
+++ wolfe-core/src/main/scala/ml/wolfe/util/RunExperimentSeries.scala
@@ -1,21 +1,16 @@
-package ml.wolfe.apps.factorization.io
+package ml.wolfe.util
 
-import java.io.{FileWriter, File}
+import java.io.{File, FileWriter}
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.concurrent.Executors
 
-import com.typesafe.config.{Config, ConfigFactory}
-import ml.wolfe.util.{OverrideConfig, Conf}
-import org.joda.time.LocalDate
-
-import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 
 /**
  * @author rockt
  */
-object RunSeriesOfExperiments {
+object RunExperimentSeries {
   type Series = Map[String, Seq[Any]]
   type Confs = Seq[Map[String, Any]]
 
@@ -28,7 +23,7 @@ object RunSeriesOfExperiments {
       }
   }
 
-  def apply(series: Series, threads: Int = 1, confPath: String = "wolfe-apps/conf/mf.conf"): Unit = {
+  def apply[T](series: Series, threads: Int = 1, confPath: String = "wolfe-apps/conf/mf.conf")(body: String => T): Unit = {
     val confSeq: Confs = series.map { case (key, value) =>
       (value zip (Stream continually Seq(key)).flatten).map(_.swap)
     }.foldLeft(Seq[Any](Nil)) { case (seqs, seq) => (seqs flatCross seq).toSeq }
@@ -48,10 +43,10 @@ object RunSeriesOfExperiments {
       ))
     } else confSeq
 
-    runConfs(confSeqWithCollectFields, threads, confPath)
+    runConfs(confSeqWithCollectFields, threads, confPath)(body)
   }
 
-  def runConfs(confs: Confs, threads: Int = 1, confPath: String = "wolfe-apps/conf/mf.conf"): Unit = {
+  private def runConfs[T](confs: Confs, threads: Int = 1, confPath: String = "wolfe-apps/conf/mf.conf")(body: String => T): Unit = {
     import scala.concurrent.{future, blocking, Future, Await}
     import scala.concurrent.duration._
 
@@ -65,51 +60,20 @@ object RunSeriesOfExperiments {
       def reportFailure(t: Throwable) {}
     }
 
-    val configsFut: Traversable[Future[ForkRunMatrixFactorization]] = confs.map {
+    val configsFut: Traversable[Future[T]] = confs.map {
       c => future {
         blocking {
           val newConfPath = File.createTempFile(System.nanoTime().toString, null).getAbsolutePath
           OverrideConfig(c, newConfPath, confPath)
-          ForkRunMatrixFactorization(newConfPath)
+          body(newConfPath)
+          body(newConfPath)
         }
       }
     }
 
     //waiting until experiments are finished; not longer than a year ;)
-    val resultsFut: Future[Traversable[ForkRunMatrixFactorization]] = Future.sequence(configsFut)
-
+    val resultsFut: Future[Traversable[T]] = Future.sequence(configsFut)
     import scala.language.postfixOps
     Await.result(resultsFut, 365 days)
   }
-}
-
-case class ForkRunMatrixFactorization(pathToConf: String = "conf/mf.conf") {
-  import scala.sys.process._
-  val userDir = System.getProperty("user.dir")
-
-  //check whether in run from the right directory
-  val correctDir = if (userDir.endsWith("/wolfe")) userDir else userDir.split("/").init.mkString("/")
-
-  println(Process(Seq(
-    "sbt",
-    "project wolfe-apps",
-    "vmargs -Xmx4G",
-    s"run-main ml.wolfe.apps.factorization.MatrixFactorization $pathToConf"), new File(correctDir)
-  ).!)
-}
-
-object SubsampleExperiments extends App {
-  val threads = args.lift(0).getOrElse("1").toInt
-  val confPath = args.lift(1).getOrElse("conf/mf.conf")
-  val logFilePath = args.lift(2).getOrElse("data/out/experiments.log")
-
-  Conf.add(OverrideConfig(Map("logFile" -> logFilePath), confPath + ".tmp", confPath))
-
-  val series = Map(
-    "mf.subsample" -> (0.1 to 1.0 by 0.1).toSeq,
-    "formulaeFile" -> Seq("None", "data/formulae/curated-50-100.txt")
-  )
-
-  RunSeriesOfExperiments(series, threads, confPath)
-  System.exit(0)
 }
