3d49b0a0e393c09fc7e23532bf2727c7503f2b23
Andreas Vlachos <av308@cl.cam.ac.uk>
2014-11-06 11:37:10 +0000

    Added the Rosenbrock function to the unit tests for the optimizer.

--- wolfe-util/src/test/scala/ml/wolfe/util/NelderMeadSpecs.scala
+++ wolfe-util/src/test/scala/ml/wolfe/util/NelderMeadSpecs.scala
@@ -8,15 +8,15 @@ import scala.math._
  */
 class NelderMeadSpecs extends WolfeSpec {
 
-  class ToyProblem extends OptimisationProblem{
-    override val parametersToOptimize: Seq[HyperParameter] = Seq(new HyperParameter("A"), new HyperParameter("B"),new HyperParameter("C"))
+  class ToyProblem extends OptimisationProblem {
+    override val parametersToOptimize: Seq[HyperParameter] = Seq(new HyperParameter("A"), new HyperParameter("B"), new HyperParameter("C"))
     /**
      * Evaluate the optimisation problem given the set of hyper parameters.
      * @param hyperparameters The map of hyper parameters
      * @return The score of this evaluation, higher is better
      */
     override def evaluate(hyperparameters: Map[String, Double]): Double = {
-/*
+      /*
     Python code from scipy
         self.F = array([[1,1,1],[1,1,0],[1,0,1],[1,0,0],[1,0,0]])
         self.K = array([1., 0.3, 0.5])
@@ -26,10 +26,10 @@ class NelderMeadSpecs extends WolfeSpec {
         return f
  */
       val Fs: Seq[Map[String, Double]] = Seq(Map("A" -> 1.0, "B" -> 1.0, "C" -> 1.0),
-      Map("A" -> 1.0, "B" -> 1.0, "C" -> 0.0),
-      Map("A" -> 1.0, "B" -> 0.0, "C" -> 1.0),
-      Map("A" -> 1.0, "B" -> 0.0, "C" -> 0.0),
-      Map("A" -> 1.0, "B" -> 0.0, "C" -> 0.0))
+        Map("A" -> 1.0, "B" -> 1.0, "C" -> 0.0),
+        Map("A" -> 1.0, "B" -> 0.0, "C" -> 1.0),
+        Map("A" -> 1.0, "B" -> 0.0, "C" -> 0.0),
+        Map("A" -> 1.0, "B" -> 0.0, "C" -> 0.0))
 
       val Ks: Map[String, Double] = Map("A" -> 1.0, "B" -> 0.3, "C" -> 0.5)
 
@@ -41,13 +41,13 @@ class NelderMeadSpecs extends WolfeSpec {
       //  }
       //}
 
-      val log_pdot = (for (key <- hyperparameters.keys) yield (key , (for (f <- Fs) yield hyperparameters(key) * f(key)).sum)).toMap
+      val log_pdot = (for (key <- hyperparameters.keys) yield (key, (for (f <- Fs) yield hyperparameters(key) * f(key)).sum)).toMap
 
 
       var Z = 0.0
       var Kx = 0.0
 
-      for (key <- hyperparameters.keys){
+      for (key <- hyperparameters.keys) {
         Z += exp(log_pdot(key))
         Kx += Ks(key) * hyperparameters(key)
       }
@@ -56,27 +56,64 @@ class NelderMeadSpecs extends WolfeSpec {
 
     }
   }
+  class RosenbrockProblem extends OptimisationProblem {
+    override val parametersToOptimize: Seq[HyperParameter] = _
+    /**
+     * Evaluate the Rosenbrock function:
+     * return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+     * @param hyperparameters The map of hyper parameters
+     * @return The score of this evaluation, higher is better
+     */
+    override def evaluate(hyperparameters: Map[String, Double]): Double = {
+
+      val values = hyperparameters.values.toArray
+      var result = 0.0
+      // iterate over all 1..N-1
+      for (index <- 0 until values.size -1){
+        println(index)
+        result += pow(1- values(index), 2.0)
+        result += 100*pow(values(index+1) - pow(values(index), 2.0), 2.0)
+
+      }
+      result
+    }
+  }
+
 
   // This implements the unit test of SciPy
   "NelderMeadSimplex" should {
-    "find the correct solution" in {
-     val toyProblem : OptimisationProblem = new ToyProblem()
-     val myOptimizer : HyperParameterOptimisationAlgorithm = new NelderMeadSimplex()
+    "find the correct solution for the toy Berger et al. (1996) problem" in {
+      val toyProblem: OptimisationProblem = new ToyProblem()
+      val myOptimizer: HyperParameterOptimisationAlgorithm = new NelderMeadSimplex()
 
 
-     // Set the starting point to 0
-     val zeros: Map[String, Double] = Map("A" -> 0, "B" -> 0, "C" -> 0)
-     myOptimizer.optimise(toyProblem, zeros)
-     val solutionFound = myOptimizer.bestParameters
-     solutionFound("A") should be(0.0)
-     solutionFound("B") should be(-0.524869316)
-     solutionFound("C") should be(0.487525860)
+      // Set the starting point to 0
+      val zeros: Map[String, Double] = Map("A" -> 0, "B" -> 0, "C" -> 0)
+      myOptimizer.optimise(toyProblem, zeros)
+      val solutionFound = myOptimizer.bestParameters
+      solutionFound("A") should be(0.0)
+      solutionFound("B") should be(-0.524869316)
+      solutionFound("C") should be(0.487525860)
 
     }
 
+    "find the correct solution for the Rosenbrock function" in {
+      val toyProblem: OptimisationProblem = new RosenbrockProblem()
+      val myOptimizer: HyperParameterOptimisationAlgorithm = new NelderMeadSimplex()
 
+      // Set the starting point to 1.3, 0.7, 0.8, 1.9, 1.2]
+      val startingPoint: Map[String, Double] = Map("A" -> 1.3, "B" -> 0.7, "C" -> 0.8, "D" -> 1.9, "E" -> 1.2)
+      myOptimizer.optimise(toyProblem, startingPoint)
+      val solutionFound = myOptimizer.bestParameters
+      solutionFound("A") should be(1.0)
+      solutionFound("B") should be(1.0)
+      solutionFound("C") should be(1.0)
+      solutionFound("D") should be(1.0)
+      solutionFound("E") should be(1.0)
+    }
   }
 }
+
 /*
   "A simple index should" should (
 
