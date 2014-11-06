6d379c460b3df8b4513d19f29b0760dc491b7406
mbosnjak <matko.bosnjak@gmail.com>
2014-11-06 00:19:56 +0000

    Further progress on Logistic regression, added blank Unit test

--- wolfe-neural/src/main/scala/ml/wolfe/neural/LogisticRegression.scala
+++ wolfe-neural/src/main/scala/ml/wolfe/neural/LogisticRegression.scala
@@ -6,55 +6,61 @@ import breeze.numerics._
 /**
  * Created by mbosnjak on 11/5/14.
  */
-// INPUT: EITHER A VECTOR OR A MATRIX!
-// TODO: betterize this code! I am but a novice here, and I need to soak up more advance knowledge!!! ^_^
+
+
+/** Logistic regression layer / classifier
+ *
+ * @param input matrix of examples
+ * @param W internal weights
+ * @param b biases
+ * @param input_size size of the feature space
+ * @param output_size size of the output (number of classes)
+ */
 class LogisticRegression(val input: DenseMatrix[Double],
                          var W: DenseMatrix[Double],
                          var b: DenseVector[Double],
                          val input_size: Int,
                          val output_size: Int) {
 
+  // FIXED
+  val N = input.rows
 
-  // if no values for W and b are provided, intialize them to 0
+  // initialize if empty
   if (W == null)
     W = DenseMatrix.zeros(input_size, output_size)
   if (b == null)
     b = DenseVector.zeros[Double](output_size)
 
-  val theta = DenseMatrix.vertcat(b.toDenseMatrix, W)
+  // variable, as it can be changed
+  var theta = DenseMatrix.vertcat(b.toDenseMatrix, W)
 
-  val X = DenseMatrix.horzcat(DenseMatrix.ones[Double](input.rows, 1), input)
+  var X = DenseMatrix.horzcat(DenseMatrix.ones[Double](input.rows, 1), input)
+
 
   def softmax(M: DenseMatrix[Double]): DenseMatrix[Double] = {
     for (i <- 0 to M.rows-1) {
-      M(i,::) := M(i,::).inner.map(math.exp(_)).t / M(i,::).inner.map(math.exp(_)).sum
+      val exprow = M(i,::).inner.map(math.exp(_))
+      M(i,::) := exprow.t / exprow.sum
     }
     M
   }
 
-
   def argmax(M: DenseMatrix[Double]): DenseVector[Double] = {
     val ret = DenseVector.zeros[Double](M.rows)
     for (i <- 0 to M.rows-1) {
-      println(M(i,::).inner)
       ret(i) = M(i, ::).inner.argmax
     }
     ret
   }
 
-
-  var p_y_given_x: DenseMatrix[Double] = softmax(X * theta)
+  def p_y_given_x: DenseMatrix[Double] = softmax(X * theta)
 
   def get_p_y_given_x(M: DenseMatrix[Double]): DenseMatrix[Double] = {
     val Q = DenseMatrix.horzcat(DenseMatrix.ones[Double](M.rows, 1), M)
     softmax(Q * theta)
   }
 
-
-
-  val y_pred = argmax(p_y_given_x)
-
-
+  def y_pred = argmax(p_y_given_x)
 
   def output = y_pred.toDenseMatrix
 
@@ -69,86 +75,88 @@ class LogisticRegression(val input: DenseMatrix[Double],
   // TODO: heavily test gradients
   // gradient for negative_log_likelihood
   // -(1/m) * sum(i=1:m, x(i) * ( I(y(i)=j) - p_y_given_x(i, y(i)) ) )
+  def grad(y: DenseVector[Double]) = {
+    // Identity-function-matrix with 1 only where row equals to an example, and column equals to the correct prediction
+    // for that example
 
-//  def grad(y: DenseVector[Double]) = {
-//    // Identity-function-matrix with 1 only where row equals to an example, and column equals to the correct prediction
-//    // for that example
-//    val sigma = DenseMatrix.zeros[Double](N, output_size)
-//    for (i <- 0 to N-1)
-//      sigma(i, y(i).toInt) = 1
-//    input.t * (sigma - p_y_given_x)
-//  }
+    val sigma = DenseMatrix.zeros[Double](N, output_size)
+    for (i <- 0 to N-1)
+      sigma(i, y(i).toInt) = 1
+    (X.t * (sigma - p_y_given_x)) :* (-1.0/N)
+  }
 
 
   def negative_log_likelihood(y: DenseVector[Double]): Double = {
     require(y.length == p_y_given_x.rows)
-    var loss = 0.0
+    var ll = 0.0
     for (i <- 0 to y.length - 1 ) {
-      loss += math.log(p_y_given_x(i, y(i).toInt))
+      ll += math.log(p_y_given_x(i, y(i).toInt))
     }
     // mean instead of sum, makes learning rate less dependent on minibatch size
-    - loss / y.length.toDouble //N
+    - ll / y.length.toDouble //N
   }
 
 
   def errors(y: DenseVector[Double]): Double = {
     require(y_pred.length == y.length)
     val neq = y_pred :!= y
-    neq.activeSize / neq.length
+    neq.activeSize.toDouble / neq.length
   }
 
-
-
-
-
-
-
-  // calculate p(y|x)
-
-
-
-
-
 }
-
-
 
 
 
 object LogisticRegressionPlayground extends App {
   val x = DenseMatrix((2.0, 3.0), (2.0, 2.0), (4.0, 4.0), (5.0, -1.0), (4.0, 3.0))
   val y = DenseVector(0.0, 0.0, 0.0, 2.0, 0.0)
-  val y1 = DenseVector(0.0, 2.0, 0.0, 1.0, 0.0)
+  val y1 = DenseVector(1.0, 2.0, 1.0, 0.0, 2.0)
   val w = DenseMatrix((2.0, 2.0, 2.0), (6.0, 4.0, 3.0))
   val b = DenseVector(1.0,2.0,3.0)
 
   val lr = new LogisticRegression(x, w, b, 2, 3)
-  println(lr.errors(y))
-  println(lr.errors(y1))
-  println(lr.negative_log_likelihood(y))
-  println(lr.negative_log_likelihood(y1))
 
-  val learningRate = 0.5
+  val learningRate = 0.02
+
+  println("errors: " + lr.errors(y1))
+  println("nll: " + lr.negative_log_likelihood(y1))
+  for (i <- 0 to 100) {
+    lr.theta = (lr.theta - (lr.grad(y1) :* learningRate))
+    println("nll: " + lr.negative_log_likelihood(y1))
+    println("errors: " + lr.errors(y1))
+  }
+
+
+//  lr.theta = (lr.theta - learningRate * lr.grad(y1))
+//
+//  println(lr.negative_log_likelihood(y1))
+//  lr.theta = (lr.theta - learningRate * lr.grad(y1))
+
+
+
+  //println(lr.W - learningRate * lr.grad(y))
+
+  //println(lr.W - learningRate * lr.grad(y1))
 
   //val gradW = w
 
   //println(lr.W - learningRate * gradW)
   //lr.b = lr.b - learningRate * gradb
 
-//
-//
-//  import BIDMat.{SBMat, CMat, CSMat, DMat, Dict, IDict, FMat, GMat, GIMat, GSMat, HMat, IMat, Mat, SMat, SDMat}
-//  import BIDMat.MatFunctions._
-//  import BIDMat.SciFunctions._
-//  import BIDMat.Solvers._
-//  import BIDMat.Plotting._
-//
-//
-//  val a = rand(100,100)
-//
-//  flip; val c = a*a; val ff=gflop
-//  print(ff)
-//
+  //
+  //
+  //  import BIDMat.{SBMat, CMat, CSMat, DMat, Dict, IDict, FMat, GMat, GIMat, GSMat, HMat, IMat, Mat, SMat, SDMat}
+  //  import BIDMat.MatFunctions._
+  //  import BIDMat.SciFunctions._
+  //  import BIDMat.Solvers._
+  //  import BIDMat.Plotting._
+  //
+  //
+  //  val a = rand(100,100)
+  //
+  //  flip; val c = a*a; val ff=gflop
+  //  print(ff)
+  //
 
 
 
---
+++ wolfe-neural/src/test/scala/ml/wolfe/neural/DeepTests.scala
@@ -0,0 +1,17 @@
+package ml.wolfe.neural
+
+import ml.wolfe.WolfeSpec
+
+/**
+ * Created by mbosnjak on 11/6/14.
+ */
+class DeepTests extends WolfeSpec {
+  "Logistic regression" should {
+
+    "Test case" in {
+      1 should be (1)
+    }
+
+  }
+}
+
