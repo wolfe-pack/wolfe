f9144010c46f335fd762ded3245bf55ed06441c2
riedelcastro <sebastian.riedel@gmail.com>
2014-11-06 01:59:46 +0000

    Iris refactoring

--- wolfe-core/src/main/scala/ml/wolfe/util/Util.scala
+++ wolfe-core/src/main/scala/ml/wolfe/util/Util.scala
@@ -65,13 +65,14 @@ object Iris {
   implicit val classes = Seq(Label("Iris-setosa"), Label("Iris-versicolor"), Label("Iris-virginica"))
 
   case class Label(label: String)
-  case class IrisData(sepalLength: Double, sepalWidth: Double, petalLength: Double, petalWidth: Double, irisClass: Label)
+  case class IrisData(features:IrisFeatures, irisClass: Label)
+  case class IrisFeatures(sepalLength: Double, sepalWidth: Double, petalLength: Double, petalWidth: Double)
 
   def loadIris() = {
     val stream = Util.getStreamFromClassPathOrFile("ml/wolfe/datasets/iris/iris.data")
     val data = for (line <- Source.fromInputStream(stream).getLines().toBuffer if line.trim != "") yield {
       val Array(sl, sw, pl, pw, ic) = line.split(",")
-      IrisData(sl.toDouble, sw.toDouble, pl.toDouble, pw.toDouble, Label(ic))
+      IrisData(IrisFeatures(sl.toDouble, sw.toDouble, pl.toDouble, pw.toDouble), Label(ic))
     }
     stream.close()
     data
