c3956311e6b55dc6e77930abc096a037817df3a1
Ingolf Becker <watercrossing@users.noreply.github.com>
2014-11-06 12:48:05 +0000

    Fix to the Nelder Mead Simplex. Unit tests now work.

--- wolfe-util/src/main/scala/ml/wolfe/util/NelderMeadSimplex.scala
+++ wolfe-util/src/main/scala/ml/wolfe/util/NelderMeadSimplex.scala
@@ -20,19 +20,14 @@ class NelderMeadSimplex extends HyperParameterOptimisationAlgorithm {
   override def optimise(problem: OptimisationProblem, startingPoint: Param): Param = {
     var dimensions = problem.parametersToOptimize.length
 
-    // this is an implicit ordering, implementing an ordering for the type (Double, Param)
-    implicit object MyOrdering extends Ordering[(Double,Map[String,Double])] {
-       def compare(x: (Double, Param), y: (Double, Param)): Int = {
-         (x._1 - y._1).toInt
-       }
-    }
-
-    // PriorityQueue searches the current scope for a potential ordering for the type, and will find the implicit ordering above
-    // Should have worst point as head, best point as last
-    var points: mutable.PriorityQueue[(Double, Param)] = new mutable.PriorityQueue[(Double, Param)]()
+    println(f"Starting optimisation with $dimensions%d dimensions.")
+    var points: mutable.ArrayBuffer[(Double, Param)] = new mutable.ArrayBuffer[(Double, Param)]()
 
     // startingPoints have been modified to form a convex simplex
-    val startingPoints = for (key <- startingPoint.keys) yield (startingPoint + (key -> (startingPoint(key) + 1 )))
+    def modifyStartingPoint(start : Double): Double =
+      if (start == 0.0) 0.00025 else 0.05 + start
+
+    val startingPoints = for (key <- startingPoint.keys) yield (startingPoint + (key -> (modifyStartingPoint(startingPoint(key)))))
 
     // evaluate the starting points
     val evaluatedStartingPoints = for (x <- startingPoints) yield (problem.evaluate(x), x)
@@ -40,68 +35,79 @@ class NelderMeadSimplex extends HyperParameterOptimisationAlgorithm {
     points += ((problem.evaluate(startingPoint), startingPoint))
 
     points ++= evaluatedStartingPoints
-    assert(points.head._1 > points.last._1,"The ordering is wrong")
+
     def accumulate(acc: Param, element: (Double, Param)): Param = (
-      element._2.keys.map(x=> (x, acc(x) + element._2(x))).toMap
+      element._2.keys.map(x=> (x, acc(x) + element._2(x) / dimensions)).toMap
     )
 
     var error = 1.0
-    while (error > 1e-6) {
-      var centroid = points.foldLeft(startingPoint.keys.map(x=>(x,0.0)).toMap) (accumulate)
-      var worstElement = points.dequeue()
+    while (error > 1e-12) {
+      val worstElement = points.maxBy(_._1)
+      points -= worstElement
 
+      val centroid = points.foldLeft(startingPoint.keys.map(x => (x, 0.0)).toMap)(accumulate)
+      var bestElement = points.minBy(_._1)
+      val secondWorstElement = points.maxBy(_._1)
+
+      println(f"bestElement: ${bestElement._1}%.8f at ${bestElement._2("A")}%.8f, ${bestElement._2("B")}%.8f, ${bestElement._2("B")}%.8f")
 
       def pointAlongVertex(centroid: Param, worstPoint: Param, scale: Double): Param = (
-        worstPoint.keys.map(x => (x, centroid(x) + scale * worstPoint(x))).toMap
+        worstPoint.keys.map(x => (x, centroid(x) + scale * (worstPoint(x) - centroid(x)))).toMap
       )
 
-      var reflectionPoint = pointAlongVertex(centroid, worstElement._2, -1.0)
-      var valueAtReflectionPoint = problem.evaluate(reflectionPoint)
+      val reflectionPoint = pointAlongVertex(centroid, worstElement._2, -1.0)
+      val valueAtReflectionPoint = problem.evaluate(reflectionPoint)
 
-      if ( valueAtReflectionPoint < points.head._1 && points.last._1 <= valueAtReflectionPoint ) {
+      if ( valueAtReflectionPoint < secondWorstElement._1 && bestElement._1 <= valueAtReflectionPoint ) {
         // reflected point is neither best or worst in the new simplex
         points += ((valueAtReflectionPoint, reflectionPoint))
-      } else if (valueAtReflectionPoint < points.last._1) {
+      } else if (valueAtReflectionPoint < bestElement._1) {
         // relected point is better than the current best, try to go further along that direction
-        var secondReflectionPoint = pointAlongVertex(centroid, points.head._2,-2.0)
-        var valueAtSecondReflectionPoint = problem.evaluate(secondReflectionPoint)
+        val secondReflectionPoint = pointAlongVertex(centroid, worstElement._2, -2.0)
+        val valueAtSecondReflectionPoint = problem.evaluate(secondReflectionPoint)
         if (valueAtSecondReflectionPoint < valueAtReflectionPoint) {
           points += ((valueAtSecondReflectionPoint, secondReflectionPoint))
         } else {
           points += ((valueAtReflectionPoint, reflectionPoint))
         }
 
-      }  else if ( valueAtReflectionPoint > points.head._1) {
+      }  else {
         // reflected point is worse than current worst, contract
         var found: Boolean = false
         if (valueAtReflectionPoint < worstElement._1) {
           // try to perform "outside" contraction
-          var halfReflectionPoint = pointAlongVertex(centroid, worstElement._2,-0.5)
-          var valueAtHalfReflection = problem.evaluate(halfReflectionPoint)
+          val halfReflectionPoint = pointAlongVertex(centroid, worstElement._2, -0.5)
+          val valueAtHalfReflection = problem.evaluate(halfReflectionPoint)
           if (valueAtHalfReflection < valueAtReflectionPoint) {
             points += ((valueAtHalfReflection, halfReflectionPoint))
             found = true
           }
-        } else if (!found) {
+        } else {
           // Try to perform "inside" contraction
-          var halfReflectionPoint = pointAlongVertex(centroid, worstElement._2, 0.5)
-          var valueAtHalfReflection = problem.evaluate(halfReflectionPoint)
+          val halfReflectionPoint = pointAlongVertex(centroid, worstElement._2, 0.5)
+          val valueAtHalfReflection = problem.evaluate(halfReflectionPoint)
           if (valueAtHalfReflection < worstElement._1) {
             points += ((valueAtHalfReflection,halfReflectionPoint))
             found = true
           }
-        } else if (!found) {
+        }
+        if (!found) {
           // Neither outside nor inside contraction was acceptable, shrink simplex toward best solution
 
-          def averageBetweenTwo(best: Param, other: Param) : Param = (
-            other.keys.map(x=> (x, 0.5 * (best(x) + other(x)))).toMap
+          def averageBetweenTwo(best: Param, other: (Double,Param)) : (Double, Param) = (
+            if (best == other._2) {
+              other
+            } else {
+              val newpoint = best.keys.map(x => (x, 0.5 * (best(x) + other._2(x)))).toMap
+              (problem.evaluate(newpoint), newpoint)
+            }
           )
-          points = points.map(x => (problem.evaluate(averageBetweenTwo(points.last._2,x._2)), averageBetweenTwo(points.last._2,x._2)))
 
-
+          points += worstElement
+          points = points.map(x => averageBetweenTwo(bestElement._2,x))
         }
       }
-      error = math.abs(points.head._1 - points.last._1)
+      error = math.abs(points.maxBy(_._1)._1 - points.minBy(_._1)._1)
 
     }
     this.bestParameters = points.last._2
