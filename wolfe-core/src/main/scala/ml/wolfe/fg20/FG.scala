817e538a0236a1e7c25ab494dc79f8edb5f1dbfd
luke <insperatum@gmail.com>
2014-11-07 00:04:10 +0000

    20!

--- wolfe-core/src/main/scala/ml/wolfe/fg20/FG.scala
+++ wolfe-core/src/main/scala/ml/wolfe/fg20/FG.scala
@@ -1,33 +1,26 @@
-//package ml.wolfe.fg20
-//
-//import ml.wolfe.FactorieVector
-//
-//
-//class Node[VarType <: Var[_], NodeContent, Msgs](val variable:VarType, val content:NodeContent) {
-//  var edgesBuffer:List[Edge[Msgs]] = Nil
-//  var edges:Array[Edge[Msgs]] = null
-//
-//  def build(): Unit ={
-//    edges = edgesBuffer.toArray
-//  }
-//}
-//
-//class Factor[FactorContent](val pot:Potential, val content:FactorContent) {
-//  var discEdges: Array[Edge[Msgs]] = null
-//  var contEdges: Array[Edge[Msgs]] = null
-//  var vectEdges: Array[Edge[Msgs]] = null
-//}
-//type DiscreteFactor[FactorContent, Msgs] = Factor[FactorContent]
-//
-//class Edge[Msgs](val node:Node[_, _, Msgs], val factor:Factor[_], val msgs:Msgs)
-//
-//
-////trait FG {
-////  type NodeContent
-////  type Msgs
-////  type FactorContent
-////}
-//
-//class State
-//
-//case class MAPResult(state: State, score: Double, gradient: FactorieVector)
+package ml.wolfe.fg20
+
+import ml.wolfe.FactorieVector
+
+
+class Node[VarType <: Var[_], NodeContent, Msgs](val variable:VarType, val content:NodeContent) {
+  var edgesBuffer:List[Edge[Msgs]] = Nil
+  var edges:Array[Edge[Msgs]] = null
+
+  def build(): Unit ={
+    edges = edgesBuffer.toArray
+  }
+}
+
+class Factor[FactorContent, DiscMsgs, ContMsgs, VectMsgs](val pot:Potential, val content:FactorContent) {
+  var discEdges: Array[Edge[DiscMsgs]] = null
+  var contEdges: Array[Edge[ContMsgs]] = null
+  var vectEdges: Array[Edge[VectMsgs]] = null
+
+  def value = pot.value(this)
+}
+
+class Edge[Msgs](val node:Node[_, _, Msgs], val factor:Factor[_, _, _, _], val msgs:Msgs)////trait FG ////  type NodeConten////  type Msg////  type FactorConten////}
+
+class State(settings:Map[Var, Any])
+case class MAPResult(state: State, score: Double, gradient: FactorieVector)
