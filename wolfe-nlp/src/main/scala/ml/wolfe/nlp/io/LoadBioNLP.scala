420a8cc300b2ad9afe8719a1d5834f31e3fb5cfc
mbosnjak <matko.bosnjak@gmail.com>
2014-11-10 23:35:05 +0000

    Finished the BioNLP dataset loader

--- wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/LoadBioNLP.scala
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/LoadBioNLP.scala
@@ -9,6 +9,7 @@ import scala.util.parsing.json.JSON
 
 /**
  * @author rockt
+ * @author mbosnjak
  */
 object LoadBioNLP extends App {
   def apply(pathToBioNLP: String): Seq[Document] = {
@@ -25,6 +26,7 @@ object LoadBioNLP extends App {
 
   def jsonFileToWolfeDoc(file: File): Document = {
     val mapper = new ObjectMapper()
+    JSON.globalNumberParser = {x: String => Integer.parseInt(x)}
     val parsed = JSON.parseFull(scala.io.Source.fromFile(file).getLines().mkString("\n"))
 
     val MapExtractor(attrMap) = parsed.get
@@ -42,23 +44,38 @@ object LoadBioNLP extends App {
 
         val wolfeTokens = tokens.map(t => {
           val MapExtractor(token) = t
-          val charOffsets = CharOffsets(token("begin").toString.toDouble.toInt, token("end").toString.toDouble.toInt)
-          Token(token("word").toString, charOffsets, token("pos").toString)
+          val charOffsets = CharOffsets(token("begin").toString.toInt, token("end").toString.toInt)
+          // ACHTUNG!!! IT'S A STEM, NOT A LEMMA (Token case class takes a lemma, not a stem, but you can use it as a stem)
+          Token(token("word").toString, charOffsets, token("pos").toString, token("stem").toString)
         }).sortBy(_.offsets.start).toArray
 
         val wolfeMentions = mentions.map(m => {
           val MapExtractor(mention) = m
-          EntityMention(mention("label").toString, mention("begin").toString.toDouble.toInt, mention("end").toString.toDouble.toInt)
+          EntityMention(mention("label").toString, mention("begin").toString.toInt, mention("end").toString.toInt)
         })
 
-        //println(eventCandidates.mkString("\n"))
+        val wolfeDependencyTreeArcs = deps.map(d => {
+          val MapExtractor(dep) = d
+          (dep("mod").toString.toInt, dep("head").toString.toInt, dep("label").toString)
+        }).sortBy(_._1).toArray
 
-        //todo: add deps
-        //todo: add (candidate) events
+        val wolfeDependencyTree = new DependencyTree(wolfeTokens, wolfeDependencyTreeArcs)
 
-        Sentence(wolfeTokens, ie = new IEAnnotation(wolfeMentions, Nil, Nil))
+        val wolfeEventCandidates = eventCandidates.map(e => {
+          val MapExtractor(event) = e
+          val entityMention = EntityMention(event("gold").toString,
+                                            event("begin").toString.toInt,
+                                            event("end").toString.toInt)
+          val ListExtractor(args) = event("arguments")
+          val arguments = args.map(a => {
+            val MapExtractor(arg) = a
+            RoleMention("", EntityMention(arg("gold").toString, arg("begin").toString.toInt, arg("end").toString.toInt))
+          }).toArray
+          EventMention("", entityMention, arguments)
+        })
+
+        Sentence(wolfeTokens, syntax = new SyntaxAnnotation(ConstituentTree.empty, wolfeDependencyTree), ie = new IEAnnotation(wolfeMentions, Nil, wolfeEventCandidates))
       }
-
     Document(txt, wolfeSentences)
   }
 }
@@ -69,6 +86,7 @@ object LoadBioNLPTest extends App {
     override def accept(dir: File, name: String): Boolean = name.endsWith(".json")
   }).toList
 
+  println(jsonFiles.head)
   val doc = LoadBioNLP.jsonFileToWolfeDoc(jsonFiles.head)
   println(doc)
 }
