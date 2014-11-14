bfa411cf7f437d572d5c60d6e8591d4749ca5472
riedelcastro <sebastian.riedel@gmail.com>
2014-11-14 19:02:49 +0000

    Gibbs Sampler bugfixes

--- wolfe-core/src/main/scala/ml/wolfe/fg20/BeliefPropagation.scala
+++ wolfe-core/src/main/scala/ml/wolfe/fg20/BeliefPropagation.scala
@@ -162,7 +162,8 @@ class MaxProduct(val problem: Problem) extends BeliefPropagationFG with EdgeProp
     val score = factors.view.map(f => f.pot.maxMarginalExpectationsAndObjective(f, gradient, weights)).sum
     val discState = problem.discVars.map(v => v -> v.dom(var2DiscNode(v).setting)).toMap[Var[Any], Any]
     val contState = problem.contVars.map(v => v -> var2ContNode(v).setting)
-    val maxMarginals = var2DiscNode.values.map(n => DiscBelief(n.variable) -> Distribution.disc(n.variable.dom, n.content.belief))
+    val maxMarginals = var2DiscNode.values.map(n =>
+      DiscBelief(n.variable) -> Distribution.disc(n.variable.dom, n.content.belief.map(math.exp)))
     MAPResult(new MapBasedState(discState ++ contState), score, gradient, new MapBasedState(maxMarginals.toMap))
   }
 
@@ -203,7 +204,8 @@ class SumProduct(val problem: Problem) extends BeliefPropagationFG with EdgeProp
     for (node <- nodes) { updateBelief(node); normalizeBelief(node) }
     val gradient = new SparseVector(1000)
     val logZ = factors.view.map(f => f.pot.marginalExpectationsAndObjective(f, gradient, weights)).sum
-    val marginals = var2DiscNode.values.map(n => DiscBelief(n.variable) -> Distribution.disc(n.variable.dom, n.content.belief))
+    val marginals = var2DiscNode.values.map(n =>
+      DiscBelief(n.variable) -> Distribution.disc(n.variable.dom, n.content.belief.map(math.exp)))
     val stats = new SparseVector(1000)
     for (f <- statsFactors) f.pot.marginalExpectationsAndObjective(f,stats,new SparseVector(0))
     MarginalResult(logZ, gradient, new MapBasedState(marginals.toMap))
