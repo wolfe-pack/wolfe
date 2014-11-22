94850f28ca15aeae4e2f9c27722362ee2455ec71
riedelcastro <sebastian.riedel@gmail.com>
2014-11-22 11:29:34 +0000

    fixed null incoming current parameters (missing call to fg.build())

--- wolfe-neural/src/main/scala/ml/wolfe/neural/BackPropagation.scala
+++ wolfe-neural/src/main/scala/ml/wolfe/neural/BackPropagation.scala
@@ -20,6 +20,8 @@ class BackPropagationLoss(edge: Edge, network: MultiLayerPerceptron, input: Dens
   println(edge.n)
   println(edge.f)
 
+
+
   override def valueAndGradientForAllEdges(): Double = {
     println("f2n = " + e.f2n)
     println("n2f = " + e.n2f)
@@ -83,6 +85,8 @@ object BackPropTest extends App {
       case "LBFGS" => new LBFGS(Double.MaxValue, Int.MaxValue) //rockt: not working atm
       case "AvgPerceptron" => new AveragedPerceptron()
     }
+
+    fg.build()
 
     Timer.time("optimization") {
       GradientBasedOptimizer(fg,new OnlineTrainer(_, gradientOptimizer, bpIters, fg.factors.size - 1) with ProgressLogging
