e42937b9fe263025ac15cb2077d44b2c3a2854c3
narad <jason.narad@gmail.com>
2014-11-29 19:46:02 +0000

    Neural Net refactoring. / OO Activations and Losses

--- wolfe-neural/src/main/scala/ml/wolfe/neural/NeuralNetwork.scala
+++ wolfe-neural/src/main/scala/ml/wolfe/neural/NeuralNetwork.scala
@@ -1,19 +1,36 @@
 package ml.wolfe.neural
 
 import breeze.linalg.{DenseVector, DenseMatrix}
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by narad on 11/24/14.
  */
-class NeuralNetwork(val layers: Seq[NetworkLayer]) {
-  require(layers.head.isInstanceOf[HiddenLayer], "Input layer must be of type HiddenLayer (for now.)")
-  require(layers.last.isInstanceOf[OutputLayer], "Output layer must be of type OutputLayer.")
+class NeuralNetwork { // (val layers: Seq[NetworkLayer]) {
+  val layers = new ArrayBuffer[NetworkLayer]
 
-  for (i <- 0 until layers.size - 2) {
-    val slice = layers.view(i, i + 1)
-    println("Slice = " + slice.mkString(", "))
-    require(slice(0).outputSize == slice(1).inputSize, "Layers %d and %d don't agree in output - input sizes".format(i, i+1))
+  def addHiddenLayer(w: DenseMatrix[Double], b: DenseVector[Double], a: ActivationFunction) = {
+    layers += new HiddenLayer(w, b, a, (layers.size, this))
   }
+
+  def addOutputLayer(w: DenseMatrix[Double], b: DenseVector[Double], a: ActivationFunction) = {
+    layers += new OutputLayer(w, b, a, (layers.size, this))
+  }
+
+  //   nn.addHiddenLayer(w1, b1, new SigmoidActivationFunction)
+//  nn.addOutputLayer(w2, b2, new TanhActivationFunction)
+
+
+
+
+  //  require(layers.head.isInstanceOf[HiddenLayer], "Input layer must be of type HiddenLayer (for now.)")
+//  require(layers.last.isInstanceOf[OutputLayer], "Output layer must be of type OutputLayer.")
+//
+//  for (i <- 0 until layers.size - 2) {
+//    val slice = layers.view(i, i + 1)
+//    println("Slice = " + slice.mkString(", "))
+//    require(slice(0).outputSize == slice(1).inputSize, "Layers %d and %d don't agree in output - input sizes".format(i, i+1))
+//  }
 
   def paramSize: Int = layers.foldLeft(0){ case(sum, l) => sum + l.W.rows * l.W.cols }
 
@@ -21,7 +38,7 @@ class NeuralNetwork(val layers: Seq[NetworkLayer]) {
     layers.map(_.grads.toDenseVector.toArray).flatten
   }
 
-  def backprop(input: DenseMatrix[Double], outputs: DenseVector[Double], updateWeights: Boolean = true, iters: Int = 1, rate: Double = 0.9, verbose: Boolean = false) = {
+  def backprop(input: DenseMatrix[Double], outputs: DenseVector[Double], loss: NeuralLossFunction, updateWeights: Boolean = true, iters: Int = 1, rate: Double = 0.9, verbose: Boolean = false) = {
     if (verbose) println("Performing backprop...")
     for (i <- 1 to iters) {
       if (verbose) println("Forward pass...")
@@ -34,15 +51,16 @@ class NeuralNetwork(val layers: Seq[NetworkLayer]) {
       if (verbose) println("Backward pass...")
       var louts = outputs.toDenseMatrix
       var lastw = null.asInstanceOf[DenseMatrix[Double]]
-      for (layer <- layers.reverse) {
-        layer match {
-          case x: OutputLayer => {
-            louts = x.gradient(louts, verbose=verbose)
-            lastw = x.W
-          }
-          case y: HiddenLayer => y.gradient(louts, lastw, verbose=verbose)
-        }
-      }
+      for (layer <- layers.reverse) louts = layer.gradient(louts, loss, verbose=verbose)
+//      for (layer <- layers.reverse) {
+//        layer match {
+//          case x: OutputLayer => {
+//            louts = x.gradient(louts, verbose=verbose)
+//            lastw = x.W
+//          }
+//          case y: HiddenLayer => y.gradient(louts, lastw, verbose=verbose)
+//        }
+//      }
       if (verbose) println("Updating network weights...")
       if (updateWeights) {
         for (layer <- layers) layer.updateWithCachedGradients(eta = rate, verbose=verbose)
