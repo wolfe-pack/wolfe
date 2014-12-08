b8f98224b090bf13f545b1a576cd28c118eec2fe
riedelcastro <sebastian.riedel@gmail.com>
2014-12-08 16:03:46 -0500

    Improved fg20 to yield an almost 1:1 correspondence to high level wolfe, but without the AST conversions.

--- wolfe-core/src/test/scala/ml/wolfe/fg20/GradientBasedOptimizerSpecs.scala
+++ wolfe-core/src/test/scala/ml/wolfe/fg20/GradientBasedOptimizerSpecs.scala
@@ -12,7 +12,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
 
   class LinearTerm(val x: ContVar, val scale: Double) extends ContPotential with StatelessDifferentiable with StatelessScorer {
     def contVars = Array(x)
-    def gradientAndValue(current: Setting, gradient: Setting) = {
+    def gradientAndValue(current: PartialSetting, gradient: Setting) = {
       gradient.cont(0) = scale
       score(current)
     }
@@ -21,7 +21,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
 
   class QuadraticTerm(val x: ContVar, val scale: Double) extends ContPotential with StatelessDifferentiable with StatelessScorer  {
     val contVars = Array(x)
-    def gradientAndValue(current: Setting, gradient: Setting) = {
+    def gradientAndValue(current: PartialSetting, gradient: Setting) = {
       gradient.cont(0) = current.cont(0) * 2.0 * scale
       score(current)
     }
@@ -31,7 +31,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
   class MultivariateLinearTerm(val x: VectVar, val scale: FactorieVector) extends VectPotential
                                                                                   with StatelessDifferentiable with StatelessScorer  {
     val vectVars = Array(x)
-    def gradientAndValue(current: Setting, gradient: Setting) = {
+    def gradientAndValue(current: PartialSetting, gradient: Setting) = {
       gradient.vect(0) = scale
       score(current)
     }
@@ -41,7 +41,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
   class MultivariateQuadraticTerm(val x: VectVar, val scale: Double) extends VectPotential
                                                                              with StatelessDifferentiable with StatelessScorer  {
     val vectVars = Array(x)
-    def gradientAndValue(current: Setting, gradient: Setting) = {
+    def gradientAndValue(current: PartialSetting, gradient: Setting) = {
       gradient.vect(0) = current.vect(0) * scale * 2.0
       score(current)
     }
@@ -55,7 +55,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
       val x = new ContVar("x")
       val problem = Problem(Seq(new QuadraticTerm(x, -1.0), new LinearTerm(x, 4.0)))
       val optimizer = new GradientBasedOptimizer(problem)
-      val result = optimizer.argmax(w => new OnlineTrainer(w, new AdaGrad(), 1000))
+      val result = optimizer.gradientBasedArgmax(w => new OnlineTrainer(w, new AdaGrad(), 1000))
       result.state(x) should be(2.0 +- 0.01)
     }
 
@@ -66,7 +66,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
         new MultivariateQuadraticTerm(x, -1.0))
       )
       val optimizer = new GradientBasedOptimizer(problem)
-      val result = optimizer.argmax(w => new OnlineTrainer(w, new AdaGrad(), 10000))
+      val result = optimizer.gradientBasedArgmax(w => new OnlineTrainer(w, new AdaGrad(), 10000))
       result.state(x)(0) should be(2.0 +- 0.01)
       result.state(x)(1) should be(0.0 +- 0.01)
     }
@@ -77,7 +77,7 @@ class GradientBasedOptimizerSpecs extends WolfeSpec {
         new MultivariateQuadraticTerm(x, -1.0))
       )
       val optimizer = new GradientBasedOptimizer(problem)
-      val result = optimizer.argmax(w => new BatchTrainer(w, new AdaGrad(), 100))
+      val result = optimizer.gradientBasedArgmax(w => new BatchTrainer(w, new AdaGrad(), 100))
       result.state(x)(0) should be(2.0 +- 0.01)
       result.state(x)(1) should be(0.0 +- 0.01)
     }
