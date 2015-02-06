2d25f2c4f0db2155577e425a3848849da1c674de
rockt <tim.rocktaeschel@gmail.com>
2015-02-06 19:01:52 +0000

    cleanup

--- wolfe-core/src/main/scala/ml/wolfe/term/playground/MatrixFactorization.scala
+++ wolfe-core/src/main/scala/ml/wolfe/term/playground/MatrixFactorization.scala
@@ -55,64 +55,6 @@ object MatrixFactorization extends App {
   })
 }
 
-object NeuralTensorFactorization extends App {
-  //tensor
-  val pos = Seq(
-    (0 -> 0) -> 0, (0 -> 0) -> 1,
-    (0 -> 1) -> 0,
-    (1 -> 1) -> 1
-  )
-
-  val neg = Seq(
-    (0 -> 1) -> 1,
-    (1 -> 1) -> 0
-  )
-
-  val k = 10
-  val num_c1 = (pos ++ neg).map(_._1._1).max + 1
-  val num_c2 = (pos ++ neg).map(_._1._2).max + 1
-  val num_r =  (pos ++ neg).map(_._2).max + 1 
-
-  val Theta = seqs(vectors(k), num_c1) x seqs(vectors(k), num_c2) x seqs(vectors(k), num_r) x matrices(k, 2 * k)
-
-  val c1Reg: DoubleTerm = -0.01
-  val c2Reg: DoubleTerm = -0.01
-  val rReg: DoubleTerm =  -0.01
-  val WCReg: DoubleTerm = -0.01
-
-  def cell(scale: Double)(r: VectorTerm, c1: VectorTerm, c2: VectorTerm, WC: MatrixTerm) = sum(
-    log(sigm((r dot (WC * (c1 cons c2))) * scale)),
-    rReg * (r dot r), 
-    c1Reg * (c1 dot c1), 
-    c2Reg * (c2 dot c2), 
-    WCReg * (WC dot WC)
-  )
-
-  def loss(positive: Seq[((Int, Int), Int)], negative: Seq[((Int, Int), Int)])(t: Theta.Term) = {
-    val w: MatrixTerm =  t._2
-    sum(positive) { case ((i, j), s) => cell(1.0)(t._1._2(s), t._1._1._1(i), t._1._1._2(j), w)} +
-      sum(negative) { case ((i, j), s) => cell(-1.0)(t._1._2(s), t._1._1._1(i), t._1._1._2(j), w)}
-  }
-
-  val random = new Random(1984l)
-  def nextDouble = random.nextGaussian() * 0.1
-  val init = Array(Theta.createZeroSetting())
-  init.head.vect.foreach(x => (0 until x.length).foreach(i => x.update(i, nextDouble)))
-  init.head.mats.foreach(x => (0 until x.length).foreach(i => x.update(i, nextDouble)))
-  
-  val argmaxTerm = argmax(Theta) { x => loss(pos, neg)(x).argmaxBy(Argmaxer.ascent(1000, 0.1)(init)) }
-  val (((c1s, c2s), rs), w) = argmaxTerm.eval()
-
-  def cons(v1: FactorieVector, v2: FactorieVector) =
-    new DenseTensor1((v1.asSeq ++ v2.asSeq).toArray)
-
-  (pos ++ neg).foreach(t => {
-    val ((c1Ix, c2Ix), rIx) = t
-    val (c1, c2, r) = (c1s(c1Ix), c2s(c2Ix), rs(rIx))
-    println(s"($c1Ix, $c2Ix) -> $rIx\t" + "%5.2f\t".format(sigmoid(r dot (w * cons(c1, c2)))))
-  })
-}
-
 object MatrixFactorizationCheck extends App {
   val k = 10
 
@@ -125,30 +67,6 @@ object MatrixFactorizationCheck extends App {
   val scale: DoubleTerm = -1.0
 
   GradientChecking(sum(log(sigm((a dot v) * scale)), rowReg * (a dot a), colReg * (v dot v)), debug = true)()
-}
-
-object NeuralTensorFactorizationCheck extends App {
-  val k = 5
-
-  val r = vectors(k).variable("r ")
-  val c1 = vectors(k).variable("c1 ")
-  val c2 = vectors(k).variable("c2 ")
-  val WC = matrices(k, 2*k).variable("WC ")
-
-  val rReg: DoubleTerm =  -0.01
-  val c1Reg: DoubleTerm = -0.01
-  val c2Reg: DoubleTerm = -0.01
-  val WCReg: DoubleTerm = -0.01
-
-  val scale: DoubleTerm = -1.0
-
-  GradientChecking(sum(
-    log(sigm((r dot (WC * (c1 cons c2))) * scale)),
-    rReg * (r dot r),
-    c1Reg * (c1 dot c1),
-    c2Reg * (c2 dot c2),
-    WCReg * (WC dot WC)
-  ), debug = true)()
 }
 
 object Scratch extends App {
