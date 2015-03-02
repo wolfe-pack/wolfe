325ccb99598ea5fc97e38e8ac6bd299e6263fbdf
Sameer Singh <sameeersingh@gmail.com>
2015-03-01 22:08:29 -0800

    update htmlgen dependency

--- project/Build.scala
+++ project/Build.scala
@@ -91,7 +91,7 @@ object BuildSettings {
 //    "org.scalamacros" %% "quasiquotes" % "2.0.0",
     "org.scalanlp" %% "breeze" % "0.8.1",
     "org.scalanlp" %% "breeze-natives" % "0.8.1",
-    "org.sameersingh.htmlgen" % "htmlgen" % "0.1.1"
+    "org.sameersingh.htmlgen" % "htmlgen" % "0.2"
     //    "org.scalanlp" %% "breeze-math" % "0.3",
     //    "org.scalanlp" %% "breeze-learn" % "0.3",
     //    "org.scalanlp" %% "breeze-process" % "0.3",
