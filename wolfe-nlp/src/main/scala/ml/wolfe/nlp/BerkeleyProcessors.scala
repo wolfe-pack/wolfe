dc7a04bfb898839e593970765bc226f5a33e7bdb
Matko Bosnjak <matko.bosnjak@gmail.com>
2015-03-07 15:31:22 +0000

    Berkeley parsing operational

--- wolfe-nlp/src/main/scala/ml/wolfe/nlp/BerkeleyProcessors.scala
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/BerkeleyProcessors.scala
@@ -1,55 +1,115 @@
 package ml.wolfe.nlp
 
-import edu.berkeley.nlp.entity.EntitySystem
+import edu.berkeley.nlp.PCFGLA.CoarseToFineMaxRuleParser
+import edu.berkeley.nlp.entity._
+import edu.berkeley.nlp.entity.coref._
+import edu.berkeley.nlp.entity.ner.NerSystemLabeled
+import edu.berkeley.nlp.entity.preprocess.{SentenceSplitter => BerkeleySentenceSplitter, PreprocessingDriver}
+import edu.berkeley.nlp.entity.preprocess.PreprocessingDriver.loadParser
+import edu.berkeley.nlp.futile.fig.basic.IOUtils
+import edu.berkeley.nlp.syntax.Tree
+import edu.berkeley.nlp.util.{Logger}
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
 
 /**
- * Created by narad on 1/9/15.
+ * @author mbosnjak
+ * @author narad
  */
 object BerkeleyProcessors {
-  val testPath = ""
-  val testSize = 10
-  val modelPath = "/Users/narad/Downloads/models/corefner-onto.ser.gz"
+  // Path to read the sentence splitter model from
+  val sentenceSplitterModelPath: String = "wolfe-nlp/models/sentsplit.txt.gz"
+  // Path to read the Berkeley Parser grammar from
+  val grammarPath: String = "wolfe-nlp/models/eng_sm6.gr"
+  // Path to read a backoff grammar from
+  val backoffGrammarPath: String = "wolfe-nlp/models/eng_sm1.gr"
+  // Path to read the NER model from
+  val nerModelPath: String = ""
 
-//  public static enum Mode {
-//    PREDICT, PREDICT_EVALUATE, TRAIN_EVALUATE,
-//    PREDICT_ACE, PREDICT_EVALUATE_ACE, TRAIN_EVALUATE_ACE,
-//    COREF_TRAIN, COREF_PREDICT, COREF_TRAIN_EVALUATE, COREF_TRAIN_PREDICT,
-//    MAKE_MASK_MODELS;
+  Logger.logss("Loading sentence splitter")
+  val splitter: BerkeleySentenceSplitter = BerkeleySentenceSplitter.loadSentenceSplitter(sentenceSplitterModelPath)
+  Logger.logss("Loading parser")
+  val parser: CoarseToFineMaxRuleParser = loadParser(grammarPath)
+  Logger.logss("Loading backoff parser")
+  val backoffParser: CoarseToFineMaxRuleParser = loadParser(backoffGrammarPath)
+  //Logger.logss("Loading NER system")
+  val nerSystem: NerSystemLabeled = if (nerModelPath.isEmpty()) null else NerSystemLabeled.loadNerSystem(nerModelPath)
+
+
+
+//  // Skip sentence splitting entirely.
+//  val skipSentenceSplitting: Boolean = false
+//  // Respect line breaks for sentence segmentation (i.e. a new line always means a new sentence). False by default.
+//  val respectInputLineBreaks: Boolean = false
+//  // Respect two consecutive line breaks for sentence segmentation (i.e. a blank line always means a new sentence). True by default.
+//  val respectInputTwoLineBreaks: Boolean = true
+//  // Use an alternate tokenizer that may respect the original input a little bit more.
+//  val useAlternateTokenizer: Boolean = false
+
+
+
+
+  // Path to read/write the model
+  val modelPath: String = "wolfe-nlp/models/coref-onto.ser.gz"
+
+  def toWolfeConstituentTree(tree: Tree[String]): ConstituentTree = {
+    if (!tree.isEmpty)
+      treeToTree(tree)
+    else
+      ConstituentTree.empty
+  }
+
+  def treeToTree(tree: Tree[String]): ConstituentTree = {
+    if (tree.isPreTerminal)
+      new ConstituentTree(new PreterminalNode(label = tree.getLabel, word = tree.getChildren.head.getLabel))
+    else
+      new ConstituentTree(new NonterminalNode(label = tree.getLabel, head = -1), tree.getChildren.map(treeToTree(_)).toList)
+  }
+
+
+
+////  what it looks like when berkeley parser parses...without memorizing offsets :S
+//  def BerkeleyPreprocessing(text: String) = {
+//    val lines = Array(text)
+//    val canonicalizedParagraphs: Array[String] = splitter.formCanonicalizedParagraphs(lines, respectInputLineBreaks, respectInputTwoLineBreaks)
+//
+//    val sentences =
+//      if (skipSentenceSplitting)
+//        canonicalizedParagraphs
+//      else
+//        splitter.splitSentences(canonicalizedParagraphs)
+//
+//    val tokenizedSentences: Array[Array[String]] =
+//      if (useAlternateTokenizer)
+//        BerkeleySentenceSplitter.tokenizeAlternate(sentences)
+//      else
+//        BerkeleySentenceSplitter.tokenize(sentences)
+//
+//    tokenizedSentences
 //  }
 
-  def annotate(text: String, coref: Boolean = true): Document = {
-    EntitySystem.runOntoPredict(testPath, testSize, modelPath)
-    ???
+
+
+  def mkParsedDocument(text: String, coref: Boolean = true) = {
+    val result = SISTAProcessors.mkDocument(text)
+    val updatedSentences = result.sentences.map{sentence =>
+      val tokenizedSentenceArray = sentence.tokens.map(token => token.word)
+      val ctree = PreprocessingDriver.parse(parser, backoffParser, tokenizedSentenceArray.toList)
+      sentence.copy(syntax = new SyntaxAnnotation(tree = toWolfeConstituentTree(ctree), dependencies = null))
+    }
+    Document(text, updatedSentences)
   }
+
+
+  def annotate(text: String): Document = {
+    mkParsedDocument(text)
+  }
+
 }
 
 
 object BerkeleyProcessorTest extends App {
-
-  BerkeleyProcessors.annotate("Jim said he is going to the store.")
+  val doc = BerkeleyProcessors.annotate("Jim said he is going to the store. He lied. He will end up in hell for lying.")
+  println(doc)
 }
-
-//    val mode =
-//    if (mode == Mode.PREDICT) {
-//      EntitySystem.runOntoPredict(testPath, testSize, modelPath);
-//    } else if (mode == Mode.PREDICT_EVALUATE) {
-//      EntitySystem.runOntoPredictEvaluate(testPath, testSize, modelPath);
-//    } else if (mode == Mode.TRAIN_EVALUATE) {
-//      EntitySystem.runTrainEvaluate(trainPath, trainSize, testPath, testSize);
-//    } else if (mode == Mode.PREDICT_ACE) {
-//      EntitySystem.runACEPredict(testPath, testSize, modelPath);
-//    } else if (mode == Mode.PREDICT_EVALUATE_ACE) {
-//      EntitySystem.runACEPredictEvaluate(testPath, testSize, modelPath);
-//    } else if (mode == Mode.TRAIN_EVALUATE_ACE) {
-//      EntitySystem.runTrainEvaluateACE(trainPath, trainSize, testPath, testSize);
-//    } else if (mode == Mode.COREF_PREDICT) {
-//      CorefSystem.runPredictWriteOutput(testPath, testSize, modelPath, outputPath, doConllPostprocessing);
-//    } else if (mode == Mode.COREF_TRAIN_EVALUATE) {
-//      CorefSystem.runTrainEvaluate(trainPath, trainSize, testPath, testSize, modelPath);
-//    } else if (mode == Mode.COREF_TRAIN_PREDICT) {
-//      CorefSystem.runTrainPredict(trainPath, trainSize, testPath, testSize, modelPath, outputPath, doConllPostprocessing);
-//    } else if (mode == Mode.MAKE_MASK_MODELS) {
-//      CorefPrunerJavaHack.trainAndSaveKFoldModels(trainPath, trainSize, maskNumFolds, maskOutPath);
-//    } else {
-//      throw new RuntimeException("Unknown mode: " + mode);
-//    }
