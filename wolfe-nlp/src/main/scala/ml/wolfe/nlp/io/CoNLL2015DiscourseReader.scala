0c4a97b69d79bb46866dcd821230ac876e92c5b4
Matko Bosnjak <matko.bosnjak@gmail.com>
2015-03-17 22:41:56 +0000

    Added CoNLL2015DiscourseReader

---
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/CoNLL2015DiscourseReader.scala
@@ -0,0 +1,108 @@
+package ml.wolfe.nlp.io
+
+import ml.wolfe.nlp._
+import org.json4s.JsonAST.{JInt, JObject, JString}
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+import scala.io.Source
+
+/**
+ * @author matko
+ */
+
+
+object CoNLL2015DiscourseReader {
+
+  implicit val formats = DefaultFormats
+
+  case class JSONDocument(sentences: List[JSONSentence])
+  case class JSONSentence(dependencies: List[List[String]], parsetree: String, words: List[List[JValue]])
+  case class JSONDiscourseRelation(Arg1: JSONDiscourseArgument, Arg2: JSONDiscourseArgument, Connective: JSONDiscourseArgument, DocID: String, ID: Int, Sense: List[String], Type: String)
+  case class JSONDiscourseArgument(CharacterSpanList: List[List[Int]] , RawText: String, TokenList: List[List[Int]])
+
+  // load relations and group them by documentID
+  def loadDiscourseRelations(filename: String): Map[String, List[JSONDiscourseRelation]] = {
+    val lines_data = Source.fromFile(filename).getLines()
+    val data = lines_data.map{ line_data =>
+      val json = parse(line_data)
+      json.extract[JSONDiscourseRelation]
+    }.toList
+    data.groupBy(_.DocID)
+  }
+
+
+
+  def loadData(dataDirectory: String): Iterable[Document] = {
+    val doc_relations = loadDiscourseRelations(dataDirectory + "pdtb-data.json")
+    val lines_parses = Source.fromFile(dataDirectory + "pdtb-parses.json").getLines()
+
+    val content = lines_parses.mkString
+    val json_parses = parse(content)
+    val parses = json_parses.extract[Map[String, JSONDocument]]
+
+
+    val documents = parses.map { document =>
+      val docID = document._1
+
+      val sentences = document._2.sentences.map { sentence =>
+
+        val tokens = sentence.words.map { word =>
+          val JString(token) = word(0)
+          val JObject(properties) = word(1)
+          val JInt(offset_start) = properties(0)._2
+          val JInt(offset_end) = properties(1)._2
+          // LINKERS NOT USED
+          //val JArray(linkers) = properties(2)._2
+          val JString(posTag) = properties(3)._2
+          val offsets = CharOffsets(offset_start.toInt, offset_end.toInt)
+          Token(token, offsets, posTag)
+        }.toIndexedSeq
+
+        val arcs = sentence.dependencies.map { dependency =>
+          val label = dependency(0)
+          val child = dependency(1).splitAt(dependency(1).lastIndexOf("-")+1)._2.toInt - 1
+          val head = dependency(2).splitAt(dependency(2).lastIndexOf("-")+1)._2.toInt - 1
+          (child, head, label)
+        }.filterNot(_._3 == "root").toSeq
+
+        val parse_tree = "(ROOT" + sentence.parsetree.drop(1).dropRight(2) + ")"
+        val cons_tree = ConstituentTree.stringToTree(parse_tree)
+        val dep_tree = DependencyTree(tokens, arcs)
+
+        Sentence(tokens, new SyntaxAnnotation(cons_tree, dep_tree))
+      }
+
+      val filename = dataDirectory + "raw/" + docID
+      val text = Source.fromFile(filename).getLines().mkString("\n")
+
+      val discourse = doc_relations.get(docID) match {
+        case Some(relations) =>
+          val rels = relations.map{ relation =>
+            def toWolfeDiscourseArgument(x: JSONDiscourseArgument): DiscourseArgument = {
+              val tokens = x.TokenList.map(tok => (tok(3), tok(4))).toSeq
+              val offsets = x.CharacterSpanList.map(span => CharOffsets(span(0), span(1)))
+              DiscourseArgument(x.RawText, offsets, tokens)
+            }
+            val arg1 = toWolfeDiscourseArgument(relation.Arg1)
+            val arg2 = toWolfeDiscourseArgument(relation.Arg2)
+            val connective = toWolfeDiscourseArgument(relation.Connective)
+            DiscourseRelation(arg1, arg2, connective, relation.ID.toString, relation.Sense, relation.Type)
+          }.toSeq
+          DiscourseAnnotation(rels)
+        case None => DiscourseAnnotation.empty
+      }
+      Document(text, sentences.toIndexedSeq, filename = Some(filename), discourse = discourse)
+    }
+    documents
+  }
+
+
+}
+
+
+object CoNLL2015Test extends App {
+  println("here")
+  CoNLL2015DiscourseReader.loadData("data/conll15st-train-dev/conll15st_data/conll15-st-03-04-15-train/")
+  println("done")
+}
