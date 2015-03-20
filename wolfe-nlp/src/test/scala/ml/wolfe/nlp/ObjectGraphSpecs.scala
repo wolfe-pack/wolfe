0c1e79c1b33e51ab7c03619084a78917484871b4
watercrossing <ingolf.becker@googlemail.com>
2015-03-20 11:08:31 +0000

    Fully working ObjectGraphSpecs

--- wolfe-nlp/src/test/scala/ml/wolfe/nlp/ObjectGraphSpecs.scala
+++ wolfe-nlp/src/test/scala/ml/wolfe/nlp/ObjectGraphSpecs.scala
@@ -40,46 +40,46 @@ class ObjectGraphSpecs extends WolfeSpec {
     }
 
     "allow implicit access to parent" in {
-      val graph = new SimpleObjectGraph[ParentChildRelation]
+      implicit val graph = new SimpleObjectGraph[ParentChildRelation]
       val child = testChild("Test Child")
       val parent = testParent("Father")
       graph.link1to1(parent, child)
       println(graph)
-      child.getParent(graph) should be (parent)
+      child.getParent should be (parent)
     }
-//
-//    "should choose the correct implicits" in {
-//      val graph3 = new SimpleObjectGraph[ParentChildRelation]
-//      val graph = new SimpleObjectGraph[FicitonalRelation]
-//      val graph2 = new SimpleObjectGraph[ParentChildRelation]
-//      val child = testChild("Test Child")
-//      val parent = testParent("Father")
-//      graph.link1to1(parent, child)
-//      child.getFictionalParent should be (parent)
-//    }
-//
-//    "link one parent to all children" in {
-//      val graph = new SimpleObjectGraph[ParentChildRelation]
-//      val children = for (i <- 1 to 10) yield testChild(i.toString)
-//      val parent = testParent("Father")
-//      graph.link1toN( parent,children)
-//      children.forall(graph.receive(_) == parent) should be (true)
-//    }
-//
-//
-//
-//    "distinguish different types of links" in {
-//      val graph = new SimpleObjectGraph[ParentChildRelation]
-//      val graph2 = new SimpleObjectGraph[ParentChildRelation]
-//      val child = testChild("Test Child")
-//      val parent = testParent("Father")
-//      val parent2 = testParent("GodFather")
-//      graph.link1to1(parent, child)
-//      graph2.link1to1(parent2, child)
-//      println(child.getParent)
-//      println(child.getFictionalParent)
-//      child.getParent should not be child.getFictionalParent
-//    }
+
+    "should choose the correct implicits" in {
+      implicit val graph3 = new SimpleObjectGraph[ParentChildRelation]
+      implicit val graph = new SimpleObjectGraph[FicitonalRelation]
+      implicit val graph2 = new SimpleObjectGraph[ParentChildRelation]
+      val child = testChild("Test Child")
+      val parent = testParent("Father")
+      graph.link1to1(parent, child)
+      child.getFictionalParent should be (parent)
+    }
+
+    "link one parent to all children" in {
+      val graph = new SimpleObjectGraph[ParentChildRelation]
+      val children = for (i <- 1 to 10) yield testChild(i.toString)
+      val parent = testParent("Father")
+      graph.link1toN( parent,children)
+      children.forall(graph.receive(_) == parent) should be (true)
+    }
+
+
+
+    "distinguish different types of links" in {
+      implicit val graph = new SimpleObjectGraph[ParentChildRelation]
+      implicit val graph2 = new SimpleObjectGraph[FicitonalRelation]
+      val child = testChild("Test Child")
+      val parent = testParent("Father")
+      val parent2 = testParent("GodFather")
+      graph.link1to1(parent, child)
+      graph2.link1to1(parent2, child)
+      println(child.getParent)
+      println(child.getFictionalParent)
+      child.getParent should not be child.getFictionalParent
+    }
 
   }
 
