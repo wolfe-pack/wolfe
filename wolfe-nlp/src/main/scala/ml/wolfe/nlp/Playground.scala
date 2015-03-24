5d17be0fb27abe03864890ec6cbb87e4aa26f7b2
riedelcastro <sebastian.riedel@gmail.com>
2015-03-24 08:15:28 +0000

    integrating wolfe changes

---
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/Playground.scala
@@ -0,0 +1,42 @@
+package ml.wolfe.nlp
+
+/**
+ * @author riedel
+ */
+object Playground extends App {
+
+
+//  def parse()
+}
+
+//trait Sent {
+//  val env:Env
+//  def tokens:IndexedSeq[env.Token]
+//}
+//
+//trait Tok {
+//  def word:String
+//}
+//
+////case class BasicTok(word:String)
+//case class BasicSent[T<:Tok](tokens:IndexedSeq[T]) extends Sent
+//case class BasicTok(word:String) extends Tok
+//case class BasicDoc[S<:Sent](sentences:IndexedSeq[S]) extends Doc
+//
+//trait Doc {
+//  val env:Env
+//  def sentences:IndexedSeq[env.Sentence]
+//}
+//
+//trait Env {
+//  type Sentence <: Sent
+//  type Token <: Tok
+//  type Document <: Doc
+//}
+//
+//object BasicEnv extends Env {
+//  type Sentence = BasicSent[Token]
+//  type Document = BasicDoc[Sentence]
+//  type Token = BasicTok
+//}
+//
