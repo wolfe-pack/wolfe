68619c3e0787448e584c1a9df06317594e23fb1d
narad <jason.narad@gmail.com>
2015-04-21 02:08:38 +0100

    FreebaseReader command line calls.

--- wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/FreebaseReader.scala
+++ wolfe-nlp/src/main/scala/ml/wolfe/nlp/io/FreebaseReader.scala
@@ -129,6 +129,11 @@ class FreebaseReader(collection: MongoCollection) {
   }
 
   def rankByShare(mids: Seq[String]): Map[String, Int] = {
+    for (m <- mids) {
+      println(m + ":")
+      parentsOf(m).map(p => "\t" + p._1 + " --> " + p._2)
+    }
+    println("-------")
     val allParents = mids.map(parentsOf(_)).toArray.flatten
     println(allParents.sortBy(_.toString).mkString("\n"))
     allParents.groupBy(_._2).map(p => p._1 -> p._2.size)
