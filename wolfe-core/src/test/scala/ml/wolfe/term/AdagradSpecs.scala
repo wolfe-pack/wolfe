6b774cdbfec1344737b5df180368a770bc40ce24
rockt <tim.rocktaeschel@gmail.com>
2015-04-25 17:46:15 +0100

    added adagrad spec for sampled quadratic objective terms

--- wolfe-core/src/test/scala/ml/wolfe/term/AdagradSpecs.scala
+++ wolfe-core/src/test/scala/ml/wolfe/term/AdagradSpecs.scala
@@ -1,7 +1,8 @@
 package ml.wolfe.term
 
-import ml.wolfe.WolfeSpec
+import ml.wolfe._
 import Argmaxer._
+import ml.wolfe.util.Math._
 
 /**
  * @author riedel
@@ -24,6 +25,27 @@ class AdagradSpecs extends WolfeSpec {
       def obj(x: X.Term) = (x dot vector(4.0, 4.0)) - (x dot x)
       val result = argmax(X) { x => obj(x) } by adaGrad
       result.eval() should equal(vector(2.0, 2.0))
+    }
+    "optimize a sampled quadratic objective" in {
+      import TermImplicits.Seqs
+      implicit val rand = ml.wolfe.util.Math.random
+
+      @domain case class Theta(params: IndexedSeq[Double])
+      implicit val Thetas = Theta.Values(Seqs(Doubles, 3))
+
+      @domain case class User(items: IndexedSeq[Int])
+      implicit val Items = Ints(0 until 4)
+      implicit val Users = User.Values(Seqs(Items, 0, 2))
+      val users = Seq(User(IndexedSeq()), User(IndexedSeq(1, 2))).toConst
+
+      def loss(t: Thetas.Term): DoubleTerm = {
+        val user = mem(users.sampleShuffled)
+        sum(user.items) { ix => { val x = t.params(ix); x * 4.0 - x * x }}
+      }
+
+      val thetaStar = (argmax(Thetas) { t => loss(t) } by adaGrad).eval()
+      thetaStar.params(1) should be(2.0 +- eps)
+      thetaStar.params(2) should be(2.0 +- eps)
     }
   }
 
