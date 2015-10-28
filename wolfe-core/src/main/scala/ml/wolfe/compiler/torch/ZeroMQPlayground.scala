fb665ee019f6544c552cfe082cd57af0afe51dc7
riedelcastro <sebastian.riedel@gmail.com>
2015-10-28 01:44:43 +0000

    Torch Client using Dynamic

--- wolfe-core/src/main/scala/ml/wolfe/compiler/torch/ZeroMQPlayground.scala
+++ wolfe-core/src/main/scala/ml/wolfe/compiler/torch/ZeroMQPlayground.scala
@@ -1,31 +1,61 @@
 package ml.wolfe.compiler.torch
 
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.json4s.jackson.JsonMethods._
 import org.zeromq.ZMQ
+import scala.language.dynamics
 
 /**
  * @author riedel
  */
 object ZeroMQPlayground {
 
+  def main(args: Array[String]) {
+
+    val client = new TorchZeroMQClient()
+
+    val test = client.call("torch.Tensor")(2)
+    println(test)
+
+    val test2 = client.global.torch.Tensor(2)
+
+    println(test2)
+
+  }
+
+}
+
+class TorchZeroMQClient(port:Int = 7000) extends LazyLogging {
 
   import org.json4s._
   import org.json4s.JsonDSL._
   import org.json4s.jackson.JsonMethods._
 
-  def main(args: Array[String]) {
-    val context = ZMQ.context(1)
+  val context = ZMQ.context(1)
+  logger.info("Connecting to Torch Server")
 
-    println("Connecting to Torch Server")
+  val socket = context.socket(ZMQ.REQ)
+  socket.connect(s"tcp://localhost:$port")
 
-    val socket = context.socket(ZMQ.REQ)
 
-    socket.connect("tcp://localhost:7000")
+  def toJson(value:Any) = {
+    value match {
+      case i:Int => JInt(i)
+      case d:Double => JDouble(d)
+      case _ => JString(value.toString)
+    }
+  }
 
+  def call(function:String)(args:Any*):Any = {
+    call(function.split("\\.").toList)(args.toList)
+  }
+
+  def call(function:List[String])(args:List[Any]):Any = {
     val json =
       ("cmd" -> "call") ~
         ("msg" -> (
-          ("func" -> List("torch", "Tensor")) ~
-            ("args" -> 3)
+          ("func" -> function) ~
+            ("args" -> args.map(toJson))
           ))
 
     val jsonString = compact(render(json))
@@ -33,9 +63,19 @@ object ZeroMQPlayground {
     socket.send(jsonString)
 
     val msg = socket.recvStr()
-    println("Received: " + msg)
-
-
+    msg
   }
+
+  val global = new Module(Nil)
+
+  class Module(prefix:List[String]) extends Dynamic {
+
+    def selectDynamic(name: String) = new Module(prefix :+ name)
+
+    def applyDynamic(name: String)(args: Any*) = {
+      call(prefix :+ name)(args.toList)
+    }
+  }
+
 
 }
