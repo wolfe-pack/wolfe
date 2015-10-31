17bf98a6f92ebc2d15d84eeeea460894ff82f75e
riedelcastro <sebastian.riedel@gmail.com>
2015-10-31 17:00:47 +0000

    nd4j works now

--- wolfe-core/src/test/scala/ml/wolfe/term/ND4SScratch.scala
+++ wolfe-core/src/test/scala/ml/wolfe/term/ND4SScratch.scala
@@ -1,12 +1,16 @@
 package ml.wolfe.term
 
-//import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.factory.Nd4j
+
 //import org.nd4s.Implicits._
 
 /**
  * @author rockt
  */
 object ND4SScratch extends App {
+  val arr = Nd4j.create(Array[Float](1,2,3,4),Array[Int](2,2))
   //val arr = (1 to 9).asNDArray(3,3)
   //println(arr)
+  println(arr)
 }
