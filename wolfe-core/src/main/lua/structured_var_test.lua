26bfabb320e7dfcd9e8bbcf97f4c129dfe8345a2
riedelcastro <sebastian.riedel@gmail.com>
2015-11-03 21:51:11 +0000

    structured variables for torch

--- wolfe-core/src/main/lua/structured_var_test.lua
+++ wolfe-core/src/main/lua/structured_var_test.lua
@@ -6,29 +6,30 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-dofile("var.lua")
+dofile("struct_var.lua")
 
-v = Var(2,2)()
-sig = nn.Sigmoid()(v)
+v = StructVar({torch.LongStorage({1,1}), torch.LongStorage({2,2})})()
+sig = nn.Sigmoid()(nn.SelectTable(2)(v))
 g = nn.gModule({v},{sig})
 ret = g:forward({})
 print(ret)
 ret = g:backward({}, torch.ones(2,2))
-
-
+print(v.data.module.gradWeight[2])
 
 --
-id = nn.Identity()()
 
-
-print(v.data.module.gradWeight)
-
+--
+--id = nn.Identity()()
+--
+--
+--print(v.data.module.gradWeight)
+--
 lin = nn.Linear(2,2)()
 siglin = nn.Sigmoid()(lin)
 gLin = nn.gModule({lin},{siglin})
 
 lin.data.module.bias = torch.Tensor(2)
-lin.data.module.weight = v.data.module.weight
+lin.data.module.weight = v.data.module.weight[2]
 
 print(gLin:forward(torch.diag(torch.ones(2))))
 gLin:backward(torch.diag(torch.ones(2)),torch.ones(2,2))
